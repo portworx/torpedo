4f0911620d1262e1a11154d7ef557886d9f0be12
Varun Turlapati <varun@portworx.com>
2017-09-27 17:48:48 -0700

    Added more info and some line breaks between outputs from diff objects

--- drivers/scheduler/k8s/k8s.go
+++ drivers/scheduler/k8s/k8s.go
@@ -712,6 +712,10 @@ func dumpPodStatusRecursively(pod v1.Pod) string {
 	return buf.String()
 }
 
+func insertLineBreak(note string) string {
+	return fmt.Sprintf("--------------------------\n%s\n--------------------", note)
+}
+
 func contains(nodes []node.Node, n node.Node) bool {
 	for _, value := range nodes {
 		if value.Name == n.Name {
