f8b0cc72561c23d5b8a4d10de7dc90bbebc85f11
Rohit-PX <rohit@portworx.com>
2018-07-23 18:27:55 -0700

    Add debug statements to check pod status in validate volume setup

--- drivers/volume/portworx/schedops/k8s-schedops.go
+++ drivers/volume/portworx/schedops/k8s-schedops.go
@@ -179,7 +179,9 @@ func (k *k8sSchedOps) ValidateVolumeSetup(vol *volume.Volume) error {
 		if ready := k8s.Instance().IsPodReady(p); !ready {
 			continue
 		}
-
+		//TODO: Debug why this fails intermittently
+		//Display pod status at this point.
+		logrus.Infof("Pod status for %s: %v", p.Name, p.Status)
 		containerPaths := getContainerPVCMountMap(p)
 		for containerName, path := range containerPaths {
 			pxMountCheckRegex := regexp.MustCompile(fmt.Sprintf("^(/dev/pxd.+|pxfs.+) %s.+", path))
