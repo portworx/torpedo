e3ffdd97158eb2a38c2d3d14809f2a7a9428f08a
Thiago Gonzaga <thi_gonzaga@yahoo.com.br>
2018-10-12 18:21:04 -0300

    move storageclasses and pvcs to mysql

--- drivers/volume/portworx/portworx.go
+++ drivers/volume/portworx/portworx.go
@@ -429,6 +429,10 @@ func (d *portworx) ValidateCreateVolume(name string, params map[string]string) e
 			if requestedSpec.SnapshotInterval != vol.Spec.SnapshotInterval {
 				return errFailedToInspectVolume(name, k, requestedSpec.SnapshotInterval, vol.Spec.SnapshotInterval)
 			}
+		case api.SpecSnapshotSchedule:
+			if requestedSpec.SnapshotSchedule != vol.Spec.SnapshotSchedule {
+				return errFailedToInspectVolume(name, k, requestedSpec.SnapshotSchedule, vol.Spec.SnapshotSchedule)
+			}
 		case api.SpecAggregationLevel:
 			if requestedSpec.AggregationLevel != vol.Spec.AggregationLevel {
 				return errFailedToInspectVolume(name, k, requestedSpec.AggregationLevel, vol.Spec.AggregationLevel)
@@ -449,9 +453,13 @@ func (d *portworx) ValidateCreateVolume(name string, params map[string]string) e
 			if requestedSpec.GroupEnforced != vol.Spec.GroupEnforced {
 				return errFailedToInspectVolume(name, k, requestedSpec.GroupEnforced, vol.Spec.GroupEnforced)
 			}
+		// portworx injects pvc name and namespace labels so response object won't be equal to request
 		case api.SpecLabels:
-			if !reflect.DeepEqual(requestedLocator.VolumeLabels, vol.Locator.VolumeLabels) {
-				return errFailedToInspectVolume(name, k, requestedLocator.VolumeLabels, vol.Locator.VolumeLabels)
+			for requestedLabelKey, requestedLabelValue := range requestedLocator.VolumeLabels {
+				if labelValue, exists := vol.Locator.VolumeLabels[requestedLabelKey];
+				!exists || requestedLabelValue != labelValue {
+					return errFailedToInspectVolume(name, k, requestedLocator.VolumeLabels, vol.Locator.VolumeLabels)
+				}
 			}
 		case api.SpecIoProfile:
 			if requestedSpec.IoProfile != vol.Spec.IoProfile {
