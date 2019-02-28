d59222d20a2ff9e84577f40c2c4e95f73dfc868b
hrishipx <hrishi@portworx.com>
2019-02-28 17:01:38 +0530

    Fix linting issues

--- tests/common.go
+++ tests/common.go
@@ -13,15 +13,21 @@ import (
 	"github.com/onsi/gomega"
 	"github.com/portworx/sched-ops/task"
 	"github.com/portworx/torpedo/drivers/node"
-	_ "github.com/portworx/torpedo/drivers/node/aws" // import ssh driver to invoke it's init
+	"github.com/sirupsen/logrus"
+	// import aws driver to invoke it's init
+	_ "github.com/portworx/torpedo/drivers/node/aws"
+	// import ssh driver to invoke it's init
 	_ "github.com/portworx/torpedo/drivers/node/ssh"
-	"github.com/portworx/torpedo/drivers/scheduler" // import scheduler drivers to invoke it's init
+	"github.com/portworx/torpedo/drivers/scheduler"
+
+	// import scheduler drivers to invoke it's init
 	_ "github.com/portworx/torpedo/drivers/scheduler/dcos"
 	_ "github.com/portworx/torpedo/drivers/scheduler/k8s"
-	"github.com/portworx/torpedo/drivers/volume" // import portworx driver to invoke it's init
+	"github.com/portworx/torpedo/drivers/volume"
+
+	// import portworx driver to invoke it's init
 	_ "github.com/portworx/torpedo/drivers/volume/portworx"
 	"github.com/portworx/torpedo/pkg/log"
-	"github.com/sirupsen/logrus" // import aws driver to invoke it's init
 )
 
 const (
