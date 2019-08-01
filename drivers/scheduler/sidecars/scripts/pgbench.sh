151656c358d4e6e86bfef7716551dff4abb8129a
Sudarshan Darga <sudarshan@portworx.com>
2019-08-01 15:34:56 +0530

    Update pvc size and the pgbench load size
    Signed-off-by: Sudarshan Darga <sudarshan@portworx.com>

--- drivers/scheduler/sidecars/scripts/pgbench.sh
+++ drivers/scheduler/sidecars/scripts/pgbench.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-until psql -h ${PG_HOST} -U ${PG_USER} -c "create database ${PG_DB}" 
+until psql -h ${PG_HOST} -U ${PG_USER} -c "create database pxdemo" 
 do
       echo "failed to create database. retrying..."
       sleep 2
@@ -8,7 +8,7 @@ done
 
 while :
 do
-      pgbench -U ${PG_USER} -i -s ${TABLE_COUNT} {PG_DB};
+      pgbench -h {PG_HOST} -U ${PG_USER} -i -s ${SIZE} pxdemo;
       sleep 2
 
 done || exit 1
