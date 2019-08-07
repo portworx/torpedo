d3ec3c6cddd686c7486479bf460315f04d14f790
Harsh Desai <harsh@portworx.com>
2019-08-07 15:27:24 -0700

    fix host variable in pgbench script
    Signed-off-by: Harsh Desai <harsh@portworx.com>

--- drivers/scheduler/sidecars/scripts/pgbench.sh
+++ drivers/scheduler/sidecars/scripts/pgbench.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-until psql -h ${PG_HOST} -U ${PG_USER} -c "create database pxdemo" 
+echo "Using host: ${PG_HOST} user: ${PG_USER}"
+until psql -h ${PG_HOST} -U ${PG_USER} -c "create database pxdemo"
 do
       echo "failed to create database. retrying..."
       sleep 2
@@ -8,7 +9,7 @@ done
 
 while :
 do
-      pgbench -h {PG_HOST} -U ${PG_USER} -i -s ${SIZE} pxdemo;
+      pgbench -h ${PG_HOST} -U ${PG_USER} -i -s ${SIZE} pxdemo;
       sleep 2
 
 done || exit 1
