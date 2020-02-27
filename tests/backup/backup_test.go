b1daf84ea4682a893d9e3da60449f7af0acc6565
Raviprasad <raviprasad@portworx.com>
2020-02-27 14:57:47 -0800

    add apis for restore, schedulepolicy and backupschedule

--- tests/backup/backup_test.go
+++ tests/backup/backup_test.go
@@ -2,10 +2,7 @@ package tests
 
 import (
 	"testing"
-	"fmt"
-	"os"
 
-	api "github.com/portworx/px-backup-api/pkg/apis/v1"
 	. "github.com/onsi/ginkgo"
 	"github.com/onsi/ginkgo/reporters"
 	. "github.com/onsi/gomega"
@@ -22,38 +19,16 @@ func TestBackup(t *testing.T) {
 }
 
 var _ = BeforeSuite(func() {
-	fmt.Printf("calling init instance\n")
 	InitInstance()
 })
 
 // This test performs basic test of starting an application and destroying it (along with storage)
 var _ = Describe("{BackupSetup}", func() {
 	It("has to connect and check the backup setup", func() {
-		fmt.Printf("Create an org\n")
-		CreateOrg("sample-org")
 	})
 })
-
-func CreateOrg(orgName string){
-	fmt.Printf("creating sample org\n")
-	backupDriver := Inst().Backup
-	fmt.Printf("backupDriver = %v\n", backupDriver)
-	req := &api.OrganizationCreateRequest{
-		CreateMetadata: &api.CreateMetadata{
-			Name: orgName,
-		},
-	}
-	_, err := backupDriver.CreateOrganization(req)
-	Expect(err).NotTo(HaveOccurred())
-}
 
 var _ = AfterSuite(func() {
 	//PerformSystemCheck()
 	//ValidateCleanup()
 })
-
-func TestMain(m *testing.M) {
-	// call flag.Parse() here if TestMain uses flags
-	ParseFlags()
-	os.Exit(m.Run())
-}
