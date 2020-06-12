cdd393b8006ce2be0a83bcc39cba3807f7368610
stgleb <glebstepanov1992@gmail.com>
2020-06-12 17:10:02 +0300

    PTX-2579 Add IO size  to sysbench

--- drivers/scheduler/sidecars/scripts/sysbench.sh
+++ drivers/scheduler/sidecars/scripts/sysbench.sh
@@ -2,6 +2,12 @@
 trap '' INT
 trap '' QUIT
 trap '' STOP
+
+if [ -z "${SIZE}" ]; then
+    echo "SIZE is not defined, exiting..."
+    exit 1
+fi
+
 until mysql -h ${MYSQL_HOST} -u root -p${MYSQL_ROOT_PASSWORD} -e "CREATE SCHEMA IF NOT EXISTS sbtest ; CREATE USER IF NOT EXISTS sysbench@'%' IDENTIFIED BY 'password'; GRANT ALL PRIVILEGES ON sbtest.* to sysbench@'%';"
 do
   echo "failed to create schema and user. retrying..."
@@ -12,5 +18,13 @@ do
     sysbench --db-driver=mysql --oltp-table-size=10000 --oltp-tables-count=10 --threads=1 --mysql-host=${MYSQL_HOST} --mysql-port=3306 --mysql-user=sysbench --mysql-password=password /usr/share/sysbench/tests/include/oltp_legacy/parallel_prepare.lua prepare
     sysbench --db-driver=mysql --report-interval=1 --mysql-table-engine=innodb --oltp-table-size=10000 --threads=1 --time=5400 --mysql-host=${MYSQL_HOST} --mysql-port=3306 --mysql-user=sysbench --mysql-password=password --olpt-skip-trx /usr/share/sysbench/tests/include/oltp_legacy/oltp.lua run
     sysbench --db-driver=mysql --oltp-table-size=10000 --oltp-tables-count=10 --threads=1 --mysql-host=${MYSQL_HOST} --mysql-port=3306 --mysql-user=sysbench --mysql-password=password /usr/share/sysbench/tests/include/oltp_legacy/oltp.lua cleanup
-    sleep 2
+
+    expected_data_size=$(($SIZE * 1024 * 1024))
+    actual_data_size=$(du -s /var/lib/mysql/* | cut -f1)
+
+    if [ $actual_data_size -le $expected_data_size ]; then
+      sleep 2
+    else
+      break
+    fi
 done
