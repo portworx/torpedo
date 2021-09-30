68209c678eaf38d2b723c8e166a7991bdb4457a7
Jim Ou <jqou@purestorage.com>
2021-09-30 00:23:40 -0600

    changes based on comments

--- tests/basic/sharedv4_svc_test.go
+++ tests/basic/sharedv4_svc_test.go
@@ -23,8 +23,7 @@ const (
 
 var _ = Describe("{NFSServerFailover}", func() {
 	var contexts []*scheduler.Context
-
-	It("has to setup, validate, failover, make sure pods on old server got restarted, and teardown apps", func() {
+	It("has to setup, validate, failover, make sure pods on old and new server got restarted, and teardown apps", func() {
 		contexts = make([]*scheduler.Context, 0)
 
 		for i := 0; i < Inst().GlobalScaleFactor; i++ {
@@ -34,34 +33,27 @@ var _ = Describe("{NFSServerFailover}", func() {
 		ValidateApplications(contexts)
 
 		for _, ctx := range contexts {
-			oneMinute := time.Second * time.Duration(60)
-			var nodesNonReplica []node.Node
+			nodeReplicaMap := make(map[string]bool)
 			var volume *volume.Volume
 			Step("disable scheduling on non replica nodes", func() {
 				vols, err := Inst().S.GetVolumes(ctx)
-				if err != nil {
-					logrus.Infof("get volumes error %v", err)
-				}
+				Expect(err).NotTo(HaveOccurred())
 				volume = vols[0]
 				replicaSets, err := Inst().V.GetReplicaSets(volume)
-				if err != nil {
-					logrus.Infof("get replicaSets error %v", err)
-				}
-				var replicaNodes []string
+				Expect(err).NotTo(HaveOccurred())
 				for _, replicaSet := range replicaSets {
-					nodes := replicaSet.Nodes
-					replicaNodes = append(replicaNodes, nodes...)
-				}
-				allNodes := node.GetWorkerNodes()
-				for _, node := range allNodes {
-					if !contains(replicaNodes, node.VolDriverNodeID) {
-						nodesNonReplica = append(nodesNonReplica, node)
+					for _, node := range replicaSet.Nodes {
+						nodeReplicaMap[node] = true
 					}
 				}
-				for _, node := range nodesNonReplica {
-					Inst().S.DisableSchedulingOnNode(node)
+				// make sure there are 2 replicas
+				Expect(len(nodeReplicaMap)).To(Equal(2))
+				allNodes := node.GetWorkerNodes()
+				for _, node := range allNodes {
+					if !nodeReplicaMap[node.VolDriverNodeID] {
+						Inst().S.DisableSchedulingOnNode(node)
+					}
 				}
-
 			})
 
 			// scale down and then scale up the app, so that pods are only scheduled on replica nodes
@@ -84,9 +76,6 @@ var _ = Describe("{NFSServerFailover}", func() {
 				err = Inst().S.ScaleApplication(ctx, applicationScaleUpMap)
 				Expect(err).NotTo(HaveOccurred())
 				ValidateApplications(contexts)
-				for _, node := range nodesNonReplica {
-					Inst().S.EnableSchedulingOnNode(node)
-				}
 			})
 
 			Step("fail over nfs server, and make sure the pod on server gets restarted", func() {
@@ -102,7 +91,7 @@ var _ = Describe("{NFSServerFailover}", func() {
 					}
 				}
 				// make sure there is a pod running on the old nfs server
-				Expect(&oldPodOnOldServer).NotTo(BeNil())
+				Expect(oldPodOnOldServer.Name).NotTo(Equal(""))
 				logrus.Infof("pod on old server %v, creation time %v", oldPodOnOldServer.Name, oldPodOnOldServer.CreationTimestamp)
 
 				timestampBeforeFailOver := time.Now()
@@ -116,17 +105,21 @@ var _ = Describe("{NFSServerFailover}", func() {
 
 				for i := 0; i < 10; i++ {
 					server, err := Inst().V.GetNodeForVolume(volume, defaultCommandTimeout, defaultCommandRetry)
-					Expect(err).NotTo(HaveOccurred())
-					if server.Id != oldServer.Id {
-						logrus.Infof("nfs server failed over, new nfs server is %s [%s]", server.SchedulerNodeName, server.Addresses[0])
-						newServer = server
-						break
+					// there could be intermittent error here
+					if err != nil {
+						logrus.Infof("Failed to get node for volume. Error: %v", err)
+					} else {
+						if server.Id != oldServer.Id {
+							logrus.Infof("nfs server failed over, new nfs server is %s [%s]", server.SchedulerNodeName, server.Addresses[0])
+							newServer = server
+							break
+						}
 					}
-					time.Sleep(oneMinute)
+					time.Sleep(time.Minute)
 				}
 				// make sure nfs server failed over
 				Expect(newServer).NotTo(BeNil())
-				logrus.Infof("new nfs server is %v", newServer)
+				logrus.Infof("new nfs server is %v [%v]", newServer.SchedulerNodeName, newServer.Addresses[0])
 
 				logrus.Infof("start px on old nfs server Id %v, Name %v", oldServer.Id, oldServer.Name)
 				Inst().V.StartDriver(*oldServer)
@@ -134,28 +127,42 @@ var _ = Describe("{NFSServerFailover}", func() {
 				Expect(err).NotTo(HaveOccurred())
 				logrus.Infof("px is up on old nfs server Id %v, Name %v", oldServer.Id, oldServer.Name)
 
+				ValidateApplications(contexts)
+
 				// make sure the pods on both old and new server are restarted
 				pods, err = core.Instance().GetPodsUsingPV(volume.ID)
 				Expect(err).NotTo(HaveOccurred())
+				podRestartedOnOldServer := false
+				podRestartedOnNewServer := false
 				for _, pod := range pods {
 					if pod.Spec.NodeName == oldServer.Name {
 						logrus.Infof("pod on old server %v, creation time %v", oldPodOnOldServer.Name, oldPodOnOldServer.CreationTimestamp)
 						logrus.Infof("After failover, pod on old server %v, creation time %v", pod.Name, pod.CreationTimestamp)
 						Expect(pod.CreationTimestamp.After(timestampBeforeFailOver)).To(BeTrue())
+						podRestartedOnOldServer = true
 					}
 					if pod.Spec.NodeName == newServer.Name {
 						logrus.Infof("After failover, pod on new server %v, creation time %v", pod.Name, pod.CreationTimestamp)
 						Expect(pod.CreationTimestamp.After(timestampBeforeFailOver)).To(BeTrue())
+						podRestartedOnNewServer = true
+					}
+				}
+
+				Expect(podRestartedOnOldServer).To(BeTrue())
+				Expect(podRestartedOnNewServer).To(BeTrue())
+
+				// re-enable scheduling on non replica nodes
+				allNodes := node.GetWorkerNodes()
+				for _, node := range allNodes {
+					if !nodeReplicaMap[node.VolDriverNodeID] {
+						Inst().S.EnableSchedulingOnNode(node)
 					}
 				}
 			})
 		}
 
-		opts := make(map[string]bool)
-		opts[scheduler.OptionsWaitForResourceLeakCleanup] = true
-
 		for _, ctx := range contexts {
-			TearDownContext(ctx, opts)
+			TearDownContext(ctx, map[string]bool{scheduler.OptionsWaitForResourceLeakCleanup: true})
 		}
 	})
 	JustAfterEach(func() {
