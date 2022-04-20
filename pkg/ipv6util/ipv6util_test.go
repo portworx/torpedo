9c578bd1db468420f53d4db3b8c62d09daed4e7f
dahuang <dahuang@purestorage.com>
2022-04-20 12:03:34 -0600

    Rename const for linters
    Signed-off-by: dahuang <dahuang@purestorage.com>

--- pkg/ipv6util/ipv6util_test.go
+++ pkg/ipv6util/ipv6util_test.go
@@ -8,25 +8,25 @@ import (
 
 func TestValidIpv6Address(t *testing.T) {
 	// pxctl status tests
-	addrs := ParseIPv6AddressInPxctlCommand(PXCTL_STATUS, sampleIpv6PxctlStatusOutput, nodeCount)
-	assert.NotEmpty(t, addrs, "addresses are not expected to be empty. running command: %v", PXCTL_STATUS)
+	addrs := ParseIPv6AddressInPxctlCommand(PxctlStatus, sampleIpv6PxctlStatusOutput, nodeCount)
+	assert.NotEmpty(t, addrs, "addresses are not expected to be empty. running command: %v", PxctlStatus)
 	isIpv6 := AreAddressesIPv6(addrs)
-	assert.True(t, isIpv6, "running command %v. addresses are expected to be ipv6, got: %v", PXCTL_STATUS, addrs)
+	assert.True(t, isIpv6, "running command %v. addresses are expected to be ipv6, got: %v", PxctlStatus, addrs)
 
-	addrs = ParseIPv6AddressInPxctlCommand(PXCTL_STATUS, sampleIpv4PxctlStatusOutput, nodeCount)
-	assert.NotEmpty(t, addrs, "addresses are not expected to be empty. running command: %v", PXCTL_STATUS)
+	addrs = ParseIPv6AddressInPxctlCommand(PxctlStatus, sampleIpv4PxctlStatusOutput, nodeCount)
+	assert.NotEmpty(t, addrs, "addresses are not expected to be empty. running command: %v", PxctlStatus)
 	isIpv6 = AreAddressesIPv6(addrs)
-	assert.False(t, isIpv6, "running command %v. addresses are expected to be ipv4, got: %v", PXCTL_STATUS, addrs)
+	assert.False(t, isIpv6, "running command %v. addresses are expected to be ipv4, got: %v", PxctlStatus, addrs)
 
 	// pxctl cluster list tests
-	addrs = ParseIPv6AddressInPxctlCommand(PXCTL_CLUSTER_LIST, sampleIpv6PxctlClusterListOutput, nodeCount)
-	assert.NotEmpty(t, addrs, "addresses are not expected to be empty. running command: %v", PXCTL_CLUSTER_LIST)
+	addrs = ParseIPv6AddressInPxctlCommand(PxctlClusterList, sampleIpv6PxctlClusterListOutput, nodeCount)
+	assert.NotEmpty(t, addrs, "addresses are not expected to be empty. running command: %v", PxctlClusterList)
 	isIpv6 = AreAddressesIPv6(addrs)
-	assert.True(t, isIpv6, "running command %v. addresses are expected to be ipv6, got: %v", PXCTL_CLUSTER_LIST, addrs)
+	assert.True(t, isIpv6, "running command %v. addresses are expected to be ipv6, got: %v", PxctlClusterList, addrs)
 
 	// pxctl cluster inspect
-	addrs = ParseIPv6AddressInPxctlCommand(PXCTL_CLUSTER_INSPECT, sampleIpv6PxctlClusterInspectOutput, nodeCount)
-	assert.NotEmpty(t, addrs, "addresses are not expected to be empty. running command: %v", PXCTL_CLUSTER_INSPECT)
+	addrs = ParseIPv6AddressInPxctlCommand(PxctlClusterInspect, sampleIpv6PxctlClusterInspectOutput, nodeCount)
+	assert.NotEmpty(t, addrs, "addresses are not expected to be empty. running command: %v", PxctlClusterInspect)
 	isIpv6 = AreAddressesIPv6(addrs)
-	assert.True(t, isIpv6, "running command %v. addresses are expected to be ipv6, got: %v", PXCTL_CLUSTER_INSPECT, addrs)
+	assert.True(t, isIpv6, "running command %v. addresses are expected to be ipv6, got: %v", PxctlClusterInspect, addrs)
 }
