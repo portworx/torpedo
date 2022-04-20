6f0b7e501de4304545d32a654f12198139646714
dahuang <dahuang@purestorage.com>
2022-04-20 03:42:29 -0600

    move parser to private class
    Signed-off-by: dahuang <dahuang@purestorage.com>

--- pkg/ipv6util/ipv6util.go
+++ pkg/ipv6util/ipv6util.go
@@ -26,15 +26,18 @@ type parserOption struct {
 	count  int
 }
 
-func NewIPv6Parser(command string, options []parserOption) parser {
+// newIPv6Parser returns a parser instance
+func newIPv6Parser(command string, options []parserOption) parser {
 	return parser{command, options, nil, nil}
 }
 
-func NewIPv6ParserOption(prefix string, index, count int) parserOption {
+// newIPv6ParserOption returns a parserOptioninstance
+func newIPv6ParserOption(prefix string, index, count int) parserOption {
 	return parserOption{prefix, index, count}
 }
 
-func (p *parser) Parse(cmdOutput string) []string {
+// parse parses the given command output and return the list of IPv6 address
+func (p *parser) parse(cmdOutput string) []string {
 	p.ips = []string{}
 	p.scanner = bufio.NewScanner(strings.NewReader(cmdOutput))
 	for p.scanner.Scan() {
@@ -72,6 +75,7 @@ func (p *parser) parseLine(line string) {
 	}
 }
 
+// ParseIPv6AddressInPxctlCommand takes output of `pxctl command` and return the list of IPs parsed
 func ParseIPv6AddressInPxctlCommand(command string, output string, nodeCount int) []string {
 	switch command {
 	case PXCTL_STATUS:
@@ -95,16 +99,16 @@ func ParseIPv6AddressInPxctlCommand(command string, output string, nodeCount int
 // 		0000:111:2222:3333:444:5555:6666:777	cedc897f-a489-4c28-9c20-12b8b4c3d1d8	...	...
 func parseIPv6AddressInPxctlStatus(status string, nodeCount int) []string {
 	// parse the case for IP: 0000:111:2222:3333:444:5555:6666:777
-	p1 := NewIPv6ParserOption("IP:", 1, 0)
+	p1 := newIPv6ParserOption("IP:", 1, 0)
 
 	// parse the case:
 	// IP					ID					SchedulerNodeName	Auth		StorageNode	Used	Capacity	Status	StorageStatus	Version		Kernel			OS
 	// 0000:111:2222:3333:444:5555:6666:777	f703597a-9772-4bdb-b630-6395b3c98658	...	...
 	// 0000:111:2222:3333:444:5555:6666:777	cedc897f-a489-4c28-9c20-12b8b4c3d1d8	...	...
-	p2 := NewIPv6ParserOption("IP\t", 0, nodeCount)
-	p := NewIPv6Parser("pxctl status", []parserOption{p1, p2})
+	p2 := newIPv6ParserOption("IP\t", 0, nodeCount)
+	p := newIPv6Parser("pxctl status", []parserOption{p1, p2})
 
-	return p.Parse(status)
+	return p.parse(status)
 }
 
 // parseIpv6AddressInPxctlClusterList takes output of `pxctl status` and return the list of IPs parsed
@@ -114,9 +118,9 @@ func parseIPv6AddressInPxctlStatus(status string, nodeCount int) []string {
 // 2ca8932b-b17e-425c-bcbe-d33b0f64b623	node03			0000:111:2222:3333:444:5555:6666:777	... ...
 // 6b9d12e0-fb28-459e-acf1-cea4d57004e2	node04			0000:111:2222:3333:444:5555:6666:777	... ...
 func parseIPv6AddressInPxctlClusterList(output string, nodeCount int) []string {
-	option := NewIPv6ParserOption("ID\t", 2, nodeCount)
-	p := NewIPv6Parser("pxctl cluster list", []parserOption{option})
-	return p.Parse(output)
+	option := newIPv6ParserOption("ID\t", 2, nodeCount)
+	p := newIPv6Parser("pxctl cluster list", []parserOption{option})
+	return p.parse(output)
 }
 
 // parseIPv6AddressInPxctlClusterInspect takes output of `pxctl cluster inspect` and return the list of IPs parsed
@@ -124,10 +128,10 @@ func parseIPv6AddressInPxctlClusterList(output string, nodeCount int) []string {
 // 1. `Mgmt IP\t :\t  <addr>` ex: Mgmt IP       		:  0000:111:2222:3333:444:5555:6666:111
 // 2. `Data IP\t :\t <addr>` ex: Data IP       		:  0000:111:2222:3333:444:5555:6666:111
 func parseIPv6AddressInPxctlClusterInspect(output string, nodeCount int) []string {
-	option1 := NewIPv6ParserOption("Mgmt IP", 3, 0)
-	option2 := NewIPv6ParserOption("Data IP", 3, 0)
-	p := NewIPv6Parser("pxctl cluster list", []parserOption{option1, option2})
-	return p.Parse(output)
+	option1 := newIPv6ParserOption("Mgmt IP", 3, 0)
+	option2 := newIPv6ParserOption("Data IP", 3, 0)
+	p := newIPv6Parser("pxctl cluster list", []parserOption{option1, option2})
+	return p.parse(output)
 }
 
 // IsAddressIPv6 checks the given address is a valid Ipv6 address
