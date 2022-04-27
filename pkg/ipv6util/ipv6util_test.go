ba43ec2f56cf7751080f1c85d0f35ecc78393d81
dahuang <dahuang@purestorage.com>
2022-04-27 14:55:03 -0600

    fixup! fixup! fixup test id
    Signed-off-by: dahuang <dahuang@purestorage.com>

--- pkg/ipv6util/ipv6util_test.go
+++ pkg/ipv6util/ipv6util_test.go
@@ -30,12 +30,12 @@ func TestValidIpv6Address(t *testing.T) {
 	isIpv6 = AreAddressesIPv6(addrs)
 	assert.True(t, isIpv6, "running command %v. addresses are expected to be ipv6, got: %v", PxctlClusterInspect, addrs)
 
-	addrs = ParseIPv6AddressInPxctlCommand(PxctlVolumeList, sampleIpv6PxctlVolumeListOutput, nodeCount)
+	addrs = ParseIPv6AddressInPxctlCommand(PxctlVolumeList, sampleIpv6PxctlVolumeListOutput, sampleNodeCount)
 	assert.NotEmpty(t, addrs, "addresses are not expected to be empty. running command: %v", PxctlVolumeList)
 	isIpv6 = AreAddressesIPv6(addrs)
 	assert.True(t, isIpv6, "running command %v. addresses are expected to be ipv6, got: %v", PxctlVolumeList, addrs)
 
-	addrs = ParseIPv6AddressInPxctlCommand(PxctlVolumeInspect, sampleIpv6PxctlVolumeInspectOutput, nodeCount)
+	addrs = ParseIPv6AddressInPxctlCommand(PxctlVolumeInspect, sampleIpv6PxctlVolumeInspectOutput, sampleNodeCount)
 	assert.NotEmpty(t, addrs, "addresses are not expected to be empty. running command: %v", PxctlVolumeInspect)
 	isIpv6 = AreAddressesIPv6(addrs)
 	assert.True(t, isIpv6, "running command %v. addresses are expected to be ipv6, got: %v", PxctlVolumeInspect, addrs)
