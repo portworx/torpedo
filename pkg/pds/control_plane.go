b34f2e9575dfe6d356518778f9cb40ca4f160979
fmilichovsky <31818576+fmilichovsky@users.noreply.github.com>
2022-04-29 13:11:16 +0200

    Add comments to exported stuff

--- pkg/pds/control_plane.go
+++ pkg/pds/control_plane.go
@@ -5,10 +5,13 @@ import (
 	"time"
 )
 
+// ControlPlane wraps a PDS control plane.
 type ControlPlane struct {
 	*cluster
 }
 
+// NewControlPlane creates a ControlPlane instance using the specified kubeconfig path.
+// Fails if a kubernetes go-client cannot be configured based on the kubeconfig.
 func NewControlPlane(kubeconfig string) (*ControlPlane, error) {
 	cluster, err := newCluster(kubeconfig)
 	if err != nil {
@@ -17,6 +20,7 @@ func NewControlPlane(kubeconfig string) (*ControlPlane, error) {
 	return &ControlPlane{cluster}, nil
 }
 
+// ComponentLogsSince extracts the logs of all relevant PDS components, beginning at the specified time.
 func (cp *ControlPlane) ComponentLogsSince(ctx context.Context, since time.Time) (string, error) {
 	components := []namespacedName{
 		{pdsSystemNamespace, "api-server"},
--- pkg/pds/target_cluster.go
+++ pkg/pds/target_cluster.go
@@ -1,9 +1,12 @@
 package pds
 
+// TargetCluster wraps a PDS target cluster.
 type TargetCluster struct {
 	*cluster
 }
 
+// NewTargetCluster creates a TargetCluster instance with the specified kubeconfig.
+// Fails if a kubernetes go-client cannot be configured based on the kubeconfig.
 func NewTargetCluster(kubeconfig string) (*TargetCluster, error) {
 	cluster, err := newCluster(kubeconfig)
 	if err != nil {
