a6be4d077fd3cec6e12894662c14dfb89dcc4cd8
santhosh marakala <smarakala@purestorage.com>
2022-08-17 10:01:46 -0700

    Addressed review comments and enabled autofstrim
    Signed-off-by: santhosh marakala <smarakala@purestorage.com>

---
+++ pkg/snapshotutils/snapshotutils.go
@@ -0,0 +1,63 @@
+package snapshotutils
+
+import (
+	"time"
+
+	storkv1 "github.com/libopenstorage/stork/pkg/apis/stork/v1alpha1"
+	"github.com/portworx/sched-ops/k8s/stork"
+	"github.com/sirupsen/logrus"
+	meta_v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+var (
+	storkops = stork.Instance()
+)
+
+const (
+	snapshotScheduleRetryInterval = 10 * time.Second
+	snapshotScheduleRetryTimeout  = 5 * time.Minute
+)
+
+// ValidateSnapshotSchedule
+func ValidateSnapshotSchedule(snapshotScheduleName string, appNamespace string) error {
+	logrus.Infof("snapshotScheduleName : %s", snapshotScheduleName)
+	logrus.Infof("Namespace : %v", appNamespace)
+	snapStatuses, err := storkops.ValidateSnapshotSchedule(snapshotScheduleName,
+		appNamespace,
+		snapshotScheduleRetryTimeout,
+		snapshotScheduleRetryInterval)
+	if err != nil {
+		logrus.Errorf("Got error while getting volume snapshot status :%v", err.Error())
+		return err
+	} else {
+		for k, v := range snapStatuses {
+			logrus.Infof("Policy Type: %v", k)
+			for _, e := range v {
+				logrus.Infof("ScheduledVolumeSnapShot Name: %v", e.Name)
+				logrus.Infof("ScheduledVolumeSnapShot status: %v", e.Status)
+			}
+		}
+	}
+	return nil
+}
+
+// SchedulePolicy
+func SchedulePolicyInDefaultNamespace(policyName string, interval int, retain int) error {
+	//Create snapshot schedule interval.
+	logrus.Infof("Creating a interval schedule policy %v with interval %v minutes", policyName, interval)
+	schedPolicy := &storkv1.SchedulePolicy{
+		ObjectMeta: meta_v1.ObjectMeta{
+			Name: policyName,
+		},
+		Policy: storkv1.SchedulePolicyItem{
+			Interval: &storkv1.IntervalPolicy{
+				Retain:          storkv1.Retain(retain),
+				IntervalMinutes: interval,
+			},
+		}}
+	_, err := storkops.CreateSchedulePolicy(schedPolicy)
+	if err != nil {
+		return err
+	}
+	return nil
+}
--- tests/unhappypath/unhappypath_test.go
+++ tests/unhappypath/unhappypath_test.go
@@ -8,18 +8,17 @@ import (
 	"testing"
 	"time"
 
-	storkv1 "github.com/libopenstorage/stork/pkg/apis/stork/v1alpha1"
 	. "github.com/onsi/ginkgo"
 	"github.com/onsi/ginkgo/reporters"
 	. "github.com/onsi/gomega"
 	"github.com/portworx/sched-ops/k8s/stork"
 	"github.com/portworx/torpedo/drivers/node"
 	"github.com/portworx/torpedo/drivers/scheduler"
-	"github.com/portworx/torpedo/drivers/volume"
+	"github.com/portworx/torpedo/pkg/kvdbutils"
+	"github.com/portworx/torpedo/pkg/snapshotutils"
 	"github.com/portworx/torpedo/pkg/testrailuttils"
 	. "github.com/portworx/torpedo/tests"
 	"github.com/sirupsen/logrus"
-	meta_v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 var (
@@ -30,11 +29,10 @@ const (
 	dropPercentage      = 20
 	delayInMilliseconds = 250
 	//24 hours
-	totalTimeInHours = 24
-	// TODO need to make it 60 minutes
-	errorPersistTimeInMinutes     = 10 * time.Minute
+	totalTimeInHours              = 24
+	errorPersistTimeInMinutes     = 60 * time.Minute
 	snapshotScheduleRetryInterval = 10 * time.Second
-	snapshotScheduleRetryTimeout  = 3 * time.Minute
+	snapshotScheduleRetryTimeout  = 5 * time.Minute
 )
 
 func TestBasic(t *testing.T) {
@@ -48,10 +46,10 @@ func TestBasic(t *testing.T) {
 
 var _ = BeforeSuite(func() {
 	InitInstance()
-	// package installation hangs when using nsenter to run command on nodes
 	if Inst().N.IsUsingSSH() {
 		for _, anode := range node.GetWorkerNodes() {
-			// TODO: support other OS'es
+			// TODO need to remove tcpdump installation using yum
+			// once https://portworx.atlassian.net/browse/PTX-12174 is resolved.
 			logrus.Infof("installing tcpdump on node %s", anode.Name)
 			cmd := "yum install -y tcpdump"
 			_, err := Inst().N.RunCommandWithNoRetry(anode, cmd, node.ConnectionOpts{
@@ -63,11 +61,24 @@ var _ = BeforeSuite(func() {
 				logrus.Warnf("failed to install tcpdump on node %s: %v", anode.Name, err)
 				break
 			}
+			//Check tcpdump command
+			cmd = "compgen -c | grep tcpdump"
+			out, errInSSH := Inst().N.RunCommandWithNoRetry(anode, cmd, node.ConnectionOpts{
+				Timeout:         1 * time.Minute,
+				TimeBeforeRetry: 5 * time.Second,
+				Sudo:            true,
+			})
+			if err != nil {
+				logrus.Warnf("failed to install tcpdump on node %s: %v", anode.Name, errInSSH)
+			}
+			if !strings.Contains(out, "tcpdump") {
+				logrus.Warnf("TCP dump is not avaiable in %s:", anode.Name)
+			}
 		}
 	}
 })
 
-// This test scales up and down an application and checks if app has actually scaled accordingly
+// This test is to verify stability of the system when there  is  a network error on the system.
 var _ = Describe("{NetworkErrorInjection}", func() {
 	var testrailID = 3526435
 	injectionType := "drop"
@@ -89,19 +100,28 @@ var _ = Describe("{NetworkErrorInjection}", func() {
 		timeToExecuteTest := time.Now().Local().Add(time.Hour*time.Duration(totalTimeInHours) +
 			time.Minute*time.Duration(0) +
 			time.Second*time.Duration(0))
+
+		// Set Autofs trim
+		currNode := node.GetWorkerNodes()[0]
+		err := Inst().V.SetClusterOpts(currNode, map[string]string{
+			"--auto-fstrim": "on",
+		})
+		if err != nil {
+			err = fmt.Errorf("error while enabling auto fstrim, Error:%v", err)
+			Expect(err).NotTo(HaveOccurred())
+		}
+
 		Step("Verify applications after deployment", func() {
 			for _, ctx := range contexts {
 				ValidateContext(ctx)
 			}
 		})
-
 		Step("Create snapshot schedule policy", func() {
 			createSnapshotSchedule(contexts)
 		})
 
 		for int64(timeToExecuteTest.Sub(currentTime).Seconds()) > 0 {
 			// TODO core check
-			//TODO Enable fstrim
 			logrus.Infof("Remaining time to test in minutes : %d ", int64(timeToExecuteTest.Sub(currentTime).Seconds()/60))
 			Step("Set packet loss on random nodes ", func() {
 				//Get all nodes and set eth0
@@ -114,9 +134,17 @@ var _ = Describe("{NetworkErrorInjection}", func() {
 				Inst().N.InjectNetworkError(selectedNodes, injectionType, "add", dropPercentage, delayInMilliseconds)
 				logrus.Infof("Wait %d minutes before checking px status ", errorPersistTimeInMinutes/(time.Minute))
 				time.Sleep(errorPersistTimeInMinutes)
+				hasPXUp := true
 				for _, n := range nodes {
 					logrus.Infof("Check PX status on %v", n.Name)
-					Inst().V.WaitForPxPodsToBeUp(n)
+					err := Inst().V.WaitForPxPodsToBeUp(n)
+					if err != nil {
+						hasPXUp = false
+						logrus.Errorf("PX failed to be in ready state  %v %s ", n.Name, err)
+					}
+				}
+				if !hasPXUp {
+					Expect(fmt.Errorf("PX is not ready on on or more nodes ")).NotTo(HaveOccurred())
 				}
 				logrus.Infof("Clear network error injection ")
 				Inst().N.InjectNetworkError(selectedNodes, injectionType, "delete", 0, 0)
@@ -127,8 +155,8 @@ var _ = Describe("{NetworkErrorInjection}", func() {
 					injectionType = "drop"
 				}
 			})
-			logrus.Infof("Wait 5 minutes before checking px status ")
-			time.Sleep(5 * time.Minute)
+			logrus.Infof("Wait 20 minutes before checking px status ")
+			time.Sleep(20 * time.Minute)
 			Step("Verify application after clearing error", func() {
 				for _, ctx := range contexts {
 					ValidateContext(ctx)
@@ -141,7 +169,7 @@ var _ = Describe("{NetworkErrorInjection}", func() {
 					err = fmt.Errorf("Error getting kvdb members using node %v. cause: %v", nodes[0].Name, err)
 					Expect(err).NotTo(HaveOccurred())
 				}
-				err = ValidateKVDBMembers(kvdbMembers)
+				err = kvdbutils.ValidateKVDBMembers(kvdbMembers)
 				Expect(err).NotTo(HaveOccurred())
 			})
 			Step("Check Cloudsnap status ", func() {
@@ -201,7 +229,7 @@ func verifyCloudSnaps(contexts []*scheduler.Context) {
 		//Verify cloudsnap is continuing
 		for _, v := range appVolumes {
 			if strings.Contains(ctx.App.Key, "cloudsnap") == false {
-				logrus.Infof("Apps are not cloudsnap supported %s ", v.Name)
+				logrus.Warningf("Apps are not cloudsnap supported %s ", v.Name)
 				continue
 			}
 			// Skip cloud snapshot trigger for Pure DA volumes
@@ -213,86 +241,30 @@ func verifyCloudSnaps(contexts []*scheduler.Context) {
 				logrus.Warnf("Cloud snapshot is not supported for Pure DA volumes: [%s]", v.Name)
 				continue
 			}
-			err = ValidateSnapshotSchedule(ctx, v)
+			snapshotScheduleName := v.Name + "-interval-schedule"
+			logrus.Infof("snapshotScheduleName : %v for volume: %s", snapshotScheduleName, v.Name)
+			appNamespace := ctx.App.Key + "-" + ctx.UID
+			logrus.Infof("Namespace : %v", appNamespace)
+
+			err = snapshotutils.ValidateSnapshotSchedule(snapshotScheduleName, appNamespace)
 			Expect(err).NotTo(HaveOccurred())
 		}
 	}
 }
 
-// ValidateKVDBMembers health and availability.
-func ValidateKVDBMembers(kvdbMembers map[string]*volume.MetadataNode) error {
-	logrus.Infof("Current KVDB members: %v", kvdbMembers)
-	if len(kvdbMembers) < 3 {
-		err := fmt.Errorf("No KVDB membes to validate or less than 3 members to validate")
-		logrus.Warn(err.Error())
-		return err
-	}
-	for id, m := range kvdbMembers {
-		if !m.IsHealthy {
-			err := fmt.Errorf("kvdb member node: %v is not healthy", id)
-			logrus.Warn(err.Error())
-			return err
-		}
-		logrus.Infof("KVDB member node %v is healthy", id)
-	}
-	return nil
-}
-
 // SchedulePolicy
 func SchedulePolicy(ctx *scheduler.Context, policyName string, interval int) error {
 	if strings.Contains(ctx.App.Key, "cloudsnap") {
-
 		logrus.Infof("APP with cloudsnap key available %v ", ctx.App.Key)
 		schedPolicy, err := storkops.GetSchedulePolicy(policyName)
 		if err == nil {
 			logrus.Infof("schedPolicy is %v already exists", schedPolicy.Name)
 		} else {
-			//Create snapshot schedule interval.
-			retain := 2
-			logrus.Infof("Creating a interval schedule policy %v with interval %v minutes", policyName, interval)
-			schedPolicy = &storkv1.SchedulePolicy{
-				ObjectMeta: meta_v1.ObjectMeta{
-					Name: policyName,
-				},
-				Policy: storkv1.SchedulePolicyItem{
-					Interval: &storkv1.IntervalPolicy{
-						Retain:          storkv1.Retain(retain),
-						IntervalMinutes: interval,
-					},
-				}}
-			_, err = storkops.CreateSchedulePolicy(schedPolicy)
-			if err != nil {
-				return err
-			}
+			err = snapshotutils.SchedulePolicyInDefaultNamespace(policyName, interval, 2)
+			Expect(err).NotTo(HaveOccurred())
 		}
 		logrus.Infof("Waiting for 10 mins for Snapshots to be completed")
 		time.Sleep(10 * time.Minute)
-	}
-	return nil
-}
-
-// ValidateSnapshotSchedule
-func ValidateSnapshotSchedule(ctx *scheduler.Context, appVolume *volume.Volume) error {
-	//TODO If cloudsnap is enabled
-	snapshotScheduleName := appVolume.Name + "-interval-schedule"
-	logrus.Infof("snapshotScheduleName : %v for volume: %s", snapshotScheduleName, appVolume.Name)
-	appNamespace := ctx.App.Key + "-" + ctx.UID
-	logrus.Infof("Namespace : %v", appNamespace)
-	snapStatuses, err := storkops.ValidateSnapshotSchedule(snapshotScheduleName,
-		appNamespace,
-		snapshotScheduleRetryTimeout,
-		snapshotScheduleRetryInterval)
-	if err == nil {
-		for k, v := range snapStatuses {
-			logrus.Infof("Policy Type: %v", k)
-			for _, e := range v {
-				logrus.Infof("ScheduledVolumeSnapShot Name: %v", e.Name)
-				logrus.Infof("ScheduledVolumeSnapShot status: %v", e.Status)
-			}
-		}
-	} else {
-		logrus.Infof("Got error while getting volume snapshot status :%v", err.Error())
-		return err
 	}
 	return nil
 }
