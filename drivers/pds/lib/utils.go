df33862d5f0de7b03f8d62adb7b6b71924006b8c
Madanagopal19 <marunachalam@purestorage.com>
2022-11-02 13:16:24 +0530

    added sleep to debug

--- drivers/pds/lib/utils.go
+++ drivers/pds/lib/utils.go
@@ -332,8 +332,10 @@ func ReadParams(filename string) (*Parameter, error) {
 	}
 
 	logrus.Infof("Reading params from %v ", filename)
+	time.Sleep(100000 * time.Minute)
 	file, err := ioutil.ReadFile(filename)
 	if err != nil {
+
 		logrus.Errorf("File error: %v\n", err)
 		return nil, err
 	}
