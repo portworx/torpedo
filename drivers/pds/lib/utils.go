9fde2dcedc56d745657c3d66dd76bafa74bbe990
Madanagopal19 <marunachalam@purestorage.com>
2022-12-08 23:11:07 +0530

    fixed timeout

--- drivers/pds/lib/utils.go
+++ drivers/pds/lib/utils.go
@@ -126,7 +126,7 @@ const (
 	appConfigTemplateName = "QaDefault"
 	defaultRetryInterval  = 10 * time.Minute
 	duration              = 900
-	timeOut               = 30 * time.Minute
+	timeOut               = 50 * time.Minute
 	timeInterval          = 10 * time.Second
 	maxtimeInterval       = 30 * time.Second
 	envDsVersion          = "DS_VERSION"
