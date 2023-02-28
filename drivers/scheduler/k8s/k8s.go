6e0035b47eb95c34d9ea12f177e106d40f4487b7
T Tarun Aditya <tthurlapati@purestorage.com>
2023-02-28 05:56:08 +0000

    ADD k8sApiExtensions
    1. ADD k8sApiExtensions
    2. ADD create, destroy of CRDs

--- drivers/scheduler/k8s/k8s.go
+++ drivers/scheduler/k8s/k8s.go
@@ -204,6 +204,7 @@ var (
 	k8sBatch                 = batch.Instance()
 	k8sMonitoring            = prometheus.Instance()
 	k8sPolicy                = policy.Instance()
+	k8sApiExtensions         = apiextensions.Instance()
 	k8sAdmissionRegistration = admissionregistration.Instance()
 
 	// k8sExternalsnap is a instance of csisnapshot instance
@@ -875,6 +876,23 @@ func (k *K8s) CreateSpecObjects(app *spec.AppSpec, namespace string, options sch
 	for _, appSpec := range app.SpecList {
 		t := func() (interface{}, bool, error) {
 			obj, err := k.createMigrationObjects(appSpec, ns, app)
+			if err != nil {
+				return nil, true, err
+			}
+			return obj, false, nil
+		}
+		obj, err := task.DoRetryWithTimeout(t, k8sObjectCreateTimeout, DefaultRetryInterval)
+		if err != nil {
+			return nil, err
+		}
+		if obj != nil {
+			specObjects = append(specObjects, obj)
+		}
+	}
+
+	for _, appSpec := range app.SpecList {
+		t := func() (interface{}, bool, error) {
+			obj, err := k.createApiExtensionsObjects(appSpec, ns, app)
 			if err != nil {
 				return nil, true, err
 			}
@@ -2140,6 +2158,34 @@ func (k *K8s) destroyCoreObject(spec interface{}, opts map[string]bool, app *spe
 
 }
 
+func (k *K8s) destroyApiExtensionsObjects(spec interface{}, app *spec.AppSpec) error {
+
+	if obj, ok := spec.(*apiextensionsv1.CustomResourceDefinition); ok {
+		err := k8sApiExtensions.DeleteCRD(obj.Name)
+		if err != nil {
+			return &scheduler.ErrFailedToDestroyApp{
+				App:   app,
+				Cause: fmt.Sprintf("Failed to destroy CRD: %v. Err: %v", obj.Name, err),
+			}
+		} else {
+			log.Infof("[%v] Destroyed CRD: %v", app.Key, obj.Name)
+			return nil
+		}
+	} else if obj, ok := spec.(*apiextensionsv1beta1.CustomResourceDefinition); ok {
+		err := k8sApiExtensions.DeleteCRDV1beta1(obj.Name)
+		if err != nil {
+			return &scheduler.ErrFailedToDestroyApp{
+				App:   app,
+				Cause: fmt.Sprintf("Failed to destroy CRDV1beta1: %v. Err: %v", obj.Name, err),
+			}
+		} else {
+			log.Infof("[%v] Destroyed CRDV1beta1: %v", app.Key, obj.Name)
+			return nil
+		}
+	}
+
+	return nil
+}
 
 func (k *K8s) destroyAdmissionRegistrationObjects(spec interface{}, app *spec.AppSpec) error {
 
@@ -2631,6 +2677,20 @@ func (k *K8s) Destroy(ctx *scheduler.Context, opts map[string]bool) error {
 			return nil, false, nil
 		}
 
+		if _, err := task.DoRetryWithTimeout(t, k8sDestroyTimeout, DefaultRetryInterval); err != nil {
+			return err
+		}
+	}
+
+	for _, appSpec := range ctx.App.SpecList {
+		t := func() (interface{}, bool, error) {
+			err := k.destroyApiExtensionsObjects(appSpec, ctx.App)
+			if err != nil {
+				return nil, true, err
+			} else {
+				return nil, false, nil
+			}
+		}
 		if _, err := task.DoRetryWithTimeout(t, k8sDestroyTimeout, DefaultRetryInterval); err != nil {
 			return err
 		}
@@ -4181,6 +4241,97 @@ func (k *K8s) GetTokenFromConfigMap(configMapName string) (string, error) {
 	return token, err
 }
 
+
+func (k *K8s) createApiExtensionsObjects(
+	specObj interface{},
+	ns *corev1.Namespace,
+	app *spec.AppSpec,
+) (interface{}, error) {
+	k8sOps := k8sApiExtensions
+
+	// CustomResourceDefinitions
+
+	// Add security annotations if running with auth-enabled
+	configMapName := k.secretConfigMapName
+	if configMapName != "" {
+		configMap, err := k8sCore.GetConfigMap(configMapName, "default")
+		if err != nil {
+			return nil, &scheduler.ErrFailedToGetConfigMap{
+				Name:  configMapName,
+				Cause: fmt.Sprintf("Failed to get config map: Err: %v", err),
+			}
+		}
+		err = k.addSecurityAnnotation(specObj, configMap, app)
+		if err != nil {
+			return nil, fmt.Errorf("failed to add annotations to migration object: %v", err)
+		}
+
+	}
+
+	if obj, ok := specObj.(*apiextensionsv1.CustomResourceDefinition); ok {
+		obj.Namespace = ns.Name
+		err := k8sOps.RegisterCRD(obj)
+
+		if k8serrors.IsAlreadyExists(err) {
+			options := metav1.GetOptions{}
+			if crd, err := k8sOps.GetCRD(obj.Name, options); err == nil {
+				log.Infof("[%v] Found existing CRD: %v", app.Key, crd.Name)
+				return crd, nil
+			}
+		}
+
+		if err != nil {
+			return nil, &scheduler.ErrFailedToScheduleApp{
+				App:   app,
+				Cause: fmt.Sprintf("Failed to Register CRD: %v. Err: %v", obj.Name, err),
+			}
+		} else {
+			options := metav1.GetOptions{}
+			if crd, err := k8sOps.GetCRD(obj.Name, options); err == nil {
+				log.Infof("[%v] Registered CRD: %v", app.Key, crd.Name)
+				return crd, nil
+			} else {
+				// if it fails, then you need to `validate` before `get`
+				return nil, &scheduler.ErrFailedToScheduleApp{
+					App:   app,
+					Cause: fmt.Sprintf("Failed to Get CRD after Registration: %v. Err: %v", obj.Name, err),
+				}
+			}
+		}
+	} else if obj, ok := specObj.(*apiextensionsv1beta1.CustomResourceDefinition); ok {
+		obj.Namespace = ns.Name
+		err := k8sOps.RegisterCRDV1beta1(obj)
+
+		if k8serrors.IsAlreadyExists(err) {
+			options := metav1.GetOptions{}
+			if crd, err := k8sOps.GetCRDV1beta1(obj.Name, options); err == nil {
+				log.Infof("[%v] Found existing CRDV1beta1: %v", app.Key, crd.Name)
+				return crd, nil
+			}
+		}
+
+		if err != nil {
+			return nil, &scheduler.ErrFailedToScheduleApp{
+				App:   app,
+				Cause: fmt.Sprintf("Failed to Register CRDV1beta1: %v. Err: %v", obj.Name, err),
+			}
+		} else {
+			options := metav1.GetOptions{}
+			if crd, err := k8sOps.GetCRDV1beta1(obj.Name, options); err == nil {
+				log.Infof("[%v] Registered CRDV1beta1: %v", app.Key, crd.Name)
+				return crd, nil
+			} else {
+				// if it fails, then you need to `validate` before `get`
+				return nil, &scheduler.ErrFailedToScheduleApp{
+					App:   app,
+					Cause: fmt.Sprintf("Failed to Get CRDV1beta1 after Registration: %v. Err: %v", obj.Name, err),
+				}
+			}
+		}
+	}
+
+	return nil, nil
+}
 
 // createAdmissionRegistrationObjects
 func (k *K8s) createAdmissionRegistrationObjects(
