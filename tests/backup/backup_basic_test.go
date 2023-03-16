3c36a90a3523a47dab813e5f518d4d58232c8d76
Mithun Koppal <mkoppal@purestorage.com>
2023-03-16 11:38:52 +0530

    Commented out cleanup code for ShareBackupsAndClusterWithUser

--- tests/backup/backup_basic_test.go
+++ tests/backup/backup_basic_test.go
@@ -2,16 +2,10 @@ package tests
 
 import (
 	"fmt"
-	"os"
-	"strings"
-	"testing"
-	"time"
-
 	. "github.com/onsi/ginkgo"
 	"github.com/onsi/ginkgo/reporters"
 	. "github.com/onsi/gomega"
 	api "github.com/portworx/px-backup-api/pkg/apis/v1"
-	"github.com/portworx/sched-ops/task"
 	"github.com/portworx/torpedo/drivers"
 	"github.com/portworx/torpedo/drivers/backup"
 	"github.com/portworx/torpedo/drivers/node"
@@ -19,6 +13,9 @@ import (
 	"github.com/portworx/torpedo/pkg/aetosutil"
 	"github.com/portworx/torpedo/pkg/log"
 	. "github.com/portworx/torpedo/tests"
+	"os"
+	"strings"
+	"testing"
 )
 
 func getBucketNameSuffix() string {
@@ -160,102 +157,102 @@ var _ = AfterSuite(func() {
 	defer dash.TestSetEnd()
 	defer EndTorpedoTest()
 
-	// Cleanup all non admin users
-	ctx, err := backup.GetAdminCtxFromSecret()
-	log.FailOnError(err, "Fetching px-central-admin ctx")
-	allUsers, err := backup.GetAllUsers()
-	dash.VerifySafely(err, nil, "Verifying cleaning up of all users from keycloak")
-	for _, user := range allUsers {
-		if !strings.Contains(user.Name, "admin") {
-			err = backup.DeleteUser(user.Name)
-			dash.VerifySafely(err, nil, fmt.Sprintf("Verifying user [%s] deletion", user.Name))
-		} else {
-			log.Infof("User %s was not deleted", user.Name)
-		}
-	}
-	// Cleanup all non admin groups
-	allGroups, err := backup.GetAllUsers()
-	dash.VerifySafely(err, nil, "Verifying cleaning up of all groups from keycloak")
-	for _, group := range allGroups {
-		if !strings.Contains(group.Name, "admin") && !strings.Contains(group.Name, "app") {
-			err = backup.DeleteGroup(group.Name)
-			dash.VerifySafely(err, nil, fmt.Sprintf("Verifying group [%s] deletion", group.Name))
-		} else {
-			log.Infof("Group %s was not deleted", group.Name)
-		}
-	}
-
-	// Cleanup all backups
-	allBackups, err := GetAllBackupsAdmin()
-	for _, backupName := range allBackups {
-		backupUID, err := Inst().Backup.GetBackupUID(ctx, backupName, orgID)
-		dash.VerifySafely(err, nil, fmt.Sprintf("Getting backuip UID for backup %s", backupName))
-		_, err = DeleteBackup(backupName, backupUID, orgID, ctx)
-		dash.VerifySafely(err, nil, fmt.Sprintf("Verifying backup deletion - %s", backupName))
-	}
-
-	// Cleanup all restores
-	allRestores, err := GetAllRestoresAdmin()
-	for _, restoreName := range allRestores {
-		err = DeleteRestore(restoreName, orgID, ctx)
-		dash.VerifySafely(err, nil, fmt.Sprintf("Verifying restore deletion - %s", restoreName))
-	}
-
-	// Cleanup all backup locations
-	allBackupLocations, err := getAllBackupLocations(ctx)
-	dash.VerifySafely(err, nil, "Verifying fetching of all backup locations")
-	for backupLocationUid, backupLocationName := range allBackupLocations {
-		err = DeleteBackupLocation(backupLocationName, backupLocationUid, orgID, true)
-		dash.VerifySafely(err, nil, fmt.Sprintf("Verifying backup location deletion - %s", backupLocationName))
-	}
-
-	backupLocationDeletionSuccess := func() (interface{}, bool, error) {
-		allBackupLocations, err := getAllBackupLocations(ctx)
-		dash.VerifySafely(err, nil, "Verifying fetching of all backup locations")
-		if len(allBackupLocations) > 0 {
-			return "", true, fmt.Errorf("found %d backup locations", len(allBackupLocations))
-		} else {
-			return "", false, nil
-		}
-	}
-	_, err = task.DoRetryWithTimeout(backupLocationDeletionSuccess, 5*time.Minute, 30*time.Second)
-	dash.VerifySafely(err, nil, "Verifying backup location deletion success")
-
-	// Cleanup all cloud credentials
-	allCloudCredentials, err := getAllCloudCredentials(ctx)
-	dash.VerifySafely(err, nil, "Verifying fetching of all cloud credentials")
-	for cloudCredentialUid, cloudCredentialName := range allCloudCredentials {
-		err = DeleteCloudCredential(cloudCredentialName, orgID, cloudCredentialUid)
-		dash.VerifySafely(err, nil, fmt.Sprintf("Deleting cloud cred %s", cloudCredentialName))
-	}
-
-	cloudCredentialDeletionSuccess := func() (interface{}, bool, error) {
-		allCloudCredentials, err := getAllCloudCredentials(ctx)
-		dash.VerifySafely(err, nil, "Verifying fetching of all cloud credentials")
-		if len(allCloudCredentials) > 0 {
-			return "", true, fmt.Errorf("found %d cloud credentials", len(allBackupLocations))
-		} else {
-			return "", false, nil
-		}
-	}
-	_, err = task.DoRetryWithTimeout(cloudCredentialDeletionSuccess, 5*time.Minute, 30*time.Second)
-	dash.VerifySafely(err, nil, "Verifying backup location deletion success")
-
-	// Cleanup all buckets after suite
-	providers := getProviders()
-	for _, provider := range providers {
-		switch provider {
-		case drivers.ProviderAws:
-			DeleteBucket(provider, globalAWSBucketName)
-			log.Infof("Bucket deleted - %s", globalAWSBucketName)
-		case drivers.ProviderAzure:
-			DeleteBucket(provider, globalAzureBucketName)
-			log.Infof("Bucket deleted - %s", globalAzureBucketName)
-		case drivers.ProviderGke:
-			DeleteBucket(provider, globalGCPBucketName)
-			log.Infof("Bucket deleted - %s", globalGCPBucketName)
-		}
-	}
+	//// Cleanup all non admin users
+	//ctx, err := backup.GetAdminCtxFromSecret()
+	//log.FailOnError(err, "Fetching px-central-admin ctx")
+	//allUsers, err := backup.GetAllUsers()
+	//dash.VerifySafely(err, nil, "Verifying cleaning up of all users from keycloak")
+	//for _, user := range allUsers {
+	//	if !strings.Contains(user.Name, "admin") {
+	//		err = backup.DeleteUser(user.Name)
+	//		dash.VerifySafely(err, nil, fmt.Sprintf("Verifying user [%s] deletion", user.Name))
+	//	} else {
+	//		log.Infof("User %s was not deleted", user.Name)
+	//	}
+	//}
+	//// Cleanup all non admin groups
+	//allGroups, err := backup.GetAllUsers()
+	//dash.VerifySafely(err, nil, "Verifying cleaning up of all groups from keycloak")
+	//for _, group := range allGroups {
+	//	if !strings.Contains(group.Name, "admin") && !strings.Contains(group.Name, "app") {
+	//		err = backup.DeleteGroup(group.Name)
+	//		dash.VerifySafely(err, nil, fmt.Sprintf("Verifying group [%s] deletion", group.Name))
+	//	} else {
+	//		log.Infof("Group %s was not deleted", group.Name)
+	//	}
+	//}
+	//
+	//// Cleanup all backups
+	//allBackups, err := GetAllBackupsAdmin()
+	//for _, backupName := range allBackups {
+	//	backupUID, err := Inst().Backup.GetBackupUID(ctx, backupName, orgID)
+	//	dash.VerifySafely(err, nil, fmt.Sprintf("Getting backuip UID for backup %s", backupName))
+	//	_, err = DeleteBackup(backupName, backupUID, orgID, ctx)
+	//	dash.VerifySafely(err, nil, fmt.Sprintf("Verifying backup deletion - %s", backupName))
+	//}
+	//
+	//// Cleanup all restores
+	//allRestores, err := GetAllRestoresAdmin()
+	//for _, restoreName := range allRestores {
+	//	err = DeleteRestore(restoreName, orgID, ctx)
+	//	dash.VerifySafely(err, nil, fmt.Sprintf("Verifying restore deletion - %s", restoreName))
+	//}
+	//
+	//// Cleanup all backup locations
+	//allBackupLocations, err := getAllBackupLocations(ctx)
+	//dash.VerifySafely(err, nil, "Verifying fetching of all backup locations")
+	//for backupLocationUid, backupLocationName := range allBackupLocations {
+	//	err = DeleteBackupLocation(backupLocationName, backupLocationUid, orgID, true)
+	//	dash.VerifySafely(err, nil, fmt.Sprintf("Verifying backup location deletion - %s", backupLocationName))
+	//}
+	//
+	//backupLocationDeletionSuccess := func() (interface{}, bool, error) {
+	//	allBackupLocations, err := getAllBackupLocations(ctx)
+	//	dash.VerifySafely(err, nil, "Verifying fetching of all backup locations")
+	//	if len(allBackupLocations) > 0 {
+	//		return "", true, fmt.Errorf("found %d backup locations", len(allBackupLocations))
+	//	} else {
+	//		return "", false, nil
+	//	}
+	//}
+	//_, err = task.DoRetryWithTimeout(backupLocationDeletionSuccess, 5*time.Minute, 30*time.Second)
+	//dash.VerifySafely(err, nil, "Verifying backup location deletion success")
+	//
+	//// Cleanup all cloud credentials
+	//allCloudCredentials, err := getAllCloudCredentials(ctx)
+	//dash.VerifySafely(err, nil, "Verifying fetching of all cloud credentials")
+	//for cloudCredentialUid, cloudCredentialName := range allCloudCredentials {
+	//	err = DeleteCloudCredential(cloudCredentialName, orgID, cloudCredentialUid)
+	//	dash.VerifySafely(err, nil, fmt.Sprintf("Deleting cloud cred %s", cloudCredentialName))
+	//}
+	//
+	//cloudCredentialDeletionSuccess := func() (interface{}, bool, error) {
+	//	allCloudCredentials, err := getAllCloudCredentials(ctx)
+	//	dash.VerifySafely(err, nil, "Verifying fetching of all cloud credentials")
+	//	if len(allCloudCredentials) > 0 {
+	//		return "", true, fmt.Errorf("found %d cloud credentials", len(allBackupLocations))
+	//	} else {
+	//		return "", false, nil
+	//	}
+	//}
+	//_, err = task.DoRetryWithTimeout(cloudCredentialDeletionSuccess, 5*time.Minute, 30*time.Second)
+	//dash.VerifySafely(err, nil, "Verifying backup location deletion success")
+	//
+	//// Cleanup all buckets after suite
+	//providers := getProviders()
+	//for _, provider := range providers {
+	//	switch provider {
+	//	case drivers.ProviderAws:
+	//		DeleteBucket(provider, globalAWSBucketName)
+	//		log.Infof("Bucket deleted - %s", globalAWSBucketName)
+	//	case drivers.ProviderAzure:
+	//		DeleteBucket(provider, globalAzureBucketName)
+	//		log.Infof("Bucket deleted - %s", globalAzureBucketName)
+	//	case drivers.ProviderGke:
+	//		DeleteBucket(provider, globalGCPBucketName)
+	//		log.Infof("Bucket deleted - %s", globalGCPBucketName)
+	//	}
+	//}
 
 })
 
