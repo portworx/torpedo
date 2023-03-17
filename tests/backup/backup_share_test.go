d17d6dcadf7e662e1328f1cdfc7b8aa479a0d4db
Mithun Koppal <mkoppal@purestorage.com>
2023-03-17 10:55:34 +0530

    Reverting the number of users

--- tests/backup/backup_share_test.go
+++ tests/backup/backup_share_test.go
@@ -425,9 +425,9 @@ var _ = Describe("{DifferentAccessSameUser}", func() {
 
 // ShareBackupWithUsersAndGroups shares backup with multiple users and groups with different access
 var _ = Describe("{ShareBackupWithUsersAndGroups}", func() {
-	numberOfUsers := 6
+	numberOfUsers := 30
 	numberOfGroups := 3
-	groupSize := 2
+	groupSize := 10
 	numberOfBackups := 9
 	users := make([]string, 0)
 	groups := make([]string, 0)
