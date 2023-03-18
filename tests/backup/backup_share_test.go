0b3b0d3ef51794e2a7ba03ca62673fff03bb8728
sn-px@purestorage.com <sn@purestorage.com>
2023-03-18 17:12:25 +0530

    Adding cleanup for share backup test and impl of polling for delete user validation

--- tests/backup/backup_share_test.go
+++ tests/backup/backup_share_test.go
@@ -3972,18 +3972,6 @@ var _ = Describe("{SwapShareBackup}", func() {
 			}
 		}
 
-		currentTestFailed := CurrentGinkgoTestDescription().Failed
-		if !currentTestFailed {
-			log.InfoD("Delete all backups")
-			log.InfoD("Current test passed")
-			for i := 0; i <= numberOfUsers-1; i++ {
-				ctx, err := backup.GetNonAdminCtx(users[i], "Password1")
-				log.FailOnError(err, "Fetching nonAdminCtx ")
-				_, err = DeleteBackup(backupName, backupUIDList[i], orgID, ctx)
-				dash.VerifySafely(err, nil, fmt.Sprintf("Verifying backup deletion - %s", backupName))
-			}
-		}
-
 		for _, userName := range users {
 			ctx, err := backup.GetNonAdminCtx(userName, "Password1")
 			log.FailOnError(err, "Fetching nonAdminCtx ")
