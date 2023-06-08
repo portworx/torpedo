a8e72cbdaa76728ada40012673e82328016edbbd
T Tarun Aditya <tthurlapati@purestorage.com>
2023-06-08 14:10:41 +0000

    REFACT(CreateRestore)
    Signed-off-by: T Tarun Aditya <tthurlapati@purestorage.com>

--- tests/backup/backup_helper.go
+++ tests/backup/backup_helper.go
@@ -676,40 +676,8 @@ func GetAllBackupsForUser(username, password string) ([]string, error) {
 }
 
 // CreateRestore creates restore
-func CreateRestore(restoreName string, backupName string, namespaceMapping map[string]string, clusterName string,
-	orgID string, ctx context.Context, storageClassMapping map[string]string) error {
-
-	var bkp *api.BackupObject
-	var bkpUid string
-	backupDriver := Inst().Backup
-	log.Infof("Getting the UID of the backup %s needed to be restored", backupName)
-	bkpEnumerateReq := &api.BackupEnumerateRequest{
-		OrgId: orgID}
-	curBackups, err := backupDriver.EnumerateBackup(ctx, bkpEnumerateReq)
-	if err != nil {
-		return err
-	}
-	for _, bkp = range curBackups.GetBackups() {
-		if bkp.Name == backupName {
-			bkpUid = bkp.Uid
-			break
-		}
-	}
-	createRestoreReq := &api.RestoreCreateRequest{
-		CreateMetadata: &api.CreateMetadata{
-			Name:  restoreName,
-			OrgId: orgID,
-		},
-		Backup:              backupName,
-		Cluster:             clusterName,
-		NamespaceMapping:    namespaceMapping,
-		StorageClassMapping: storageClassMapping,
-		BackupRef: &api.ObjectRef{
-			Name: backupName,
-			Uid:  bkpUid,
-		},
-	}
-	_, err = backupDriver.CreateRestore(ctx, createRestoreReq)
+func CreateRestore(restoreName string, backupName string, namespaceMapping map[string]string, clusterName string, orgID string, ctx context.Context, storageClassMapping map[string]string) error {
+	_, err := CreateRestoreWithoutCheck(ctx, restoreName, backupName, namespaceMapping, storageClassMapping, nil, clusterName, orgID)
 	if err != nil {
 		return err
 	}
