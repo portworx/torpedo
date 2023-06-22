94528ac4656fe163bff6a10668cc0931ce643871
kphalgun-px <kphalgun@purestorage.com>
2023-06-22 11:29:10 +0530

    Make changes

--- drivers/backup/controller/torpedotest/controller.go
+++ drivers/backup/controller/torpedotest/controller.go
@@ -4,6 +4,19 @@ type TorpedoTestController struct {
 	TorpedoTestManager *TorpedoTestManager
 }
 
-func (c *TorpedoTestController) TorpedoTest(testId string) {
+func (c *TorpedoTestController) GetTorpedoTestManager() *TorpedoTestManager {
+	return c.TorpedoTestManager
+}
 
+func (c *TorpedoTestController) SetTorpedoTestManager(manager *TorpedoTestManager) {
+	c.TorpedoTestManager = manager
+}
+
+func (c *TorpedoTestController) TorpedoTest(testId string) *TorpedoTestConfig {
+	torpedoTestConfig := NewTorpedoTestConfig()
+	torpedoTestMetaData := NewTorpedoTestMetaData()
+	torpedoTestMetaData.SetTestId(testId)
+	torpedoTestConfig.SetTorpedoTestMetaData(torpedoTestMetaData)
+	torpedoTestConfig.SetTorpedoTestController(c)
+	return torpedoTestConfig
 }
--- drivers/backup/controller/torpedotest/torpedotest_manager.go
+++ drivers/backup/controller/torpedotest/torpedotest_manager.go
@@ -1,7 +1,15 @@
 package torpedotest
 
 import (
+	"fmt"
+	"github.com/portworx/torpedo/drivers/scheduler"
+	"github.com/portworx/torpedo/pkg/log"
+	"github.com/portworx/torpedo/pkg/testrailuttils"
+	"github.com/portworx/torpedo/tests"
 	"gopkg.in/natefinch/lumberjack.v2"
+	"strconv"
+	"strings"
+	"sync"
 )
 
 // TorpedoTestMetaData represents the metadata for a TorpedoTest
@@ -55,6 +63,65 @@ func (c *TorpedoTestConfig) GetTorpedoTestController() *TorpedoTestController {
 // SetTorpedoTestController sets the TorpedoTestController for the TorpedoTestConfig
 func (c *TorpedoTestConfig) SetTorpedoTestController(controller *TorpedoTestController) {
 	c.TorpedoTestController = controller
+}
+
+func (c *TorpedoTestConfig) Start(testRailId int, testName string, testDescription string, testAuthor string, apps []string, tags map[string]string) {
+	if testRailId != 0 {
+		c.GetTorpedoTestController().GetTorpedoTestManager().IsTorpedoTestPresent()
+		if pxBackupTorpedoTestInfo, ok := PxBackupTorpedoTestInfoMap[testRailId]; ok {
+			err := fmt.Errorf("the test [%s] shares the same TestRail id as [%s] and has already been executed", testName, pxBackupTorpedoTestInfo.TestName)
+			return err
+		}
+	}
+	testTags := map[string]string{
+		"author": testAuthor,
+		"apps":   strings.Join(apps, ","),
+	}
+	if len(tags) > 0 {
+		for tagKey, tagValue := range tags[0] {
+			testTags[tagKey] = tagValue
+		}
+	}
+	testLogger := tests.CreateLogger(fmt.Sprintf("%s.log", testName))
+	log.SetTorpedoFileOutput(testLogger)
+	tests.Inst().Dash.TestCaseBegin(testName, testDescription, strconv.Itoa(testRailId), testTags)
+	var testRunIdForSuite int
+	if tests.TestRailSetupSuccessful && testRailId != 0 {
+		testRunIdForSuite = testrailuttils.AddRunsToMilestone(testRailId)
+	}
+	pxBackupTorpedoTestInfo := &PxBackupTorpedoTestInfo{
+		TestName:          testName,
+		TestDescription:   testDescription,
+		TestRailID:        testRailId,
+		TestAuthor:        testAuthor,
+		TestTags:          testTags,
+		TestLogger:        testLogger,
+		TestRunIdForSuite: testRunIdForSuite,
+	}
+	PxBackupTorpedoTestInfoMap[testRailId] = pxBackupTorpedoTestInfo
+	return nil
+}
+
+func (c *TorpedoTestConfig) End() {
+	if pxBackupTorpedoTestInfo, ok := PxBackupTorpedoTestInfoMap[testRailId]; ok {
+		tests.CloseLogger(pxBackupTorpedoTestInfo.testLogger)
+		tests.Inst().Dash.TestCaseEnd()
+		if tests.TestRailSetupSuccessful && pxBackupTorpedoTestInfo.testRailID != 0 && pxBackupTorpedoTestInfo.testRunIdForSuite != 0 {
+			contexts := make([]*scheduler.Context, 0)
+			tests.AfterEachTest(contexts, pxBackupTorpedoTestInfo.testRailID, pxBackupTorpedoTestInfo.testRunIdForSuite)
+		}
+	} else {
+		err := fmt.Errorf("no test has been executed with the TestRail id [%d]", testRailId)
+		return err
+	}
+	return nil
+}
+
+func NewTorpedoTestConfig() *TorpedoTestConfig {
+	newTorpedoTestConfig := &TorpedoTestConfig{}
+	newTorpedoTestConfig.SetTorpedoTestMetaData(NewTorpedoTestMetaData())
+	newTorpedoTestConfig.SetTorpedoTestController(nil)
+	return newTorpedoTestConfig
 }
 
 // TorpedoTest represents an TorpedoTest
@@ -153,34 +220,80 @@ func NewTorpedoTest() *TorpedoTest {
 
 // TorpedoTestManager represents a manager for TorpedoTest
 type TorpedoTestManager struct {
-	TorpedoTestMap           map[string]*TorpedoTest
-	CompletedTorpedoTestsMap map[string][]*TorpedoTest
+	sync.RWMutex
+	TorpedoTestMap         map[string]*TorpedoTest
+	RemovedTorpedoTestsMap map[string][]*TorpedoTest
 }
 
 // GetTorpedoTestMap returns the TorpedoTestMap of the TorpedoTestManager
 func (m *TorpedoTestManager) GetTorpedoTestMap() map[string]*TorpedoTest {
+	m.RLock()
+	defer m.RUnlock()
 	return m.TorpedoTestMap
 }
 
 // SetTorpedoTestMap sets the TorpedoTestMap of the TorpedoTestManager
 func (m *TorpedoTestManager) SetTorpedoTestMap(testMap map[string]*TorpedoTest) {
+	m.Lock()
+	defer m.Unlock()
 	m.TorpedoTestMap = testMap
 }
 
-// GetCompletedTorpedoTestsMap returns the CompletedTorpedoTestsMap of the TorpedoTestManager
-func (m *TorpedoTestManager) GetCompletedTorpedoTestsMap() map[string][]*TorpedoTest {
-	return m.CompletedTorpedoTestsMap
+// GetRemovedTorpedoTestsMap returns the RemovedTorpedoTestsMap of the TorpedoTestManager
+func (m *TorpedoTestManager) GetRemovedTorpedoTestsMap() map[string][]*TorpedoTest {
+	m.RLock()
+	defer m.RUnlock()
+	return m.RemovedTorpedoTestsMap
 }
 
-// SetCompletedTorpedoTestsMap sets the CompletedTorpedoTestsMap of the TorpedoTestManager
-func (m *TorpedoTestManager) SetCompletedTorpedoTestsMap(completedTestsMap map[string][]*TorpedoTest) {
-	m.CompletedTorpedoTestsMap = completedTestsMap
+// SetRemovedTorpedoTestsMap sets the RemovedTorpedoTestsMap of the TorpedoTestManager
+func (m *TorpedoTestManager) SetRemovedTorpedoTestsMap(removedTestsMap map[string][]*TorpedoTest) {
+	m.Lock()
+	defer m.Unlock()
+	m.RemovedTorpedoTestsMap = removedTestsMap
+}
+
+// GetTorpedoTest returns the TorpedoTest with the given TorpedoTest uid
+func (m *TorpedoTestManager) GetTorpedoTest(torpedoTestUid string) *TorpedoTest {
+	m.RLock()
+	defer m.RUnlock()
+	return m.GetTorpedoTestMap()[torpedoTestUid]
+}
+
+// IsTorpedoTestPresent checks if the TorpedoTest with the given TorpedoTest uid is present
+func (m *TorpedoTestManager) IsTorpedoTestPresent(torpedoTestUid string) bool {
+	m.RLock()
+	defer m.RUnlock()
+	_, isPresent := m.GetTorpedoTestMap()[torpedoTestUid]
+	return isPresent
+}
+
+// SetTorpedoTest sets the TorpedoTest with the given TorpedoTest uid
+func (m *TorpedoTestManager) SetTorpedoTest(torpedoTestUid string, torpedoTest *TorpedoTest) {
+	m.Lock()
+	defer m.Unlock()
+	m.GetTorpedoTestMap()[torpedoTestUid] = torpedoTest
+}
+
+// DeleteTorpedoTest deletes the TorpedoTest with the given TorpedoTest uid
+func (m *TorpedoTestManager) DeleteTorpedoTest(torpedoTestUid string) {
+	m.Lock()
+	defer m.Unlock()
+	delete(m.GetTorpedoTestMap(), torpedoTestUid)
+}
+
+// RemoveTorpedoTest removes the TorpedoTest with the given TorpedoTest uid
+func (m *TorpedoTestManager) RemoveTorpedoTest(torpedoTestUid string) {
+	m.Lock()
+	defer m.Unlock()
+	m.GetRemovedTorpedoTestsMap()[torpedoTestUid] = append(m.GetRemovedTorpedoTestsMap()[torpedoTestUid], m.GetTorpedoTest(torpedoTestUid))
+	m.DeleteTorpedoTest(torpedoTestUid)
 }
 
 // NewTorpedoTestManager creates a new instance of the TorpedoTestManager
 func NewTorpedoTestManager() *TorpedoTestManager {
 	newTorpedoTestManager := &TorpedoTestManager{}
 	newTorpedoTestManager.SetTorpedoTestMap(make(map[string]*TorpedoTest, 0))
-	newTorpedoTestManager.SetCompletedTorpedoTestsMap(make(map[string][]*TorpedoTest, 0))
+	newTorpedoTestManager.SetRemovedTorpedoTestsMap(make(map[string][]*TorpedoTest, 0))
 	return newTorpedoTestManager
 }
