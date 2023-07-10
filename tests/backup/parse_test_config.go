f6813025476be8abbafaa0890e3d94e62c39c4f5
sn-px@purestorage.com <sn@purestorage.com>
2023-07-10 16:26:05 +0530

    test inputs

--- tests/backup/parse_test_config.go
+++ tests/backup/parse_test_config.go
@@ -7,75 +7,97 @@ import (
 	"os"
 )
 
-type CloudProvider struct {
-	Provider        string `json:"provider"`
-	AccessKeyID     string `json:"access_key_id,omitempty"`
-	SecretAccessKey string `json:"secret_access_key,omitempty"`
-	Region          string `json:"region,omitempty"`
-	Tag             string `json:"tag,omitempty"`
-	SubscriptionID  string `json:"subscription_id,omitempty"`
-	ClientID        string `json:"client_id,omitempty"`
-	ClientSecret    string `json:"client_secret,omitempty"`
-	TenantID        string `json:"tenant_id,omitempty"`
-	ProjectID       string `json:"project_id,omitempty"`
-	ClusterName     string `json:"cluster_name,omitempty"`
-	Location        string `json:"location,omitempty"`
-	CredentialsFile string `json:"credentials_file,omitempty"`
-	APIKey          string `json:"api_key,omitempty"`
-	ResourceGroup   string `json:"resource_group,omitempty"`
+type CloudProviders struct {
+	AWS   AWS   `json:"aws"`
+	Azure Azure `json:"azure"`
+	GKE   GKE   `json:"gke"`
+	IBM   IBM   `json:"ibm"`
 }
 
-type BackupTarget struct {
+type AWS struct {
+	Default AWSCredentials `json:"default"`
+}
+
+type Azure struct {
+	Default AzureCredentials `json:"default"`
+}
+
+type GKE struct {
+	Default GKECredentials `json:"default"`
+}
+
+type IBM struct {
+	Default IBMCredentials `json:"default"`
+}
+
+type AWSCredentials struct {
+	AccessKeyID     string `json:"access_key_id"`
+	SecretAccessKey string `json:"secret_access_key"`
+	Region          string `json:"region"`
+}
+
+type AzureCredentials struct {
+	SubscriptionID string `json:"subscription_id"`
+	ClientID       string `json:"client_id"`
+	ClientSecret   string `json:"client_secret"`
+	TenantID       string `json:"tenant_id"`
+}
+
+type GKECredentials struct {
+	ProjectID       string `json:"project_id"`
+	ClusterName     string `json:"cluster_name"`
+	Location        string `json:"location"`
+	CredentialsFile string `json:"credentials_file"`
+}
+
+type IBMCredentials struct {
+	APIKey        string `json:"api_key"`
+	Region        string `json:"region"`
+	ResourceGroup string `json:"resource_group"`
+}
+
+type BackupTargets struct {
+	Buckets    Buckets    `json:"buckets"`
+	NFSServers NFSServers `json:"nfsServer"`
+}
+
+type Buckets struct {
+	AWS BucketsAWS `json:"aws"`
+}
+
+type BucketsAWS struct {
+	Default Bucket `json:"default"`
+}
+
+type Bucket struct {
 	Name            string `json:"name"`
 	Provider        string `json:"provider"`
-	AccessKeyID     string `json:"access_key_id,omitempty"`
-	SecretAccessKey string `json:"secret_access_key,omitempty"`
-	Region          string `json:"region,omitempty"`
-	Tag             string `json:"tag,omitempty"`
-	IP              string `json:"ip,omitempty"`
-	ExportPath      string `json:"export_path,omitempty"`
-	SubPath         string `json:"sub_path,omitempty"`
-	MountOptions    string `json:"mount_options,omitempty"`
-	EncryptionKey   string `json:"encryption_key,omitempty"`
+	AccessKeyID     string `json:"access_key_id"`
+	SecretAccessKey string `json:"secret_access_key"`
+	Region          string `json:"region"`
+	Tag             string `json:"tag"`
 }
 
-type Configuration struct {
-	CloudProviders []CloudProvider `json:"cloudProviders"`
-	BackupTargets  struct {
-		Buckets   []BackupTarget `json:"buckets"`
-		NFSServer []BackupTarget `json:"nfs-server"`
-	} `json:"backupTargets"`
+type NFSServers struct {
+	Default NFSServer `json:"default"`
 }
 
-func getCloudProviderCred(Cloudprovider string, tag string) (*CloudProvider, error) {
-	config, _ := getConfigObj()
-	// Access the parsed data
-	for _, provider := range config.CloudProviders {
-		fmt.Println("Provider:", provider.Provider)
-		fmt.Println("Tag:", provider.Tag)
-
-		if provider.Provider == Cloudprovider && provider.Tag == tag {
-			return &provider, nil
-		}
-	}
-	return nil, fmt.Errorf("unable to find the cloud provider %s with tag %s", Cloudprovider, tag)
+type NFSServer struct {
+	Name          string `json:"name"`
+	IP            string `json:"ip"`
+	ExportPath    string `json:"export_path"`
+	SubPath       string `json:"sub_path"`
+	MountOptions  string `json:"mount_options"`
+	EncryptionKey string `json:"encryption_key"`
+	Tag           string `json:"tag"`
 }
 
-func getBackupTargets(backupTarget string, tag string) (*BackupTarget, error) {
-	config, _ := getConfigObj()
-	// Access the parsed data
-	for _, provider := range config.BackupTargets.Buckets {
-		fmt.Println("Provider:", provider.Provider)
-		fmt.Println("Tag:", provider.Tag)
-
-		if provider.Provider == backupTarget && provider.Tag == tag {
-			return &provider, nil
-		}
-	}
-	return nil, fmt.Errorf("unable to find the cloud provider %s with tag %s", backupTarget, tag)
+type Config struct {
+	CloudProviders CloudProviders `json:"cloudProviders"`
+	BackupTargets  BackupTargets  `json:"backupTargets"`
 }
 
-func getConfigObj() (*Configuration, error) {
+func getConfigObj() (*Config, error) {
 
 	_, err := os.Getwd()
 	// Read JSON file into a variable
@@ -86,7 +108,7 @@ func getConfigObj() (*Configuration, error) {
 		return nil, fmt.Errorf("unable to read the test configutation file in the path %s", testConfigPath)
 	}
 	// Parse JSON into Configuration struct
-	var config Configuration
+	var config Config
 	err = json.Unmarshal(jsonData, &config)
 
 	return &config, nil
