7e345ccd19f4a0e5d347d5dcea78f850206c204a
sn-px@purestorage.com <sn@purestorage.com>
2023-07-13 15:33:41 +0530

    add cred to cloud_config

--- tests/backup/backup_test.go
+++ tests/backup/backup_test.go
@@ -40,6 +40,7 @@ var _ = Describe("{testParseConfig}", func() {
 
 	It("Testing parse config", func() {
 		provider, _ := backup.GetConfigObj()
+		time.Sleep(300 * time.Second)
 		log.InfoD("Aws Access Key ID: %s", provider.CloudProviders.GetAWSCredential("default").AccessKeyID)
 		log.InfoD("Aws secret Key ID: %s", provider.CloudProviders.GetAWSCredential("default").SecretAccessKey)
 		log.InfoD("Region from backup target: %s", provider.BackupTargets.GetAWSBucket("default").Region)
