7ca1a9ab52fd449001dad7b7653a4057a82caae4
Mithun Koppal <mkoppal@purestorage.com>
2023-07-19 00:59:16 +0530

    Changed name

--- tests/backup/backup_test.go
+++ tests/backup/backup_test.go
@@ -35,7 +35,7 @@ var _ = Describe("{BackupClusterVerification}", func() {
 })
 
 // Reference test to access the cloud config details from the global variable
-var _ = Describe("{testParseConfig}", func() {
+var _ = Describe("{DummyParseConfig}", func() {
 	StartTorpedoTest("testParseConfig", "testcreds", nil, 11111)
 	It("Testing parse config", func() {
 		log.InfoD("Aws Access Key ID: %s", GlobalCredentialConfig.CloudProviders.GetAWSCredential("default").AccessKeyID)
