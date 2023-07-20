95acbbebf62ea8042d440fb8ecefca6997356e99
gauraa2 <agaurav@purestorage.com>
2023-07-20 17:28:05 +0530

    Addressed comment.

--- tests/pds/restore_test.go
+++ tests/pds/restore_test.go
@@ -10,7 +10,6 @@ import (
 	tc "github.com/portworx/torpedo/drivers/pds/targetcluster"
 	"github.com/portworx/torpedo/pkg/log"
 	. "github.com/portworx/torpedo/tests"
-	"net/http"
 )
 
 var (
@@ -30,6 +29,7 @@ var _ = Describe("{PerformRestoreToSameCluster}", func() {
 		bkpTarget, err = bkpClient.CreateAwsS3BackupCredsAndTarget(tenantID, fmt.Sprintf("%v-aws", bkpTargetName), deploymentTargetID)
 		log.FailOnError(err, "Failed to create S3 backup target.")
 		log.InfoD("AWS S3 target - %v created successfully", bkpTarget.GetName())
+		awsBkpTargets = append(awsBkpTargets, bkpTarget)
 	})
 
 	It("Perform multiple restore within same cluster", func() {
@@ -51,6 +51,9 @@ var _ = Describe("{PerformRestoreToSameCluster}", func() {
 					deployment, _, _, err = DeployandValidateDataServices(ds, params.InfraToTest.Namespace, tenantID, projectID)
 					deploymentsToBeCleaned = append(deploymentsToBeCleaned, deployment)
 					log.FailOnError(err, "Error while deploying data services")
+
+					// TODO: Add workload generation
+
 					dsEntity = restoreBkp.DSEntity{
 						Deployment: deployment,
 					}
@@ -58,11 +61,11 @@ var _ = Describe("{PerformRestoreToSameCluster}", func() {
 				stepLog = "Perform adhoc backup and validate them"
 				Step(stepLog, func() {
 					log.InfoD(stepLog)
-					log.Infof("Deployment ID: %v, backuptargetID: %v", deployment.GetId(), bkpTarget.GetId())
+					log.Infof("Deployment ID: %v, backup target ID: %v", deployment.GetId(), bkpTarget.GetId())
 					err = bkpClient.TriggerAndValidateAdhocBackup(deployment.GetId(), bkpTarget.GetId(), "s3")
 					log.FailOnError(err, "Failed while performing adhoc backup")
 				})
-				stepLog = "Perform restore for above backup."
+				stepLog = "Perform restore for the backup jobs."
 				Step(stepLog, func() {
 					log.InfoD(stepLog)
 					ctx := pdslib.GetAndExpectStringEnvVar("PDS_RESTORE_TARGET_CLUSTER")
@@ -80,32 +83,23 @@ var _ = Describe("{PerformRestoreToSameCluster}", func() {
 						log.Infof("[Restoring] Details Backup job name- %v, Id- %v", backupJob.GetName(), backupJob.GetId())
 						restoredModel, err := restoreClient.TriggerAndValidateRestore(backupJob.GetId(), params.InfraToTest.Namespace, dsEntity, true, true)
 						log.FailOnError(err, "Failed during restore.")
+						log.Infof("Validate ")
 						restoredDeployment, err = restoreClient.Components.DataServiceDeployment.GetDeployment(restoredModel.GetDeploymentId())
 						log.FailOnError(err, fmt.Sprintf("Failed while fetching the restore data service instance: %v", restoredModel.GetClusterResourceName()))
 						deploymentsToBeCleaned = append(deploymentsToBeCleaned, restoredDeployment)
-						log.Infof("Restored successfully. Details: Deployment- %v, Status - %v", restoredModel.GetClusterResourceName(), restoredModel.GetStatus())
+						log.InfoD("Restored successfully. Details: Deployment- %v, Status - %v", restoredModel.GetClusterResourceName(), restoredModel.GetStatus())
 					}
 				})
 
 				Step("Delete Deployments", func() {
-					cleanupDeployment(deploymentsToBeCleaned)
+					CleanupDeployments(deploymentsToBeCleaned)
 				})
 			}
 		})
 	})
 	JustAfterEach(func() {
 		defer EndTorpedoTest()
+		err := bkpClient.AWSStorageClient.DeleteBucket()
+		log.FailOnError(err, "Failed while deleting the bucket")
 	})
 })
-
-func cleanupDeployment(dsInstances []*pds.ModelsDeployment) {
-	for _, dsInstance := range dsInstances {
-		log.InfoD("Deleting Deployment %v ", *dsInstance.ClusterResourceName)
-		resp, err := pdslib.DeleteDeployment(dsInstance.GetId())
-		log.FailOnError(err, "Error while deleting data services")
-		dash.VerifyFatal(resp.StatusCode, http.StatusAccepted, "validating the status response")
-		log.InfoD("Getting all PV and associated PVCs and deleting them")
-		err = pdslib.DeletePvandPVCs(*dsInstance.ClusterResourceName, false)
-		log.FailOnError(err, "Error while deleting PV and PVCs")
-	}
-}
