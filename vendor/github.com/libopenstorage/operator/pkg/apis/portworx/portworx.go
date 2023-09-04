a47776e4e7f6795b49ec3f3819ffff02d0356837
Mithun Koppal <mkoppal@purestorage.com>
2023-09-04 18:39:42 +0530

    Vendored in 2.5.2 Px Backup APIs

--- vendor/github.com/libopenstorage/operator/pkg/apis/portworx/portworx.go
+++
@@ -1,9 +0,0 @@
-// Package v1 contains API Schema definitions for the portworx API group
-// +k8s:deepcopy-gen=package,register
-// +groupName=portworx.io
-package portworx
-
-const (
-	// GroupName is the group name of the CRD
-	GroupName = "portworx.io"
-)
--- vendor/github.com/libopenstorage/stork/pkg/objectstore/common/common.go
+++
@@ -1,9 +0,0 @@
-package common
-
-// ObjLockInfo struct
-type ObjLockInfo struct {
-	LockMode             string
-	LockEnabled          bool
-	RetentionPeriodDays  int64
-	RetentionPeriodYears int64
-}
