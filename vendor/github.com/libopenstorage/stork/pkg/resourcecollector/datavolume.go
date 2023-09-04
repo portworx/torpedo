2ebf8a4e88578ec51c88d1c2fcb0cb373374599b
Mithun Koppal <mkoppal@purestorage.com>
2023-09-04 18:39:42 +0530

    Vendored in 2.5.2 Px Backup APIs

--- vendor/github.com/libopenstorage/stork/pkg/resourcecollector/datavolume.go
+++
@@ -1,9 +0,0 @@
-package resourcecollector
-
-import "k8s.io/apimachinery/pkg/runtime"
-
-func (r *ResourceCollector) dataVolumesToBeCollected(
-	object runtime.Unstructured,
-) (bool, error) {
-	return false, nil
-}
