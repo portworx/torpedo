468cc3d8cd9dbfd2ef1c33d7bffbc96fb1f92c7d
Mithun Koppal <mkoppal@purestorage.com>
2023-09-04 18:39:42 +0530

    Vendored in 2.5.2 Px Backup APIs

--- vendor/github.com/libopenstorage/autopilot-api/pkg/apis/autopilot/register.go
+++
@@ -1,8 +0,0 @@
-package autopilot
-
-const (
-	// GroupName is the group name of the CRD
-	GroupName = "autopilot.libopenstorage.org"
-	// Version is the version of the CRD
-	Version = "v1alpha1"
-)
