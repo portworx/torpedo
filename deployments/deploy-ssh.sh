bb8a5cc5336ba72761567969e4b6169d11464157
Avinash Vasantha Reddy <avasanthareddy@purestorage.com>
2023-09-27 10:37:24 +0530

    debug the gin issue

--- deployments/deploy-ssh.sh
+++ deployments/deploy-ssh.sh
@@ -469,8 +469,6 @@ spec:
     imagePullPolicy: Always
     securityContext:
       privileged: ${SECURITY_CONTEXT}
-
-    command: ["sh", "-c", "cd /torpedo-gin && go run apiServer/pxone/apiserver.go &"]
     command: ["sh", "-c", "sleep 3600"]
     tty: true
     volumeMounts: [${VOLUME_MOUNTS}]
