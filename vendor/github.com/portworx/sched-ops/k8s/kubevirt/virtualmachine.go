489e89d77fc3aff602d9d9570010ad923e68a1c6
Mithun Koppal <mkoppal@purestorage.com>
2023-10-12 15:03:23 +0530

    Added more logging

--- vendor/github.com/portworx/sched-ops/k8s/kubevirt/virtualmachine.go
+++ vendor/github.com/portworx/sched-ops/k8s/kubevirt/virtualmachine.go
@@ -75,7 +75,7 @@ func (c *Client) ValidateVirtualMachineRunning(name, namespace string, timeout, 
 	if err != nil {
 		return fmt.Errorf("failed to get Virtual Machine")
 	}
-
+	fmt.Printf("VM spec - %v", &vm.Spec)
 	// Start the VirtualMachine if its not Started yet
 	if !*vm.Spec.Running {
 		if err = instance.StartVirtualMachine(vm); err != nil {
