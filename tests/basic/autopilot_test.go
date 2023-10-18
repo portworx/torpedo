f8863192f791604f667bdb3824c6fe3cd98464a4
spai-px <spai@purestorage.com>
2023-10-18 14:41:49 +0530

    remove node config

--- tests/basic/autopilot_test.go
+++ tests/basic/autopilot_test.go
@@ -1785,7 +1785,6 @@ func scheduleAppsWithAutopilot(testName string, testScaleFactor int, apRules []a
 					StorageProvisioner: Inst().Provisioner,
 					PvcNodesAnnotation: options.PvcNodesAnnotation,
 					PvcSize:            options.PvcSize,
-					Nodes:              options.Nodes,
 				})
 				Expect(err).NotTo(HaveOccurred())
 				Expect(context).NotTo(BeEmpty())
