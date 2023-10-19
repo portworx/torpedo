de5b449f4d3dfda2928c6e1855b9d638ebd7923f
Mithun Koppal <mkoppal@purestorage.com>
2023-10-19 20:26:53 +0530

    Fixed cleanup

--- tests/backup/backup_kubevirt_test.go
+++ tests/backup/backup_kubevirt_test.go
@@ -373,15 +373,6 @@ var _ = Describe("{KubevirtVMBackupRestoreWithDifferentStates}", func() {
 		err = SetClusterContext("")
 		log.FailOnError(err, "failed to SetClusterContext to default cluster")
 
-		backupDriver := Inst().Backup
-		log.Info("Deleting backed up namespaces")
-		for _, backupName := range backupNames {
-			backupUID, err := backupDriver.GetBackupUID(ctx, backupName, orgID)
-			log.FailOnError(err, "Failed while trying to get backup UID for - %s", backupName)
-			backupDeleteResponse, err := DeleteBackup(backupName, backupUID, orgID, ctx)
-			log.FailOnError(err, "Backup [%s] could not be deleted", backupName)
-			dash.VerifyFatal(backupDeleteResponse.String(), "", fmt.Sprintf("Verifying [%s] backup deletion is successful", backupName))
-		}
 		log.Info("Deleting restored namespaces")
 		for _, restoreName := range restoreNames {
 			err = DeleteRestore(restoreName, orgID, ctx)
