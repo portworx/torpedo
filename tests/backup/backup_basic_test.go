55685cabd56203b34c2c0508e3cb047401b88ef6
sn-px@purestorage.com <sn@purestorage.com>
2023-10-26 21:25:05 +0530

    SSE TC Automation

--- tests/backup/backup_basic_test.go
+++ tests/backup/backup_basic_test.go
@@ -5,9 +5,7 @@ import (
 	. "github.com/onsi/ginkgo"
 	"github.com/onsi/ginkgo/reporters"
 	. "github.com/onsi/gomega"
-	api "github.com/portworx/px-backup-api/pkg/apis/v1"
 	_ "github.com/portworx/px-backup-api/pkg/kubeauth/gcp"
-	"github.com/portworx/sched-ops/task"
 	"github.com/portworx/torpedo/drivers"
 	"github.com/portworx/torpedo/drivers/backup"
 	"github.com/portworx/torpedo/drivers/node"
@@ -19,7 +17,6 @@ import (
 	"os"
 	"strings"
 	"testing"
-	"time"
 )
 
 func getBucketNameSuffix() string {
@@ -201,133 +198,133 @@ var _ = AfterSuite(func() {
 	defer dash.TestSetEnd()
 	defer EndTorpedoTest()
 
-	ctx, err := backup.GetAdminCtxFromSecret()
-	log.FailOnError(err, "Fetching px-central-admin ctx")
-
-	//Cleanup policy
-	s3EncryptionPolicy := os.Getenv("S3_ENCRYPTION_POLICY")
-	if s3EncryptionPolicy != "" {
-		err = RemoveS3BucketPolicy(globalAWSBucketName)
-		dash.VerifySafely(err, nil, fmt.Sprintf("Verify removal of S3 bucket policy"))
-	}
-
-	// Cleanup all backups
-	allBackups, err := GetAllBackupsAdmin()
-	for _, backupName := range allBackups {
-		backupUID, err := Inst().Backup.GetBackupUID(ctx, backupName, orgID)
-		dash.VerifySafely(err, nil, fmt.Sprintf("Getting backuip UID for backup %s", backupName))
-		_, err = DeleteBackup(backupName, backupUID, orgID, ctx)
-		dash.VerifySafely(err, nil, fmt.Sprintf("Verifying backup deletion - %s", backupName))
-	}
-
-	// Cleanup all restores
-	allRestores, err := GetAllRestoresAdmin()
-	for _, restoreName := range allRestores {
-		err = DeleteRestore(restoreName, orgID, ctx)
-		dash.VerifySafely(err, nil, fmt.Sprintf("Verifying restore deletion - %s", restoreName))
-	}
-
-	// Deleting clusters and the corresponding cloud cred
-	var clusterCredName string
-	var clusterCredUID string
-	kubeconfigs := os.Getenv("KUBECONFIGS")
-	kubeconfigList := strings.Split(kubeconfigs, ",")
-	for _, kubeconfig := range kubeconfigList {
-		clusterName := strings.Split(kubeconfig, "-")[0] + "-cluster"
-		isPresent, err := IsClusterPresent(clusterName, ctx, orgID)
-		if err != nil {
-			Inst().Dash.VerifySafely(err, nil, fmt.Sprintf("Verifying if cluster [%s] is present", clusterName))
-		}
-		if isPresent {
-			clusterReq := &api.ClusterInspectRequest{OrgId: orgID, Name: clusterName}
-			clusterResp, err := Inst().Backup.InspectCluster(ctx, clusterReq)
-			if err != nil {
-				if strings.Contains(err.Error(), "object not found") {
-					log.InfoD("Cluster %s is not present", clusterName)
-				} else {
-					Inst().Dash.VerifySafely(err, nil, fmt.Sprintf("Inspecting cluster [%s]", clusterName))
-				}
-			} else {
-				clusterObj := clusterResp.GetCluster()
-				clusterProvider := GetClusterProviders()
-				for _, provider := range clusterProvider {
-					switch provider {
-					case drivers.ProviderRke:
-						if clusterObj.PlatformCredentialRef != nil {
-							clusterCredName = clusterObj.PlatformCredentialRef.Name
-							clusterCredUID = clusterObj.PlatformCredentialRef.Uid
-						} else {
-							log.Warnf("the platform credential ref of the cluster [%s] is nil", clusterName)
-						}
-					default:
-						if clusterObj.CloudCredentialRef != nil {
-							clusterCredName = clusterObj.CloudCredentialRef.Name
-							clusterCredUID = clusterObj.CloudCredentialRef.Uid
-						} else {
-							log.Warnf("the cloud credential ref of the cluster [%s] is nil", clusterName)
-						}
-					}
-					err = DeleteCluster(clusterName, orgID, ctx, true)
-					Inst().Dash.VerifySafely(err, nil, fmt.Sprintf("Deleting cluster %s", clusterName))
-					clusterDeleteStatus := func() (interface{}, bool, error) {
-						status, err := IsClusterPresent(clusterName, ctx, orgID)
-						if err != nil {
-							return "", true, fmt.Errorf("cluster %s still present with error %v", clusterName, err)
-						}
-						if status {
-							return "", true, fmt.Errorf("cluster %s is not deleted yet", clusterName)
-						}
-						return "", false, nil
-					}
-					_, err = task.DoRetryWithTimeout(clusterDeleteStatus, clusterDeleteTimeout, clusterDeleteRetryTime)
-					Inst().Dash.VerifySafely(err, nil, fmt.Sprintf("Deleting cluster %s", clusterName))
-					if clusterCredName != "" {
-						err = DeleteCloudCredential(clusterCredName, orgID, clusterCredUID)
-						Inst().Dash.VerifySafely(err, nil, fmt.Sprintf("Verifying deletion of cluster cloud cred [%s]", clusterCredName))
-					}
-				}
-			}
-		}
-	}
-	// Cleanup all backup locations
-	allBackupLocations, err := getAllBackupLocations(ctx)
-	dash.VerifySafely(err, nil, "Verifying fetching of all backup locations")
-	for backupLocationUid, backupLocationName := range allBackupLocations {
-		err = DeleteBackupLocation(backupLocationName, backupLocationUid, orgID, true)
-		dash.VerifySafely(err, nil, fmt.Sprintf("Verifying backup location deletion - %s", backupLocationName))
-	}
-
-	backupLocationDeletionSuccess := func() (interface{}, bool, error) {
-		allBackupLocations, err := getAllBackupLocations(ctx)
-		dash.VerifySafely(err, nil, "Verifying fetching of all backup locations")
-		if len(allBackupLocations) > 0 {
-			return "", true, fmt.Errorf("found %d backup locations", len(allBackupLocations))
-		} else {
-			return "", false, nil
-		}
-	}
-	_, err = DoRetryWithTimeoutWithGinkgoRecover(backupLocationDeletionSuccess, 5*time.Minute, 30*time.Second)
-	dash.VerifySafely(err, nil, "Verifying backup location deletion success")
-
-	// Cleanup all cloud credentials
-	allCloudCredentials, err := getAllCloudCredentials(ctx)
-	dash.VerifySafely(err, nil, "Verifying fetching of all cloud credentials")
-	for cloudCredentialUid, cloudCredentialName := range allCloudCredentials {
-		err = DeleteCloudCredential(cloudCredentialName, orgID, cloudCredentialUid)
-		dash.VerifySafely(err, nil, fmt.Sprintf("Deleting cloud cred %s", cloudCredentialName))
-	}
-
-	cloudCredentialDeletionSuccess := func() (interface{}, bool, error) {
-		allCloudCredentials, err := getAllCloudCredentials(ctx)
-		dash.VerifySafely(err, nil, "Verifying fetching of all cloud credentials")
-		if len(allCloudCredentials) > 0 {
-			return "", true, fmt.Errorf("found %d cloud credentials", len(allBackupLocations))
-		} else {
-			return "", false, nil
-		}
-	}
-	_, err = DoRetryWithTimeoutWithGinkgoRecover(cloudCredentialDeletionSuccess, 5*time.Minute, 30*time.Second)
-	dash.VerifySafely(err, nil, "Verifying cloud credential deletion success")
+	//ctx, err := backup.GetAdminCtxFromSecret()
+	//log.FailOnError(err, "Fetching px-central-admin ctx")
+	//
+	////Cleanup policy
+	//s3EncryptionPolicy := os.Getenv("S3_ENCRYPTION_POLICY")
+	//if s3EncryptionPolicy != "" {
+	//	err = RemoveS3BucketPolicy(globalAWSBucketName)
+	//	dash.VerifySafely(err, nil, fmt.Sprintf("Verify removal of S3 bucket policy"))
+	//}
+	//
+	//// Cleanup all backups
+	//allBackups, err := GetAllBackupsAdmin()
+	//for _, backupName := range allBackups {
+	//	backupUID, err := Inst().Backup.GetBackupUID(ctx, backupName, orgID)
+	//	dash.VerifySafely(err, nil, fmt.Sprintf("Getting backuip UID for backup %s", backupName))
+	//	_, err = DeleteBackup(backupName, backupUID, orgID, ctx)
+	//	dash.VerifySafely(err, nil, fmt.Sprintf("Verifying backup deletion - %s", backupName))
+	//}
+	//
+	//// Cleanup all restores
+	//allRestores, err := GetAllRestoresAdmin()
+	//for _, restoreName := range allRestores {
+	//	err = DeleteRestore(restoreName, orgID, ctx)
+	//	dash.VerifySafely(err, nil, fmt.Sprintf("Verifying restore deletion - %s", restoreName))
+	//}
+	//
+	//// Deleting clusters and the corresponding cloud cred
+	//var clusterCredName string
+	//var clusterCredUID string
+	//kubeconfigs := os.Getenv("KUBECONFIGS")
+	//kubeconfigList := strings.Split(kubeconfigs, ",")
+	//for _, kubeconfig := range kubeconfigList {
+	//	clusterName := strings.Split(kubeconfig, "-")[0] + "-cluster"
+	//	isPresent, err := IsClusterPresent(clusterName, ctx, orgID)
+	//	if err != nil {
+	//		Inst().Dash.VerifySafely(err, nil, fmt.Sprintf("Verifying if cluster [%s] is present", clusterName))
+	//	}
+	//	if isPresent {
+	//		clusterReq := &api.ClusterInspectRequest{OrgId: orgID, Name: clusterName}
+	//		clusterResp, err := Inst().Backup.InspectCluster(ctx, clusterReq)
+	//		if err != nil {
+	//			if strings.Contains(err.Error(), "object not found") {
+	//				log.InfoD("Cluster %s is not present", clusterName)
+	//			} else {
+	//				Inst().Dash.VerifySafely(err, nil, fmt.Sprintf("Inspecting cluster [%s]", clusterName))
+	//			}
+	//		} else {
+	//			clusterObj := clusterResp.GetCluster()
+	//			clusterProvider := GetClusterProviders()
+	//			for _, provider := range clusterProvider {
+	//				switch provider {
+	//				case drivers.ProviderRke:
+	//					if clusterObj.PlatformCredentialRef != nil {
+	//						clusterCredName = clusterObj.PlatformCredentialRef.Name
+	//						clusterCredUID = clusterObj.PlatformCredentialRef.Uid
+	//					} else {
+	//						log.Warnf("the platform credential ref of the cluster [%s] is nil", clusterName)
+	//					}
+	//				default:
+	//					if clusterObj.CloudCredentialRef != nil {
+	//						clusterCredName = clusterObj.CloudCredentialRef.Name
+	//						clusterCredUID = clusterObj.CloudCredentialRef.Uid
+	//					} else {
+	//						log.Warnf("the cloud credential ref of the cluster [%s] is nil", clusterName)
+	//					}
+	//				}
+	//				err = DeleteCluster(clusterName, orgID, ctx, true)
+	//				Inst().Dash.VerifySafely(err, nil, fmt.Sprintf("Deleting cluster %s", clusterName))
+	//				clusterDeleteStatus := func() (interface{}, bool, error) {
+	//					status, err := IsClusterPresent(clusterName, ctx, orgID)
+	//					if err != nil {
+	//						return "", true, fmt.Errorf("cluster %s still present with error %v", clusterName, err)
+	//					}
+	//					if status {
+	//						return "", true, fmt.Errorf("cluster %s is not deleted yet", clusterName)
+	//					}
+	//					return "", false, nil
+	//				}
+	//				_, err = task.DoRetryWithTimeout(clusterDeleteStatus, clusterDeleteTimeout, clusterDeleteRetryTime)
+	//				Inst().Dash.VerifySafely(err, nil, fmt.Sprintf("Deleting cluster %s", clusterName))
+	//				if clusterCredName != "" {
+	//					err = DeleteCloudCredential(clusterCredName, orgID, clusterCredUID)
+	//					Inst().Dash.VerifySafely(err, nil, fmt.Sprintf("Verifying deletion of cluster cloud cred [%s]", clusterCredName))
+	//				}
+	//			}
+	//		}
+	//	}
+	//}
+	//// Cleanup all backup locations
+	//allBackupLocations, err := getAllBackupLocations(ctx)
+	//dash.VerifySafely(err, nil, "Verifying fetching of all backup locations")
+	//for backupLocationUid, backupLocationName := range allBackupLocations {
+	//	err = DeleteBackupLocation(backupLocationName, backupLocationUid, orgID, true)
+	//	dash.VerifySafely(err, nil, fmt.Sprintf("Verifying backup location deletion - %s", backupLocationName))
+	//}
+	//
+	//backupLocationDeletionSuccess := func() (interface{}, bool, error) {
+	//	allBackupLocations, err := getAllBackupLocations(ctx)
+	//	dash.VerifySafely(err, nil, "Verifying fetching of all backup locations")
+	//	if len(allBackupLocations) > 0 {
+	//		return "", true, fmt.Errorf("found %d backup locations", len(allBackupLocations))
+	//	} else {
+	//		return "", false, nil
+	//	}
+	//}
+	//_, err = DoRetryWithTimeoutWithGinkgoRecover(backupLocationDeletionSuccess, 5*time.Minute, 30*time.Second)
+	//dash.VerifySafely(err, nil, "Verifying backup location deletion success")
+	//
+	//// Cleanup all cloud credentials
+	//allCloudCredentials, err := getAllCloudCredentials(ctx)
+	//dash.VerifySafely(err, nil, "Verifying fetching of all cloud credentials")
+	//for cloudCredentialUid, cloudCredentialName := range allCloudCredentials {
+	//	err = DeleteCloudCredential(cloudCredentialName, orgID, cloudCredentialUid)
+	//	dash.VerifySafely(err, nil, fmt.Sprintf("Deleting cloud cred %s", cloudCredentialName))
+	//}
+	//
+	//cloudCredentialDeletionSuccess := func() (interface{}, bool, error) {
+	//	allCloudCredentials, err := getAllCloudCredentials(ctx)
+	//	dash.VerifySafely(err, nil, "Verifying fetching of all cloud credentials")
+	//	if len(allCloudCredentials) > 0 {
+	//		return "", true, fmt.Errorf("found %d cloud credentials", len(allBackupLocations))
+	//	} else {
+	//		return "", false, nil
+	//	}
+	//}
+	//_, err = DoRetryWithTimeoutWithGinkgoRecover(cloudCredentialDeletionSuccess, 5*time.Minute, 30*time.Second)
+	//dash.VerifySafely(err, nil, "Verifying cloud credential deletion success")
 
 	// Cleanup all buckets after suite
 	providers := getProviders()
