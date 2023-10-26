3a5b682341bb9c04b2e035dfb2994ed4bac4c197
Jyoti K <126552572+jkagliwal-px@users.noreply.github.com>
2023-10-26 11:23:50 +0530

    Update pds_dataservice_test.go

--- tests/pds/pds_dataservice_test.go
+++ tests/pds/pds_dataservice_test.go
@@ -3,7 +3,6 @@ package tests
 import (
 	"errors"
 	"fmt"
-	pdsdriver "github.com/portworx/torpedo/drivers/pds"
 	"math/rand"
 	"net/http"
 	"os"
@@ -1899,16 +1898,6 @@ var _ = Describe("{GetPvcToFullCondition}", func() {
 
 	JustBeforeEach(func() {
 		StartTorpedoTest("GetPvcToFullCondition", "Deploys and increases the pvc size of DS once trhreshold is met", pdsLabels, 0)
-		wkloadParams = pdsdriver.LoadGenParams{
-			LoadGenDepName: params.LoadGen.LoadGenDepName,
-			Namespace:      params.InfraToTest.Namespace,
-			NumOfRows:      params.LoadGen.NumOfRows,
-			Timeout:        params.LoadGen.Timeout,
-			Replicas:       params.LoadGen.Replicas,
-			TableName:      params.LoadGen.TableName,
-			Iterations:     params.LoadGen.Iterations,
-			FailOnError:    params.LoadGen.FailOnError,
-		}
 	})
 
 	It("Deploy Dataservices", func() {
@@ -1949,9 +1938,10 @@ var _ = Describe("{GetPvcToFullCondition}", func() {
 					for ds, deployment := range deployments {
 						if Contains(dataServicePodWorkloads, ds.Name) || Contains(dataServiceDeploymentWorkloads, ds.Name) {
 							log.InfoD("Running Workloads on DataService %v ", ds.Name)
-							_, wlDep, err := dsTest.InsertDataAndReturnChecksum(deployment, wkloadParams)
+							var params pdslib.WorkloadGenerationParams
+							pod, dep, err = RunWorkloads(params, ds, deployment, namespace)
 							log.FailOnError(err, "Error while genearating workloads")
-							generateWorkloads[ds.Name] = wlDep.Name
+							generateWorkloads[ds.Name] = dep.Name
 							for dsName, workloadContainer := range generateWorkloads {
 								log.Debugf("dsName %s, workloadContainer %s", dsName, workloadContainer)
 							}
@@ -1975,7 +1965,6 @@ var _ = Describe("{GetPvcToFullCondition}", func() {
 				}()
 
 				Step("Checking the PVC usage", func() {
-					log.FailOnError(err, "Unable to create scheduler context")
 					err = CheckStorageFullCondition(namespace, deployment)
 					log.FailOnError(err, "Failing while filling the PVC to 90 percentage of its capacity due to ...")
 					_, err = IncreasePVCby1Gig(namespace, deployment, 1)
@@ -2032,7 +2021,6 @@ var _ = Describe("{ResizePVCBy1GB}", func() {
 			}()
 
 			Step("Resizing the PVC size", func() {
-				log.FailOnError(err, "Unable to create scheduler context")
 				_, err = IncreasePVCby1Gig(namespace, deployment, 1)
 				log.FailOnError(err, "Failing while Increasing the PVC name...")
 			})
