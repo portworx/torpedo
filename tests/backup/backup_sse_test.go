ee1237cc15980b41738b8cac5e81c247f1a14362
sn-px@purestorage.com <sn@purestorage.com>
2023-10-26 17:03:14 +0530

    mend

--- tests/backup/backup_sse_test.go
+++ tests/backup/backup_sse_test.go
@@ -242,6 +242,7 @@ var _ = Describe("{sseS3encryption}", func() {
 
 			for _, provider := range providers {
 				err = UpdateBackupLocation(provider, customBackupLocationName1, backupLocationUID1, OrgID, ctx, api.S3Config_SSE_S3)
+				dash.VerifyFatal(err, nil, fmt.Sprintf("Updation of backuplocation [%s]", customBackupLocationName1))
 			}
 		})
 	})
