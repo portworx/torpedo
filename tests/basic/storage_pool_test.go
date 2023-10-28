ab9819b6e311529f04eb0ab0fbefc2bb4c6a3a3c
spai-px <spai@purestorage.com>
2023-10-28 19:02:00 +0530

    add condition when pool expand fails without force

--- tests/basic/storage_pool_test.go
+++ tests/basic/storage_pool_test.go
@@ -2255,6 +2255,11 @@ var _ = Describe("{VolUpdateResizeDisk}", func() {
 
 			log.InfoD("Current Size of the pool %s is %d", selectedPool.Uuid, poolToBeResized.TotalSize/units.GiB)
 			err = Inst().V.ExpandPool(selectedPool.Uuid, api.SdkStoragePool_RESIZE_TYPE_RESIZE_DISK, expectedSize, false)
+			if err != nil {
+				if strings.Contains(fmt.Sprintf("%v", err), "Please re-issue expand with force") {
+					err = Inst().V.ExpandPool(poolToBeResized.Uuid, api.SdkStoragePool_RESIZE_TYPE_RESIZE_DISK, expectedSize, true)
+				}
+			}
 			dash.VerifyFatal(err, nil, "Pool expansion init successful?")
 
 			resizeErr := waitForPoolToBeResized(expectedSize, selectedPool.Uuid, isjournal)
