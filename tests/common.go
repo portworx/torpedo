1d4dc292ad5c3bd3aaaccf09011878214612ce3c
sn-px@purestorage.com <sn@purestorage.com>
2023-11-02 17:43:04 +0530

    SSE S3 TC Automation

--- tests/common.go
+++ tests/common.go
@@ -4315,16 +4315,29 @@ func CreateS3BackupLocation(name, uid, cloudCred, cloudCredUID, bucketName, orgI
 
 	backupDriver := Inst().Backup
 	_, _, endpoint, region, disableSSLBool := s3utils.GetAWSDetailsFromEnv()
+<<<<<<< HEAD
+=======
+
+>>>>>>> 5fe9e6f32 (SSE S3 TC Automation)
 	// Initialize a new variable to hold the SSE S3 Encryption Type
 	var sseType api.S3Config_Sse
 	if len(sseS3EncryptionType) == 0 {
 		// If sseS3EncryptionType Type parameter is not passed , then take it from environment variable
 		sseType, err = GetSseS3EncryptionType()
+<<<<<<< HEAD
 		if err != nil {
 			return err
 		}
 	} else {
 		sseType = sseS3EncryptionType[0]
+=======
+	} else {
+		sseType = sseS3EncryptionType[0]
+	}
+
+	if err != nil {
+		return err
+>>>>>>> 5fe9e6f32 (SSE S3 TC Automation)
 	}
 	bLocationCreateReq := &api.BackupLocationCreateRequest{
 		CreateMetadata: &api.CreateMetadata{
@@ -4402,6 +4415,7 @@ func CreateS3BackupLocationWithContext(name, uid, cloudCred, cloudCredUID, bucke
 }
 
 // CreateS3BackupLocationWithSseType creates backup location for S3 with SSE type
+<<<<<<< HEAD
 //
 //	func CreateS3BackupLocationWithSseType(name string, uid, cloudCred string, cloudCredUID string, bucketName string, orgID string, encryptionKey string, sseS3EncryptionType api.S3Config_Sse) error {
 //		time.Sleep(60 * time.Second)
@@ -4483,6 +4497,48 @@ func CreateS3BackupLocationWithSseType(name, uid, cloudCred, cloudCredUID, bucke
 	}
 	return nil
 }
+=======
+//func CreateS3BackupLocationWithSseType(name, uid, cloudCred, cloudCredUID, bucketName, orgID, encryptionKey string, sseS3EncryptionType api.S3Config_Sse, validate bool) error {
+//	backupDriver := Inst().Backup
+//	_, _, endpoint, region, disableSSLBool := s3utils.GetAWSDetailsFromEnv()
+//	bLocationCreateReq := &api.BackupLocationCreateRequest{
+//		CreateMetadata: &api.CreateMetadata{
+//			Name:  name,
+//			OrgId: orgID,
+//			Uid:   uid,
+//		},
+//		BackupLocation: &api.BackupLocationInfo{
+//			Path:                    bucketName,
+//			EncryptionKey:           encryptionKey,
+//			ValidateCloudCredential: validate,
+//			CloudCredentialRef: &api.ObjectRef{
+//				Name: cloudCred,
+//				Uid:  cloudCredUID,
+//			},
+//			Type: api.BackupLocationInfo_S3,
+//			Config: &api.BackupLocationInfo_S3Config{
+//				S3Config: &api.S3Config{
+//					Endpoint:   endpoint,
+//					Region:     region,
+//					DisableSsl: disableSSLBool,
+//					SseType:    sseS3EncryptionType,
+//				},
+//			},
+//		},
+//	}
+//
+//	ctx, err := backup.GetAdminCtxFromSecret()
+//	if err != nil {
+//		return err
+//	}
+//
+//	_, err = backupDriver.CreateBackupLocation(ctx, bLocationCreateReq)
+//	if err != nil {
+//		return fmt.Errorf("failed to create backup location: %v", err)
+//	}
+//	return nil
+//}
+>>>>>>> 5fe9e6f32 (SSE S3 TC Automation)
 
 // UpdateS3BackupLocation with the provided values
 func UpdateS3BackupLocation(name string, uid string, orgID string, cloudCred string, cloudCredUID string, ctx context1.Context, sseS3EncryptionType api.S3Config_Sse, validate bool) error {
