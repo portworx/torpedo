2fc56b678c4ad6f238b845ce1e7d4188396abba3
sn-px@purestorage.com <sn@purestorage.com>
2023-11-04 08:17:33 +0530

    mend
    SSE S3 fix the validation in restore

--- tests/backup/backup_sse_test.go
+++ tests/backup/backup_sse_test.go
@@ -291,8 +291,6 @@ var _ = Describe("{CreateBackupAndRestoreForAllCombinationsOfSSES3AndDenyPolicy}
 				log.InfoD("Restoring the backed up applications after setting the sse to true of backupLocationWithoutSse")
 				ctx, err := backup.GetAdminCtxFromSecret()
 				log.FailOnError(err, "Fetching px-central-admin ctx")
-				var appContextsForValidation []*scheduler.Context
-				appContextsForValidation = append(appContextsForValidation, scheduledAppContexts[:midpoint][0])
 				restoreName := fmt.Sprintf("%s-%s-%v", "restore-after-bl-sse-true", RandomString(randomStringLength), time.Now().Unix())
 				err = CreateRestoreWithValidation(ctx, restoreName, backupsAfterSettingSseTrue[0], make(map[string]string), make(map[string]string), destinationClusterName, orgID, namespaceBackupMap[backupsAfterSettingSseTrue[0]])
 				dash.VerifyFatal(err, nil, fmt.Sprintf("Creating restore [%s]", restoreName))
