96697d356af3c16749fe65fc9b1890c9fd1a2bc4
sn-px@purestorage.com <sn@purestorage.com>
2023-11-04 07:21:21 +0530

    mend
    SSE S3 fix

--- tests/backup/backup_sse_test.go
+++ tests/backup/backup_sse_test.go
@@ -320,7 +320,7 @@ var _ = Describe("{CreateBackupAndRestoreForAllCombinationsOfSSES3AndDenyPolicy}
 				log.InfoD("Create a schedule policy")
 				ctx, err := backup.GetAdminCtxFromSecret()
 				log.FailOnError(err, "Fetching px-central-admin ctx")
-				schedulePolicyintervalInMins := 15
+				schedulePolicyintervalInMins := 30
 				log.InfoD("Creating a schedule policy with interval [%v] mins", schedulePolicyintervalInMins)
 				schedulePolicyName = fmt.Sprintf("interval-%v-%v", schedulePolicyintervalInMins, time.Now().Unix())
 				schedulePolicyInfo := Inst().Backup.CreateIntervalSchedulePolicy(5, int64(schedulePolicyintervalInMins), 5)
