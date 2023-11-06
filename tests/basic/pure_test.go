aa3816dbb1ee53889b14e6d76a5a23da2872d683
spai-px <spai@purestorage.com>
2023-11-06 19:10:19 +0530

    added wait for all nodes to come up

--- tests/basic/pure_test.go
+++ tests/basic/pure_test.go
@@ -1039,7 +1039,7 @@ var _ = Describe("{AppCleanUpWhenPxKill}", func() {
 		var selectedNodes []node.Node
 		selectedNodes = append(selectedNodes, storageNodes[rand.Intn(len(storageNodes))])
 		selectedNodes = append(selectedNodes, storageLessNodes[rand.Intn(len(storageLessNodes))])
-		selectedKvdbNode, err := node.GetNodeDetailsByNodeID(kvdbNodes[rand.Intn(len(storageLessNodes))].ID)
+		selectedKvdbNode, err := node.GetNodeDetailsByNodeID(kvdbNodes[rand.Intn(len(kvdbNodes))].ID)
 		log.FailOnError(err, "Failed to get kvdb node details")
 		for _, kvdbNode := range kvdbNodes {
 			if kvdbNode.ID != selectedNodes[0].Id && kvdbNode.ID != selectedNodes[1].Id {
