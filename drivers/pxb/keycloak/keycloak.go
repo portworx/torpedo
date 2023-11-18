11507fd70ad90041ad42a28ee1e24fdb9c7d7abe
kphalgun-px <kphalgun@purestorage.com>
2023-11-19 02:43:16 +0530

    [PTX-20643] Add ResizePVCWhilePxRestart

--- drivers/pxb/keycloak/keycloak.go
+++ drivers/pxb/keycloak/keycloak.go
@@ -9,18 +9,25 @@ import (
 )
 
 const (
-	// EnvPxCentralUIURL is the environment variable key for the px-central UI URL.
-	// Example: http://pxcentral-keycloak-http:80
-	EnvPxCentralUIURL = "PX_CENTRAL_UI_URL"
-)
-
-const (
-	// PxBackupOIDCSecret is the Kubernetes secret that contains OIDC (OpenID Connect) credentials
+	// PxBackupOIDCSecret is the Kubernetes secret storing OIDC (OpenID Connect) credentials
 	PxBackupOIDCSecret = "pxc-backup-secret"
 	// PxBackupOIDCEndpointKey is the key in PxBackupOIDCSecret for the OIDC endpoint
 	PxBackupOIDCEndpointKey = "OIDC_ENDPOINT"
 	// PxBackupKeycloakService is the Kubernetes service for Keycloak-based user authentication
 	PxBackupKeycloakService = "pxcentral-keycloak-http"
+)
+
+const (
+	// EnvPxCentralUIURL is the environment variable key for the Px-Central UI URL
+	// Example: http://pxcentral-keycloak-http:80
+	EnvPxCentralUIURL = "PX_CENTRAL_UI_URL"
+)
+
+var (
+	AdminURL    string
+	NonAdminURL string
+	// PxCentralAdminPassword is the password for px-central-admin user
+	PxCentralAdminPassword string
 )
 
 type SignIn struct {
@@ -35,17 +42,17 @@ func NewSignIn(username string, password string) *SignIn {
 	}
 }
 
+// GetAdminAndNonAdminURL retrieves the Keycloak admin and non-admin access URLs from the given Px-Backup namespace
 func GetAdminAndNonAdminURL(namespace string) (string, string, error) {
 	realmPath, adminPath, realmName := "auth/realms", "admin", "master"
-	adminURL, nonAdminURL := "", ""
+	baseURL := ""
 	pxCentralUIURL := os.Getenv(EnvPxCentralUIURL)
 	// The condition checks whether pxCentralUIURL is set. This condition is added to
 	// handle scenarios where Torpedo is not running as a pod in the cluster. In such
 	// cases, gRPC calls pxcentral-keycloak-http:80 would fail when made from a VM or
 	// local machine using the Ginkgo CLI.
 	if pxCentralUIURL != "" && len(pxCentralUIURL) > 0 {
-		adminURL = fmt.Sprintf("%s/%s/%s/%s", pxCentralUIURL, realmPath, adminPath, realmName)
-		nonAdminURL = fmt.Sprintf("%s/%s/%s", pxCentralUIURL, realmPath, realmName)
+		baseURL = pxCentralUIURL
 	} else {
 		oidcSecret, err := core.Instance().GetSecret(PxBackupOIDCSecret, namespace)
 		if err != nil {
@@ -56,10 +63,10 @@ func GetAdminAndNonAdminURL(namespace string) (string, string, error) {
 		// ensure DNS resolution within Kubernetes, especially for requests originating
 		// from different namespace
 		keycloakFQDN := fmt.Sprintf("%s.%s.svc.cluster.local", PxBackupKeycloakService, namespace)
-		newOIDCEndpoint := strings.Replace(oidcEndpoint, PxBackupKeycloakService, keycloakFQDN, 1)
-		adminURL = fmt.Sprintf("%s/%s/%s/%s", newOIDCEndpoint, realmPath, adminPath, realmName)
-		nonAdminURL = fmt.Sprintf("%s/%s/%s", newOIDCEndpoint, realmPath, realmName)
+		baseURL = strings.Replace(oidcEndpoint, PxBackupKeycloakService, keycloakFQDN, 1)
 	}
+	adminURL := fmt.Sprintf("%s/%s/%s/%s", baseURL, realmPath, adminPath, realmName)
+	nonAdminURL := fmt.Sprintf("%s/%s/%s", baseURL, realmPath, realmName)
 	return adminURL, nonAdminURL, nil
 }
 
