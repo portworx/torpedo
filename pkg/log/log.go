6238f732a6b1f12d2cca8265fca4c91123ad593e
kphalgun-px <kphalgun@purestorage.com>
2023-11-27 21:05:52 +0530

    [PTX-21248] Refactor CurrentGinkgoTestDescription to CurrentSpecReport

--- pkg/log/log.go
+++ pkg/log/log.go
@@ -213,11 +213,11 @@ func (mf *MyFormatter) Format(entry *logrus.Entry) ([]byte, error) {
 		funcName = funcName[subIndex+1:]
 	}
 
-	// TODO: Once we migrate to ginkgo v2, we can extract test name as follows:
-	// report := CurrentSpecReport()
-	// testName := report.ContainerHierarchyTexts[0]
-	report := CurrentGinkgoTestDescription()
-	testName := strings.Split(report.FullTestText, " ")[0]
+	report := CurrentSpecReport()
+	testName := ""
+	if len(report.ContainerHierarchyTexts) > 0 {
+		testName = report.ContainerHierarchyTexts[0]
+	}
 	if testName != "" {
 		writeString = fmt.Sprintf("%s:[%s] [%s] %s\n",
 			entry.Time.Format("2006-01-02 15:04:05 -0700"), level, testName,
--- tests/basic/volumeplacementstrategy_test.go
+++ tests/basic/volumeplacementstrategy_test.go
@@ -108,7 +108,7 @@ var _ = Describe("{VolumePlacementStrategyFunctional}", func() {
 	AfterEach(func() {
 		Step("destroy apps", func() {
 			log.InfoD("destroying apps")
-			if CurrentGinkgoTestDescription().Failed {
+			if CurrentSpecReport().Failed() {
 				log.InfoD("not destroying apps because the test failed\n")
 				return
 			}
