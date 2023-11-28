1a91ace94799b0478d49cb2f97f6f6f68354d755
kphalgun-px <kphalgun@purestorage.com>
2023-11-28 16:31:28 +0530

    [PTX-21248] Refactor --slowSpecThreshold to --slow-spec-threshold

--- deployments/deploy-ssh.sh
+++ deployments/deploy-ssh.sh
@@ -495,7 +495,7 @@ spec:
     args: [ "--trace",
             "--timeout", "${TIMEOUT}",
             "$FAIL_FAST",
-            "--slowSpecThreshold", "600",
+            "--slow-spec-threshold", "10m",
             "$FOCUS_ARG",
             "$SKIP_ARG",
             $TEST_SUITE,
