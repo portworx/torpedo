f67bdc81c4aa0f2fa44dda6e7c8ecfafbb29bb9b
kphalgun-px <kphalgun@purestorage.com>
2023-11-29 17:04:32 +0530

    [PTX-21248] Refactor Context to By in leaf nodes for spec compliance

--- tests/testTriggers.go
+++ tests/testTriggers.go
@@ -529,7 +529,7 @@ func TriggerCoreChecker(contexts *[]*scheduler.Context, recordChan *chan *EventR
 	coresMap = make(map[string]string)
 	setMetrics(*event)
 
-	context("checking for core files...", func() {
+	Step("checking for core files...", func() {
 		Step("verifying if core files are present on each node", func() {
 			log.InfoD("verifying if core files are present on each node")
 			nodes := node.GetStorageDriverNodes()
@@ -4799,7 +4799,7 @@ func TriggerUpgradeVolumeDriver(contexts *[]*scheduler.Context, recordChan *chan
 	setMetrics(*event)
 
 	stepLog := "upgrade volume driver"
-	context(stepLog, func() {
+	Step(stepLog, func() {
 		if len(Inst().UpgradeStorageDriverEndpointList) == 0 {
 			log.Fatalf("Unable to perform volume driver upgrade hops, none were given")
 		}
@@ -4880,7 +4880,7 @@ func TriggerAutoFsTrim(contexts *[]*scheduler.Context, recordChan *chan *EventRe
 	setMetrics(*event)
 
 	stepLog := "Validate AutoFsTrim of the volumes"
-	context(stepLog, func() {
+	Step(stepLog, func() {
 		log.InfoD(stepLog)
 		stepLog = "enable auto fstrim "
 		Step(stepLog,
@@ -4986,7 +4986,7 @@ func TriggerVolumeUpdate(contexts *[]*scheduler.Context, recordChan *chan *Event
 
 	setMetrics(*event)
 	stepLog := "Validate update of the volumes"
-	context(stepLog, func() {
+	Step(stepLog, func() {
 		log.InfoD(stepLog)
 		stepLog = "Update Io priority on volumes "
 		Step(stepLog,
@@ -5018,7 +5018,7 @@ func TriggerVolumeIOProfileUpdate(contexts *[]*scheduler.Context, recordChan *ch
 	}()
 	setMetrics(*event)
 	stepLog := "Validate IO profile update on volumes"
-	context(stepLog, func() {
+	Step(stepLog, func() {
 		log.InfoD(stepLog)
 		Step(stepLog,
 			func() {
@@ -5353,7 +5353,7 @@ func TriggerTrashcan(contexts *[]*scheduler.Context, recordChan *chan *EventReco
 
 	stepLog := "Validate Trashcan feature of the volumes"
 
-	context(stepLog, func() {
+	Step(stepLog, func() {
 		log.InfoD(stepLog)
 		if !isTrashcanEnabled {
 			stepLog = "enable trashcan"
@@ -5445,7 +5445,7 @@ func TriggerRelaxedReclaim(contexts *[]*scheduler.Context, recordChan *chan *Eve
 	setMetrics(*event)
 
 	stepLog := "Validate Relaxed Reclaim of the volumes"
-	context(stepLog, func() {
+	Step(stepLog, func() {
 		log.InfoD(stepLog)
 		if !isRelaxedReclaimEnabled {
 			stepLog = "enable relaxed reclaim "
@@ -5995,7 +5995,7 @@ func TriggerKVDBFailover(contexts *[]*scheduler.Context, recordChan *chan *Event
 
 	setMetrics(*event)
 	stepLog := "perform kvdb failover in a cyclic manner"
-	context(stepLog, func() {
+	Step(stepLog, func() {
 		log.InfoD(stepLog)
 		stepLog = "Get KVDB nodes and perform failover"
 		Step(stepLog, func() {
@@ -6178,7 +6178,7 @@ func TriggerAppTasksDown(contexts *[]*scheduler.Context, recordChan *chan *Event
 
 	chaosLevel := ChaosMap[AppTasksDown]
 	stepLog := "deletes all pods from a given app and validate if they recover"
-	context(stepLog, func() {
+	Step(stepLog, func() {
 		log.InfoD(stepLog)
 		for _, ctx := range *contexts {
 			for i := 0; i < chaosLevel; i++ {
