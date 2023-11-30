1403e343799e5401039c6f5efd2da4884501e7e9
Jyoti K <126552572+jkagliwal-px@users.noreply.github.com>
2023-11-30 13:23:13 +0530

    Update pds_ds_rbac_test.go

--- tests/pds/pds_ds_rbac_test.go
+++ tests/pds/pds_ds_rbac_test.go
@@ -231,7 +231,7 @@ var _ = Describe("{ServiceIdentityNsLevel}", func() {
 						customParams.SetParamsForServiceIdentityTest(params, false)
 						_, _, config, err := pdslib.ValidateDataServiceVolumes(updatedDeployment, *resDep.Name, dataServiceDefaultResourceTemplateID, storageTemplateID, ns2.Name)
 						log.FailOnError(err, "error on ValidateDataServiceVolumes method")
-						dash.VerifyFatal(int32(ds.ScaleReplicas), config.Replicas, "Validating replicas after scaling up of dataservice")
+						dash.VerifyFatal(int32(ds.ScaleReplicas), int32(config.Replicas), "Validating replicas after scaling up of dataservice")
 					}
 				})
 				//ToDo : Add workload generation for restored-deps with RBAC roles on ns2
