898c0e965643caaeac958943fa2af2ed0586b0b3
atrivedi-px <atrivedi-px@purestorage.com>
2023-12-11 01:18:53 +0530

    Adding libraries for mysql and postgres data injection support

---
+++ drivers/applications/postgres/postgres.go
@@ -0,0 +1,167 @@
+package applications
+
+import (
+	"context"
+	"fmt"
+	"strings"
+	"time"
+
+	"github.com/jackc/pgx/v4"
+	. "github.com/portworx/torpedo/drivers/utilities"
+	"github.com/portworx/torpedo/pkg/log"
+)
+
+type PostgresConfig struct {
+	Hostname    string
+	User        string
+	Password    string
+	Port        int
+	DBName      string
+	SQLCommands map[string][]string
+}
+
+func (app *PostgresConfig) GetConnection(ctx context.Context) (*pgx.Conn, error) {
+
+	if app.Port == 0 {
+		app.Port = app.DefaultPort()
+	}
+
+	if app.DBName == "" {
+		app.DBName = app.DefaultDBName()
+	}
+
+	url := fmt.Sprintf("postgres://%s:%s@%s:%d/%s",
+		app.User, app.Password, app.Hostname, app.Port, app.DBName)
+	conn, err := pgx.Connect(ctx, url)
+	if err != nil {
+		return nil, fmt.Errorf("unable to connect to database: %s, Conn String - [%s]", err, url)
+	}
+
+	return conn, nil
+}
+
+func (app *PostgresConfig) DefaultPort() int { return 5432 }
+
+func (app *PostgresConfig) DefaultDBName() string { return "postgres" }
+
+func (app *PostgresConfig) ExecuteCommand(commads []string, ctx context.Context) error {
+
+	conn, err := app.GetConnection(ctx)
+	if err != nil {
+		return err
+	}
+	for _, eachCommand := range commads {
+		_, err = conn.Exec(ctx, eachCommand)
+		log.Infof("[%v]", err)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (app *PostgresConfig) InsertBackupData(ctx context.Context) error {
+
+	log.Infof("Inserting data")
+	err := app.ExecuteCommand(app.SQLCommands["insert"], ctx)
+
+	return err
+}
+
+func (app *PostgresConfig) CheckDataPresent(selectQueries []string, ctx context.Context) error {
+
+	log.Infof("Running Select Queries")
+
+	conn, err := app.GetConnection(ctx)
+	if err != nil {
+		return err
+	}
+
+	var key string
+	var value string
+	var queryNotFoundList []string
+
+	for _, eachQuery := range selectQueries {
+		currentRow := conn.QueryRow(ctx, eachQuery)
+		err := currentRow.Scan(&key, &value)
+
+		if err != nil {
+			log.Infof("Select query failed - [%s] Error - [%s]", eachQuery, err.Error())
+			queryNotFoundList = append(queryNotFoundList, eachQuery)
+		}
+	}
+
+	if len(queryNotFoundList) != 0 {
+		errorMessage := strings.Join(queryNotFoundList, "\n")
+		return fmt.Errorf("Below results not found in the table:\n %s", errorMessage)
+	}
+	return nil
+}
+
+func (app *PostgresConfig) UpdateBackupData(ctx context.Context) error {
+
+	log.Infof("Running Update Queries")
+	err := app.ExecuteCommand(app.SQLCommands["update"], ctx)
+
+	return err
+}
+
+func (app *PostgresConfig) DeleteBackupData(ctx context.Context) error {
+
+	log.Infof("Running Delete Queries")
+	err := app.ExecuteCommand(app.SQLCommands["delete"], ctx)
+
+	return err
+}
+
+func (app *PostgresConfig) StartData(command <-chan string, ctx context.Context) error {
+	var status = "Start"
+	var allSelectCommands []string
+	var tableName = "table_" + RandomString(4)
+
+	createTableQuery := fmt.Sprintf(`CREATE TABLE IF NOT EXISTS %s (
+		key varchar(45) NOT NULL,
+		value varchar(45) NOT NULL
+	  )`, tableName)
+	err := app.ExecuteCommand([]string{createTableQuery}, ctx)
+	if err != nil {
+		return err
+	}
+	for {
+		select {
+		case cmd := <-command:
+			switch cmd {
+			case "Stop":
+				log.Infof("All select command [%v] for [%s]", allSelectCommands, app.Hostname)
+				err := app.CheckDataPresent(allSelectCommands, ctx)
+				return err
+
+			case "Pause":
+				status = "Pause"
+			default:
+				status = "Start"
+			}
+		default:
+			if status == "Start" {
+				commandPair, err := app.startInsertingData(tableName, ctx)
+				if err != nil {
+					return err
+				}
+				allSelectCommands = append(allSelectCommands, commandPair["select"]...)
+				time.Sleep(2 * time.Second)
+			}
+		}
+	}
+}
+
+func (app *PostgresConfig) startInsertingData(tableName string, ctx context.Context) (map[string][]string, error) {
+
+	commandPair := GenerateSQLCommandPair(tableName)
+
+	err := app.ExecuteCommand(commandPair["insert"], ctx)
+	if err != nil {
+		return commandPair, err
+	}
+
+	return commandPair, nil
+}
---
+++ drivers/utilities/utilities.go
@@ -0,0 +1,144 @@
+package utilities
+
+import (
+	"fmt"
+	"math/rand"
+	"strconv"
+
+	"github.com/portworx/torpedo/drivers/scheduler"
+	corev1 "k8s.io/api/core/v1"
+)
+
+type AppInfo struct {
+	StartDataSupport bool
+	User             string
+	Password         string
+	Port             int
+	DBName           string
+	Hostname         string
+	AppType          string
+	Namespace        string
+}
+
+const (
+	svcAnnotationKey      = "startDataSupported"
+	userAnnotationKey     = "username"
+	passwordAnnotationKey = "password"
+	databaseAnnotationKey = "databaseName"
+	portAnnotationKey     = "port"
+	appTypeAnnotationKey  = "appType"
+)
+
+// RandomString generates a random lowercase string of length characters.
+func RandomString(length int) string {
+	const letters = "abcdefghijklmnopqrstuvwxyz"
+	randomBytes := make([]byte, length)
+	for i := range randomBytes {
+		randomBytes[i] = letters[rand.Intn(len(letters))]
+	}
+	randomString := string(randomBytes)
+	return randomString
+}
+
+func GenerateRandomSQLCommands(count int) map[string][]string {
+	var randomSqlCommands = make(map[string][]string)
+	var tableName string
+	var insertCommands []string
+	var selectCommands []string
+	var deleteCommands []string
+	var updateCommands []string
+
+	tableName = "pg_validation_" + RandomString(5)
+
+	insertCommands = append(insertCommands, fmt.Sprintf(`CREATE TABLE IF NOT EXISTS %s (
+		key varchar(45) NOT NULL,
+		value varchar(45) NOT NULL
+	  )`, tableName))
+
+	for counter := 0; counter < count; counter++ {
+		currentCounter := strconv.Itoa(counter)
+		randomValue := "Value-" + RandomString(10)
+		updatedRandomValue := "Value-Updated-" + RandomString(10)
+		insertCommands = append(insertCommands, fmt.Sprintf("INSERT INTO %s VALUES('%s', '%s')", tableName, currentCounter, randomValue))
+		selectCommands = append(selectCommands, fmt.Sprintf("SELECT * FROM %s WHERE key='%s'", tableName, currentCounter))
+		deleteCommands = append(deleteCommands, fmt.Sprintf("DELETE FROM %s WHERE key='%s'", tableName, currentCounter))
+		updateCommands = append(updateCommands, fmt.Sprintf("UPDATE %s SET value='%s' WHERE key='%s'", tableName, updatedRandomValue, currentCounter))
+	}
+
+	randomSqlCommands["insert"] = insertCommands
+	randomSqlCommands["select"] = selectCommands
+	randomSqlCommands["update"] = updateCommands
+	randomSqlCommands["delete"] = deleteCommands
+
+	// log.Infof("Insert Queries - [%v]", insertCommands)
+	// log.Infof("Select Queries - [%v]", selectCommands)
+	// log.Infof("Update Queries - [%v]", updateCommands)
+	// log.Infof("Delete Queries - [%v]", deleteCommands)
+
+	return randomSqlCommands
+
+}
+
+func GenerateSQLCommandPair(tableName string) map[string][]string {
+	var sqlCommandMap = make(map[string][]string)
+
+	randomKey := "key-" + RandomString(10)
+	randomValue := "value-" + RandomString(10)
+
+	insertQuery := fmt.Sprintf("INSERT INTO %s VALUES('%s', '%s')", tableName, randomKey, randomValue)
+	selectQuery := fmt.Sprintf("SELECT * FROM %s WHERE key='%s'", tableName, randomKey)
+
+	sqlCommandMap["insert"] = append(sqlCommandMap["insert"], insertQuery)
+	sqlCommandMap["select"] = append(sqlCommandMap["select"], selectQuery)
+
+	return sqlCommandMap
+}
+
+func CreateHostNameForApp(serviceName string, namespace string) string {
+	return fmt.Sprintf("%s.%s.svc.cluster.local", serviceName, namespace)
+}
+
+func ExtractConnectionInfo(ctx *scheduler.Context) (AppInfo, error) {
+
+	// TODO: This needs to be enhanced to support multiple application in one ctx
+	var appInfo AppInfo
+
+	for _, specObj := range ctx.App.SpecList {
+		if obj, ok := specObj.(*corev1.Service); ok {
+			appInfo.Namespace = obj.Namespace
+			appInfo.Hostname = CreateHostNameForApp(obj.Name, obj.Namespace)
+			if svcAnnotationValue, ok := obj.Annotations[svcAnnotationKey]; ok {
+				appInfo.StartDataSupport = (svcAnnotationValue == "true")
+				if !appInfo.StartDataSupport {
+					break
+				}
+			} else {
+				appInfo.StartDataSupport = false
+				break
+			}
+			if userAnnotationValue, ok := obj.Annotations[userAnnotationKey]; ok {
+				appInfo.User = userAnnotationValue
+			} else {
+				return appInfo, fmt.Errorf("Username not found")
+			}
+			if appTypeAnnotationValue, ok := obj.Annotations[appTypeAnnotationKey]; ok {
+				appInfo.AppType = appTypeAnnotationValue
+			} else {
+				return appInfo, fmt.Errorf("AppType not found")
+			}
+			if passwordAnnotationValue, ok := obj.Annotations[passwordAnnotationKey]; ok {
+				appInfo.Password = passwordAnnotationValue
+			} else {
+				return appInfo, fmt.Errorf("Password not found")
+			}
+			if portAnnotationValue, ok := obj.Annotations[portAnnotationKey]; ok {
+				appInfo.Port, _ = strconv.Atoi(portAnnotationValue)
+			}
+			if databaseAnnotationValue, ok := obj.Annotations[databaseAnnotationKey]; ok {
+				appInfo.DBName = databaseAnnotationValue
+			}
+		}
+	}
+
+	return appInfo, nil
+}
