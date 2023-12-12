ca8b2f2089c4990c14d80abb283b28542e65dde3
sn-px@purestorage.com <sn@purestorage.com>
2023-12-12 20:37:16 +0530

    Adding support for gce volume driver

--- drivers/volume/gce/gce.go
+++ drivers/volume/gce/gce.go
@@ -8,6 +8,7 @@ import (
 
 	torpedovolume "github.com/portworx/torpedo/drivers/volume"
 	"github.com/portworx/torpedo/drivers/volume/portworx/schedops"
+	"github.com/portworx/torpedo/pkg/errors"
 )
 
 const (
@@ -57,6 +58,45 @@ func (d *gce) Init(sched, nodeDriver, token, storageProvisioner, csiGenericDrive
 	return nil
 }
 
+func (d *gce) ValidateCreateVolume(name string, params map[string]string) error {
+	// TODO: Implementation of ValidateCreateVolume will be provided in the coming PRs
+	log.Warnf("ValidateCreateVolume function has not been implemented for volume driver - %s", d.String())
+	return nil
+}
+
+func (d *gce) ValidateVolumeSetup(vol *torpedovolume.Volume) error {
+	// TODO: Implementation of ValidateVolumeSetup will be provided in the coming PRs
+	log.Warnf("ValidateVolumeSetup function has not been implemented for volume driver - %s", d.String())
+	return nil
+}
+
+func (d *gce) ValidateDeleteVolume(vol *torpedovolume.Volume) error {
+	// TODO: Implementation of ValidateDeleteVolume will be provided in the coming PRs
+	log.Warnf("ValidateDeleteVolume function has not been implemented for volume driver - %s", d.String())
+	return nil
+}
+
+func (d *gce) GetDriverVersion() (string, error) {
+	// TODO: Implementation of ValidateDeleteVolume will be provided in the coming PRs
+	log.Warnf("GetDriverVersion function has not been implemented for volume driver - %s", d.String())
+	return "", nil
+}
+
+// RefreshDriverEndpoints get the updated driver endpoints for the cluster
+func (d *gce) RefreshDriverEndpoints() error {
+
+	log.Warnf("RefreshDriverEndpoints function has not been implemented for volume driver - %s", d.String())
+	return nil
+}
+
+// InspectVolume inspects the volume with the given name
+func (d *gce) InspectVolume(name string) (*api.Volume, error) {
+	log.Warnf("InspectVolume function has not been implemented for volume driver - %s", d.String())
+	return nil, &errors.ErrNotSupported{
+		Type:      "Function",
+		Operation: "InspectVolume()",
+	}
+}
 func init() {
 	torpedovolume.Register(DriverName, provisioners, &gce{})
 }
