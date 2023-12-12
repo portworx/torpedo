8fd86c3ce7606447ef607e13868a7bfe534ff478
atrivedi-px <atrivedi-px@purestorage.com>
2023-12-13 01:42:45 +0530

    Resetting namespaceAppWithDataMap before starting validations for new apps

--- tests/common.go
+++ tests/common.go
@@ -1915,7 +1915,11 @@ func ValidateApplications(contexts []*scheduler.Context) {
 // ValidateApplications validates applications
 func ValidateApplicationsStartData(contexts []*scheduler.Context, appContext context1.Context) (chan string, *errgroup.Group, map[string][]*appDriver.ApplicationDriver) {
 
+	// Resetting the global map before starting the new App Validations
+	namespaceAppWithDataMap = make(map[string][]*appDriver.ApplicationDriver)
+
 	var allHandlers []appDriver.ApplicationDriver
+
 	Step("validate applications", func() {
 		log.InfoD("Validate applications")
 		for _, ctx := range contexts {
