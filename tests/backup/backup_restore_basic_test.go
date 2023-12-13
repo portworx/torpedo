1f21ce28e13e538dc07c0cb79f8cc1a27a6bb092
Kshithij Iyer <kiyer@purestorage.com>
2023-12-13 15:54:59 +0530

    Code to be reverted

--- tests/backup/backup_restore_basic_test.go
+++ tests/backup/backup_restore_basic_test.go
@@ -3859,9 +3859,9 @@ var _ = Describe("{kiyertest}", func() {
 				collectionName := "restoreobjects"
 				srcClusterConfigPath, err := GetSourceClusterConfigPath()
 				dash.VerifyFatal(err, nil, fmt.Sprintf("Getting kubeconfig path for source cluster %v", srcClusterConfigPath))
+				time.Sleep(15 * time.Minute)
 				err = dumpMongodbCollectionOnConsole(srcClusterConfigPath, collectionName, mongodbusername, mongodbpasswd)
 				dash.VerifyFatal(err, nil, fmt.Sprintf("Dump mongodb collection Colletion name %s", collectionName))
-				time.Sleep(15 * time.Minute)
 			}
 		})
 	})
