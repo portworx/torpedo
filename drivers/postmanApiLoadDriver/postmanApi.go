605e480f272500b76c85c870e6251012704aceb1
jkagliwal-px <jkagliwal@purestorage.com>
2023-12-21 22:23:46 +0530

    DS-7260: First Integration of Postman with torpedo

---
+++ drivers/postmanApiLoadDriver/postmanApi.go
@@ -0,0 +1,161 @@
+package postmanApiLoadDriver
+
+import (
+	"fmt"
+	"github.com/portworx/sched-ops/k8s/apps"
+	"github.com/portworx/sched-ops/k8s/core"
+	"github.com/portworx/sched-ops/k8s/rbac"
+	"github.com/portworx/torpedo/pkg/log"
+	"github.com/portworx/torpedo/pkg/osutils"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"os"
+	"path/filepath"
+	"strings"
+	"time"
+)
+
+const (
+	postmanCentosImage          = "18jyotikagliwal/newmanshort:v2"
+	deploymentName              = "postman-newman"
+	defaultCollectionPath       = "../drivers/postmanApiLoadDriver/collections/collection.json"
+	timeOut                     = 30 * time.Minute
+	timeInterval                = 10 * time.Second
+	PxDataServices              = "pds"
+	PdsControlPlaneApiKeyExpiry = "2025-01-02T15:04:05Z"
+)
+
+var k8sRbac = rbac.Instance()
+var k8sCore = core.Instance()
+
+type PostmanDriver struct {
+	ResultsFileName string
+	ResultType      string
+	Namespace       string
+	Replicas        int32
+	Iteration       string
+	Kubeconfig      string
+}
+
+// ConfigMapData represents the structure of the ConfigMap data field
+type ConfigMapData struct {
+	YourField string `yaml:"postman-configmap"`
+}
+
+var k8sApps = apps.Instance()
+
+func GetProjectNameToExecutePostman(projectName string, driver *PostmanDriver) {
+	if projectName == PxDataServices {
+		_, _, err := ExecutePostmanForPds(driver)
+		if err != nil {
+			log.FailOnError(err, "Postman execution failed.. Please check the logs manually.")
+		}
+	}
+	//ToDo: Add cases for other PX Projects
+}
+
+func GetPostmanCollectionPath() (string, error) {
+	postmanCollectionFile, err := filepath.Abs(defaultCollectionPath)
+	if err != nil {
+		return "", fmt.Errorf("postman Collection Json not found, Please create a Collection json manually and export to {%v} folder", defaultCollectionPath)
+	}
+	flag, _ := CheckIfPostmanCollectionIsAvailable(postmanCollectionFile)
+	if flag != true {
+		return "", fmt.Errorf("postman Collection Json not found, Please create a Collection json manually and export to {%v} folder", defaultCollectionPath)
+	}
+	log.InfoD("PostmanCollectionFile found is- [%v]", postmanCollectionFile)
+	return postmanCollectionFile, nil
+}
+
+func CheckIfPostmanCollectionIsAvailable(postmanCollectionFile string) (bool, error) {
+	_, err := os.Stat(postmanCollectionFile)
+	if os.IsNotExist(err) {
+		return false, err
+	}
+	return true, nil
+}
+
+// Create MySql Workload (Non-TPCC)
+func (postman *PostmanDriver) RunNewmanWorkload(postmanParams *PostmanDriver) (*corev1.Pod, error) {
+	podSpec := &corev1.Pod{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "Pod",
+			APIVersion: "v1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			GenerateName: deploymentName + "-",
+			Namespace:    postmanParams.Namespace,
+		},
+		Spec: corev1.PodSpec{
+			Containers: []corev1.Container{
+				{
+					Name:    deploymentName,
+					Image:   postmanCentosImage,
+					Command: []string{"sleep", "infinity"},
+				},
+			},
+			RestartPolicy: corev1.RestartPolicyOnFailure,
+		},
+	}
+
+	pod, err := k8sCore.CreatePod(podSpec)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create pod [%s], Err: %v", podSpec.Name, err)
+	}
+
+	err = k8sCore.ValidatePod(pod, timeOut, timeInterval)
+	if err != nil {
+		return nil, fmt.Errorf("failed to validate pod [%s], Err: %v", pod.Name, err)
+	}
+
+	//TODO: Remove static sleep and verify the injected data
+	time.Sleep(1 * time.Minute)
+
+	return pod, nil
+}
+
+func RunKubectlCommandInPod(podName string, namespace string, kubeconfig string, command string) (string, error) {
+	cmd := fmt.Sprintf("kubectl --kubeconfig %v -n %v exec -it %v -- %v", kubeconfig, namespace, podName, command)
+	log.Infof("Command: ", cmd)
+	output, _, err := osutils.ExecShell(cmd)
+	if err != nil {
+		return "", err
+	}
+	log.Infof("Terminal output: %v", output)
+	return output, nil
+}
+
+func ExecuteCommandInShell(command string) (string, string, error) {
+	out, res, err := osutils.ExecShell(command)
+	if err != nil {
+		return "", "", err
+	}
+	return out, res, nil
+}
+
+func ExecutePostmanForPds(postmanParams *PostmanDriver) (*corev1.Pod, bool, error) {
+	collectionPath, err := GetPostmanCollectionPath()
+	if err != nil {
+		log.FailOnError(err, "Postman Collection Json not found, Please create a Collection json manually and export to {%v} folder", defaultCollectionPath)
+	}
+	log.InfoD("Postman Collection found is- %v", collectionPath)
+
+	postDep, err := postmanParams.RunNewmanWorkload(postmanParams)
+	copyCmd := "kubectl cp " + collectionPath + " " + postDep.Name + ":collection.json" + " -n " + postmanParams.Namespace
+	out, res, err := ExecuteCommandInShell(copyCmd)
+	if err != nil {
+		return nil, false, fmt.Errorf("there was some problem in executing Postman Newman container due to- [%v]", err)
+	}
+	log.Info("output of copy command is - %v, %v", out, res)
+	newmanCmd := "newman run collection.json --verbose"
+	output, err := RunKubectlCommandInPod(postDep.Name, postmanParams.Namespace, postmanParams.Kubeconfig, newmanCmd)
+	if err != nil {
+		return nil, false, fmt.Errorf("there was some problem in executing Postman Newman container due to- [%v]", err)
+	}
+	log.InfoD("output from the newman execution is- %v", output)
+	if strings.Contains(output, "failure") {
+		log.FailOnError(err, "newman exited with a failure.. Please check logs for more details")
+	}
+	k8sCore.DeletePod(postDep.Name, postDep.Namespace, true)
+	return postDep, true, nil
+}
---
+++ tests/pds/pds_postman_test.go
@@ -0,0 +1,37 @@
+package tests
+
+import (
+	. "github.com/onsi/ginkgo"
+	postmanLib "github.com/portworx/torpedo/drivers/postmanApiLoadDriver"
+	"github.com/portworx/torpedo/pkg/log"
+	. "github.com/portworx/torpedo/tests"
+)
+
+var _ = Describe("{RunPdsPostManApiLoadTests}", func() {
+
+	JustBeforeEach(func() {
+		StartTorpedoTest("RunPdsPostManApiLoadTests", "Run PDS Specific Api Load Tests using Postman-Newman ", pdsLabels, 0)
+
+	})
+
+	It("Deploy Dataservices", func() {
+		Step("Starting to execute Postman-Newman on PDS", func() {
+			var resultsFileName = "postman_results"
+			ctx, err := GetSourceClusterConfigPath()
+			log.FailOnError(err, "failed while getting dest cluster path")
+			//collectionPath, err := postmanLib.GetPostmanCollectionPath(resultsFileName)
+			postmanParams := postmanLib.PostmanDriver{
+				ResultsFileName: resultsFileName,
+				ResultType:      "json, cli",
+				Namespace:       params.InfraToTest.Namespace,
+				Replicas:        2,
+				Iteration:       "2",
+				Kubeconfig:      ctx}
+			postmanLib.GetProjectNameToExecutePostman("pds", &postmanParams)
+		})
+
+	})
+	JustAfterEach(func() {
+		defer EndTorpedoTest()
+	})
+})
