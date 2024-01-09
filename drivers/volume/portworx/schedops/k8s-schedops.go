d7346bac90372842317276549a2b0a2400898dbb
Dhruv Bhatnagar <dbhatnagar@purestorage.com>
2024-01-09 23:55:48 +0530

    workaround for a fix

--- drivers/volume/portworx/schedops/k8s-schedops.go
+++ drivers/volume/portworx/schedops/k8s-schedops.go
@@ -812,7 +812,14 @@ func (k *k8sSchedOps) IsPXEnabled(n node.Node) (bool, error) {
 	kubeNode := node.(*corev1.Node)
 	// if node has px/enabled label set to false or node-type public or
 	// has any taints then px is disabled on node
-	if kubeNode.Labels[PXEnabledLabelKey] == "false" || kubeNode.Labels[dcosNodeType] == "public" || len(kubeNode.Spec.Taints) > 2 {
+	skipTaint := false
+	for _, taint := range kubeNode.Spec.Taints {
+		if taint.Key == "UpdateInProgress" && taint.Effect == "PreferNoSchedule" {
+			skipTaint = true
+			break
+		}
+	}
+	if kubeNode.Labels[PXEnabledLabelKey] == "false" || kubeNode.Labels[dcosNodeType] == "public" || (!skipTaint && len(kubeNode.Spec.Taints) > 0) {
 		log.Infof("PX is not enabled on node %v. Will be skipped for tests.", n.Name)
 		return false, nil
 	}
