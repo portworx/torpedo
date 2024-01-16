246510aaf17748ad206b343bef99d2e13ddcae8e
spai-px <spai@purestorage.com>
2024-01-16 14:27:18 +0530

    change replication to 2

--- tests/basic/pure_test.go
+++ tests/basic/pure_test.go
@@ -2307,7 +2307,7 @@ var _ = Describe("{ReplIncWithNodeNotInReplicaSet}", func() {
 			for vol := 0; vol < numberOfVolumes; vol++ {
 				volName := fmt.Sprintf("vol-%d", vol)
 				log.InfoD("Creating volume [%s]", volName)
-				_, err := Inst().V.CreateVolume(volName, 2, 2)
+				_, err := Inst().V.CreateVolume(volName, 2, 1)
 				log.FailOnError(err, "Failed to create volume [%s]", volName)
 			}
 		})
