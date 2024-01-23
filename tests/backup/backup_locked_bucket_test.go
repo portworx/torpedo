aec5a510dfdf377b780d22b51d450d31380b0489
Kshithij Iyer <kiyer@purestorage.com>
2024-01-23 15:34:31 +0530

    Add check for rule based execution

--- tests/backup/backup_locked_bucket_test.go
+++ tests/backup/backup_locked_bucket_test.go
@@ -1270,19 +1270,23 @@ var _ = Describe("{BackupToLockedBucketWithSharedObjects}", func() {
 			ctx, err := backup.GetAdminCtxFromSecret()
 			log.FailOnError(err, "failed to fetch px-admin ctx")
 
-			preRuleUid, err := Inst().Backup.GetRuleUid(orgID, ctx, preRuleNameList[0])
-			dash.VerifyFatal(err, nil, fmt.Sprintf("Getting prerule object for  - %s", preRuleNameList[0]))
+			if len(preRuleNameList) > 0 {
+				preRuleUid, err := Inst().Backup.GetRuleUid(orgID, ctx, preRuleNameList[0])
+				dash.VerifyFatal(err, nil, fmt.Sprintf("Getting prerule object for  - %s", preRuleNameList[0]))
 
-			log.InfoD("Update pre-rule ownership for users - [%v]", userNames)
-			err = AddRuleOwnership(preRuleNameList[0], preRuleUid, userNames, nil, Read, Invalid, ctx)
-			dash.VerifyFatal(err, nil, fmt.Sprintf("Verifying updation of ownership for pre-rule - %s", preRuleNameList[0]))
+				log.InfoD("Update pre-rule ownership for users - [%v]", userNames)
+				err = AddRuleOwnership(preRuleNameList[0], preRuleUid, userNames, nil, Read, Invalid, ctx)
+				dash.VerifyFatal(err, nil, fmt.Sprintf("Verifying updation of ownership for pre-rule - %s", preRuleNameList[0]))
+			}
 
-			postRuleUid, err := Inst().Backup.GetRuleUid(orgID, ctx, postRuleNameList[0])
-			dash.VerifyFatal(err, nil, fmt.Sprintf("Getting postrule object for  - %s", postRuleNameList[0]))
+			if len(postRuleNameList) > 0 {
+				postRuleUid, err := Inst().Backup.GetRuleUid(orgID, ctx, postRuleNameList[0])
+				dash.VerifyFatal(err, nil, fmt.Sprintf("Getting postrule object for  - %s", postRuleNameList[0]))
 
-			log.InfoD("Update post-rule ownership for users - [%v]", userNames)
-			err = AddRuleOwnership(postRuleNameList[0], postRuleUid, userNames, nil, Read, Invalid, ctx)
-			dash.VerifyFatal(err, nil, fmt.Sprintf("Verifying updation of ownership for post-rule - %s", postRuleNameList[0]))
+				log.InfoD("Update post-rule ownership for users - [%v]", userNames)
+				err = AddRuleOwnership(postRuleNameList[0], postRuleUid, userNames, nil, Read, Invalid, ctx)
+				dash.VerifyFatal(err, nil, fmt.Sprintf("Verifying updation of ownership for post-rule - %s", postRuleNameList[0]))
+			}
 
 		})
 
