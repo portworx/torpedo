5c7a8e736bfd0ddbe1820f9a2b36d0beb417750c
kphalgun-px <kphalgun@purestorage.com>
2024-01-24 11:59:49 +0530

    [PTX-21248] Refactor Imports for Ginkgo v2

--- tests/backup/backup_basic_test.go
+++ tests/backup/backup_basic_test.go
@@ -7,8 +7,8 @@ import (
 	"testing"
 	"time"
 
-	. "github.com/onsi/ginkgo"
 	"github.com/onsi/ginkgo/reporters"
+	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	api "github.com/portworx/px-backup-api/pkg/apis/v1"
 	_ "github.com/portworx/px-backup-api/pkg/kubeauth/gcp"
