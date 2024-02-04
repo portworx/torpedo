4c968504aa724d588f01c21769e188f7a57c067b
lsrinivas-pure <lsrinivas@purestorage.com>
2024-02-04 19:40:18 +0530

    Adding node validations during platform upgrade

--- tests/basic/upgrade_cluster_test.go
+++ tests/basic/upgrade_cluster_test.go
@@ -2,13 +2,17 @@ package tests
 
 import (
 	"fmt"
+	"github.com/portworx/sched-ops/k8s/core"
+	"github.com/portworx/torpedo/pkg/log"
 	"net/url"
 	"strings"
+	"time"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	"github.com/portworx/torpedo/drivers/scheduler"
 	. "github.com/portworx/torpedo/tests"
+	"k8s.io/api/core/v1"
 )
 
 var _ = Describe("{UpgradeCluster}", func() {
@@ -34,12 +38,18 @@ var _ = Describe("{UpgradeCluster}", func() {
 			versions = strings.Split(Inst().SchedUpgradeHops, ",")
 		}
 		Expect(versions).NotTo(BeEmpty())
+		stopSignal := make(chan struct{})
+		var mError error
+		go getClusterNodesInfo(stopSignal, &mError)
+		defer close(stopSignal)
 
 		for _, version := range versions {
 			Step("start scheduler upgrade", func() {
 				err := Inst().S.UpgradeScheduler(version)
 				Expect(err).NotTo(HaveOccurred())
 			})
+
+			dash.VerifyFatal(mError, nil, "validate no parallel upgrade of nodes")
 
 			Step("validate storage components", func() {
 				u, err := url.Parse(fmt.Sprintf("%s/%s", Inst().StorageDriverUpgradeEndpointURL, Inst().StorageDriverUpgradeEndpointVersion))
@@ -66,3 +76,56 @@ var _ = Describe("{UpgradeCluster}", func() {
 		AfterEachTest(contexts)
 	})
 })
+
+func getClusterNodesInfo(stopSignal <-chan struct{}, mError *error) {
+
+	masterNodeStatus := make(map[string]bool)
+	workerNodeStatus := make(map[string]bool)
+	itr := 1
+	for {
+		log.Infof("K8s node validation iteration: #%d", itr)
+		select {
+		case <-stopSignal:
+			log.Infof("Exiting node validations routine")
+			return
+		default:
+			nodeList, err := core.Instance().GetNodes()
+			if err != nil {
+				mError = &err
+				return
+			}
+			masterCount := 0
+			workerCount := 0
+			for _, k8sNode := range nodeList.Items {
+				if strings.Contains(k8sNode.Name, "master") {
+					masterNodeStatus[k8sNode.Name] = k8sNode.Spec.Unschedulable
+					if k8sNode.Spec.Unschedulable {
+						masterCount += 1
+					}
+				} else {
+					workerNodeStatus[k8sNode.Name] = k8sNode.Spec.Unschedulable
+					if k8sNode.Spec.Unschedulable {
+						workerCount += 1
+					}
+				}
+			}
+			log.Infof("Master Map status is %#v", masterNodeStatus)
+			log.Infof("Worker Map status is %#v", workerNodeStatus)
+			if masterCount > 1 || workerCount > 1 {
+				err = fmt.Errorf("multiple controlpnane or worker nodes are Unschedulable at same time,"+
+					"controlplane:%#v,worker:%#v", masterNodeStatus, workerNodeStatus)
+				mError = &err
+				return
+			}
+
+			itr++
+			time.Sleep(30 * time.Second)
+		}
+	}
+
+}
+
+func isNodeUpgradeInProgress(node v1.Node) bool {
+	log.Infof("Node [%s] Unschedulable status: %v ", node.Name, node.Spec.Unschedulable)
+	return node.Spec.Unschedulable
+}
