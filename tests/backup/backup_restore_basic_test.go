bcce7ec1cf87b643f7e53e194306ed3d549846e0
Kshithij Iyer <kiyer@purestorage.com>
2024-02-15 11:56:59 +0530

    PB-5664: Fix concurrent map writes issue in backup tests

--- tests/backup/backup_restore_basic_test.go
+++ tests/backup/backup_restore_basic_test.go
@@ -1931,6 +1931,7 @@ var _ = Describe("{MultipleInPlaceRestoreSameTime}", func() {
 			log.InfoD("Taking backup of application for different combination of restores")
 			var sem = make(chan struct{}, 10)
 			var wg sync.WaitGroup
+			var mutex sync.Mutex
 			for _, bkpNameSpace := range bkpNamespaces {
 				sem <- struct{}{}
 				backupName = fmt.Sprintf("%s-%s-%v", BackupNamePrefix, bkpNameSpace, RandomString(5))
@@ -1942,7 +1943,9 @@ var _ = Describe("{MultipleInPlaceRestoreSameTime}", func() {
 					appContextsToBackup := FilterAppContextsByNamespace(scheduledAppContexts, []string{bkpNameSpace})
 					err := CreateBackupWithValidation(ctx, backupName, SourceClusterName, bkpLocationName, backupLocationUID, appContextsToBackup, labelSelectors, BackupOrgID, clusterUid, "", "", "", "")
 					dash.VerifyFatal(err, nil, fmt.Sprintf("Creation and Validation of backup [%s] of namespace (scheduled Context) [%s]", backupName, bkpNameSpace))
+					mutex.Lock()
 					backupNamespaceMapping[bkpNameSpace] = backupName
+					mutex.Unlock()
 				}(bkpNameSpace, backupName)
 			}
 			wg.Wait()
@@ -1952,6 +1955,7 @@ var _ = Describe("{MultipleInPlaceRestoreSameTime}", func() {
 			ctx, err := backup.GetAdminCtxFromSecret()
 			log.FailOnError(err, "Fetching px-central-admin ctx")
 			var wg sync.WaitGroup
+			var mutex sync.Mutex
 			for bkpNameSpace, backupName := range backupNamespaceMapping {
 				restoreName := fmt.Sprintf("%s-%s-%v", "test-restore-recent-backup", bkpNameSpace, RandomString(5))
 				restoreList = append(restoreList, restoreName)
@@ -1960,7 +1964,9 @@ var _ = Describe("{MultipleInPlaceRestoreSameTime}", func() {
 					defer GinkgoRecover()
 					defer wg.Done()
 					appContextsToBackup := FilterAppContextsByNamespace(scheduledAppContexts, []string{bkpNameSpace})
+					mutex.Lock()
 					restoreContextMapping[restoreName] = appContextsToBackup
+					mutex.Unlock()
 					err = CreateRestore(restoreName, backupName, make(map[string]string), SourceClusterName, BackupOrgID, ctx, make(map[string]string))
 					dash.VerifyFatal(err, nil, fmt.Sprintf("Restoring backup %v into namespce %v with replacing existing resources", backupName, bkpNameSpace))
 				}(bkpNameSpace, backupName)
@@ -1988,7 +1994,9 @@ var _ = Describe("{MultipleInPlaceRestoreSameTime}", func() {
 							errors = append(errors, fmt.Sprintf("Failed while context tranforming of restore [%s]. Error - [%s]", restoreName, err.Error()))
 							mutex.Unlock()
 						}
+						mutex.Lock()
 						restoredAppContextsInSourceCluster = append(restoredAppContextsInSourceCluster, restoredAppContext)
+						mutex.Lock()
 					}
 					err = ValidateRestore(ctx, restoreName, BackupOrgID, restoredAppContextsInSourceCluster, make([]string, 0))
 					if err != nil {
@@ -2006,6 +2014,7 @@ var _ = Describe("{MultipleInPlaceRestoreSameTime}", func() {
 			ctx, err := backup.GetAdminCtxFromSecret()
 			log.FailOnError(err, "Fetching px-central-admin ctx")
 			var wg sync.WaitGroup
+			var mutex sync.Mutex
 			for bkpNameSpace, backupName := range backupNamespaceMapping {
 				restoreName := fmt.Sprintf("%s-%s-%v", "test-restore-recent-backup", bkpNameSpace, RandomString(5))
 				restoreList = append(restoreList, restoreName)
@@ -2014,7 +2023,9 @@ var _ = Describe("{MultipleInPlaceRestoreSameTime}", func() {
 					defer GinkgoRecover()
 					defer wg.Done()
 					appContextsToBackup := FilterAppContextsByNamespace(scheduledAppContexts, []string{bkpNameSpace})
+					mutex.Lock()
 					restoreWithReplaceContextMapping[restoreName] = appContextsToBackup
+					mutex.Unlock()
 					err = CreateRestoreWithReplacePolicy(restoreName, backupName, make(map[string]string), SourceClusterName, BackupOrgID, ctx, make(map[string]string), ReplacePolicyDelete)
 					dash.VerifyFatal(err, nil, fmt.Sprintf("Restoring backup %v into namespce %v with replacing existing resources", backupName, bkpNameSpace))
 				}(bkpNameSpace, backupName)
