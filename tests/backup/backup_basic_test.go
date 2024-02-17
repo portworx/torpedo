c9e8c14b3bffb214360b6598dd9156ab687671a3
atrivedi-px <atrivedi-px@purestorage.com>
2024-02-17 12:05:24 +0530

    Revert "Removing cluster present check"
    This reverts commit 84b686f2c551fd0d8e7642c60641ef502e8812f0.

--- tests/backup/backup_basic_test.go
+++ tests/backup/backup_basic_test.go
@@ -357,13 +357,21 @@ var _ = AfterSuite(func() {
 		// Validating custom resource object cleanup
 		for _, kubeconfig := range kubeconfigList {
 			clusterName := strings.Split(kubeconfig, "-")[0] + "-cluster"
-			log.Infof("Cluster Name - [%s]", clusterName)
-			clusterReq := &api.ClusterInspectRequest{OrgId: BackupOrgID, Name: clusterName, IncludeSecrets: true}
-			clusterResp, err := Inst().Backup.InspectCluster(ctx, clusterReq)
-			dash.VerifyFatal(err, nil, fmt.Sprintf("Unable to get cluster response"))
-			clusterObj := clusterResp.GetCluster()
-			err = ValidateAllBackupCreatedResourceCleanup(clusterObj)
-			dash.VerifyFatal(err, nil, fmt.Sprintf("CR/Backup resources found after cleanup"))
+			isPresent, err := IsClusterPresent(clusterName, ctx, BackupOrgID)
+			if err != nil {
+				Inst().Dash.VerifySafely(err, nil, fmt.Sprintf("Verifying if cluster [%s] is present", clusterName))
+			}
+			if isPresent {
+				clusterUID, err := Inst().Backup.GetClusterUID(ctx, BackupOrgID, clusterName)
+				dash.VerifyFatal(err, nil, fmt.Sprintf("Unable to get cluster UID"))
+				log.Infof("Cluster UID - [%s], Cluster Name - [%s]", clusterUID, clusterName)
+				clusterReq := &api.ClusterInspectRequest{OrgId: BackupOrgID, Name: clusterName, IncludeSecrets: true, Uid: clusterUID}
+				clusterResp, err := Inst().Backup.InspectCluster(ctx, clusterReq)
+				dash.VerifyFatal(err, nil, fmt.Sprintf("Unable to get cluster response"))
+				clusterObj := clusterResp.GetCluster()
+				err = ValidateAllBackupCreatedResourceCleanup(clusterObj)
+				dash.VerifyFatal(err, nil, fmt.Sprintf("CR/Backup resources found after cleanup"))
+			}
 		}
 
 		// Deleting clusters and the corresponding cloud cred
