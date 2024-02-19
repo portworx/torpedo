a42a6a8adf9211a202dcc05c6cc4f242ae2bc59d
Avinash GV PX <avinash@portworx.com>
2024-02-19 16:22:07 +0530

    increased sleep time

--- deployments/deploy-ssh-taas.sh
+++ deployments/deploy-ssh-taas.sh
@@ -489,7 +489,7 @@ spec:
     imagePullPolicy: Always
     securityContext:
       privileged: ${SECURITY_CONTEXT}
-    command: ["sh", "-c", "cd /bin && ./taas && sleep 1800"]
+    command: ["sh", "-c", "cd /bin && ./taas && sleep 10800"]
     tty: true
     volumeMounts: [${VOLUME_MOUNTS}]
     env:
