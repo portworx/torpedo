6ad3f4a42af56f6cb9eef4f9eb637d0a945cec0d
atrivedi-px <atrivedi-px@purestorage.com>
2024-02-23 11:14:24 +0530

    Vendoring in new sched-ops changes

--- vendor/github.com/portworx/sched-ops/k8s/kubevirt/virtualmachineinstance.go
+++ vendor/github.com/portworx/sched-ops/k8s/kubevirt/virtualmachineinstance.go
@@ -2,6 +2,7 @@ package kubevirt
 
 import (
 	"context"
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	kubevirtv1 "kubevirt.io/api/core/v1"
 )
@@ -10,6 +11,8 @@ import (
 type VirtualMachineInstanceOps interface {
 	// GetVirtualMachineInstance gets updated Virtual Machine Instance from client matching name and namespace
 	GetVirtualMachineInstance(context.Context, string, string) (*kubevirtv1.VirtualMachineInstance, error)
+	// UpdateVirtualMachineInstance updates existing Virtual Machine Instance
+	UpdateVirtualMachineInstance(context.Context, *kubevirtv1.VirtualMachineInstance) (*kubevirtv1.VirtualMachineInstance, error)
 }
 
 // GetVirtualMachineInstance gets updated Virtual Machine Instance from client matching name and namespace
@@ -19,4 +22,18 @@ func (c *Client) GetVirtualMachineInstance(ctx context.Context, name string, nam
 	}
 
 	return c.kubevirt.VirtualMachineInstance(namespace).Get(ctx, name, &metav1.GetOptions{})
+}
+
+// UpdateVirtualMachineInstance updates existing Virtual Machine Instance
+func (c *Client) UpdateVirtualMachineInstance(ctx context.Context, vmInstance *kubevirtv1.VirtualMachineInstance) (*kubevirtv1.VirtualMachineInstance, error) {
+	if err := c.initClient(); err != nil {
+		return nil, err
+	}
+
+	ns := vmInstance.Namespace
+	if len(ns) == 0 {
+		ns = corev1.NamespaceDefault
+	}
+
+	return c.kubevirt.VirtualMachineInstance(ns).Update(ctx, vmInstance)
 }
