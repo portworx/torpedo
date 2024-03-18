5af0e105b7b4fc0e76e347a9caf2e049c4018852
sn-px@purestorage.com <sn@purestorage.com>
2024-03-18 22:52:48 +0530

    multiple provisioner support

--- tests/backup/backup_sanity_test.go
+++ tests/backup/backup_sanity_test.go
@@ -260,7 +260,7 @@ var _ = Describe("{BasicBackupCreationDummyTest}", func() {
 			err = IsFullBackup(forceKdmpSchBackupName, BackupOrgID, ctx)
 			dash.VerifyFatal(err, nil, fmt.Sprintf("Fetching the name of the next schedule backup for schedule: [%s] for backup location %s", forceKdmpSchBackupName, forceKdmpSchBackupName))
 			scheduleUid, err := Inst().Backup.GetBackupScheduleUID(ctx, kdmpScheduleName, BackupOrgID)
-			err = DeleteScheduleWithUIDAndWait(forceKdmpSchBackupName, scheduleUid, SourceClusterName, sourceClusterUid, BackupOrgID, ctx)
+			err = DeleteScheduleWithUIDAndWait(kdmpScheduleName, scheduleUid, SourceClusterName, sourceClusterUid, BackupOrgID, ctx)
 			log.FailOnError(err, "Fetching px-central-admin")
 		})
 
