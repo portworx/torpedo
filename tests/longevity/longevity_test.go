a1d3daa4699c337aa98859e16d5aaa24e0991e85
kphalgun-px <kphalgun@purestorage.com>
2024-03-29 01:34:45 +0530

    [PTX-22914] LONGEVITY_UPGRADE_EXECUTION_THRESHOLD can be 0

--- tests/longevity/longevity_test.go
+++ tests/longevity/longevity_test.go
@@ -287,7 +287,7 @@ var _ = Describe("{UpgradeLongevity}", func() {
 			log.InfoD("Upgrade longevity tests timeout set to %d minutes", Inst().MinRunTimeMins)
 		}
 		upgradeExecutionThreshold = 1 // default value
-		if val, err := strconv.Atoi(os.Getenv("LONGEVITY_UPGRADE_EXECUTION_THRESHOLD")); err == nil && val > 0 {
+		if val, err := strconv.Atoi(os.Getenv("LONGEVITY_UPGRADE_EXECUTION_THRESHOLD")); err == nil {
 			upgradeExecutionThreshold = val
 		}
 		disruptiveTriggerWrapper = func(fn TriggerFunction) TriggerFunction {
