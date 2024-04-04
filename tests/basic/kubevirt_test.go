e0505fae88bf0608c0f49f879e417c40c2b930f5
Dhruv Bhatnagar <dbhatnagar@purestorage.com>
2024-04-04 10:31:38 +0530

    first set of negative tests for 3102

--- tests/basic/kubevirt_test.go
+++ tests/basic/kubevirt_test.go
@@ -4,9 +4,11 @@ import (
 	context1 "context"
 	"fmt"
 	. "github.com/onsi/ginkgo/v2"
+	"github.com/portworx/torpedo/drivers/node"
 	"github.com/portworx/torpedo/drivers/scheduler"
 	"github.com/portworx/torpedo/pkg/log"
 	. "github.com/portworx/torpedo/tests"
+	"time"
 )
 
 var _ = Describe("{AddNewDiskToKubevirtVM}", func() {
@@ -107,6 +109,223 @@ var _ = Describe("{KubeVirtLiveMigration}", func() {
 		})
 
 	})
+	JustAfterEach(func() {
+		defer EndTorpedoTest()
+		AfterEachTest(appCtxs)
+	})
+})
+
+var _ = Describe("{PxKillBeforeAddDiskToVM}", func() {
+	JustBeforeEach(func() {
+		StartTorpedoTest("PxKillBeforeAddDiskToVM", "Kill Px on host node of Kubuevirt VM and then Add a disk", nil, 0)
+	})
+
+	var appCtxs []*scheduler.Context
+	var nodes []string
+	itLog := "Kill Px then Add disk to Kubevirt VM"
+	It(itLog, func() {
+		appList := Inst().AppList
+		defer func() {
+			Inst().AppList = appList
+		}()
+		numberOfVolumes := 1
+		Inst().AppList = []string{"kubevirt-cirros-cd-with-pvc"}
+		stepLog := "schedule a kubevirtVM"
+		Step(stepLog, func() {
+			for i := 0; i < Inst().GlobalScaleFactor; i++ {
+				appCtxs = append(appCtxs, ScheduleApplications("test")...)
+			}
+		})
+		ValidateApplications(appCtxs)
+		for _, appCtx := range appCtxs {
+			bindMount, err := IsVMBindMounted(appCtx, false)
+			log.FailOnError(err, "Failed to verify bind mount")
+			dash.VerifyFatal(bindMount, true, "Failed to verify bind mount")
+		}
+
+		stepLog = "Kill Px on node hosting VM"
+		Step(stepLog, func() {
+			log.InfoD(stepLog)
+			// Collect all nodes to restart Px on
+			for _, appCtx := range appCtxs {
+				vms, err := GetAllVMsFromScheduledContexts([]*scheduler.Context{appCtx})
+				log.FailOnError(err, "Failed to get VMs from context")
+				for _, vm := range vms {
+					nodeName, err := GetNodeOfVM(vm)
+					log.FailOnError(err, "Failed to get node of vm %v", vm.Name)
+					nodes = append(nodes, nodeName)
+				}
+			}
+			// Restart Px on all relevant nodes one by one
+			for _, appNode := range node.GetStorageDriverNodes() {
+				for _, vmNode := range nodes {
+					if vmNode == appNode.Name {
+						stepLog = fmt.Sprintf("stop volume driver %s on node: %s",
+							Inst().V.String(), appNode.Name)
+						Step(stepLog,
+							func() {
+								log.InfoD(stepLog)
+								StopVolDriverAndWait([]node.Node{appNode})
+							})
+
+						stepLog = fmt.Sprintf("starting volume %s driver on node %s",
+							Inst().V.String(), appNode.Name)
+						Step(stepLog,
+							func() {
+								log.InfoD(stepLog)
+								StartVolDriverAndWait([]node.Node{appNode})
+							})
+
+						stepLog = "Giving few seconds for volume driver to stabilize"
+						Step(stepLog, func() {
+							log.InfoD(stepLog)
+							time.Sleep(20 * time.Second)
+						})
+					}
+				}
+			}
+		})
+
+		stepLog = "Add one disk to the kubevirt VM"
+		Step(stepLog, func() {
+			log.InfoD(stepLog)
+			success, err := AddDisksToKubevirtVM(appCtxs, numberOfVolumes, "0.5Gi")
+			log.FailOnError(err, "Failed to add disks to kubevirt VM")
+			dash.VerifyFatal(success, true, "Failed to add disks to kubevirt VM?")
+		})
+
+		stepLog = "Verify the new disk added is also bind mounted"
+		Step(stepLog, func() {
+			log.InfoD(stepLog)
+			for _, appCtx := range appCtxs {
+				isVmBindMounted, err := IsVMBindMounted(appCtx, true)
+				log.FailOnError(err, "Failed to verify disks in kubevirt VM")
+				if !isVmBindMounted {
+					log.Errorf("The newly added disk to vm %s is not bind mounted", appCtx.App.Key)
+				}
+			}
+		})
+
+	})
+
+	JustAfterEach(func() {
+		defer EndTorpedoTest()
+		AfterEachTest(appCtxs)
+	})
+})
+
+var _ = Describe("{PxKillAfterAddDiskToVM}", func() {
+	JustBeforeEach(func() {
+		StartTorpedoTest("PxKillAfterAddDiskToVM", "Add a disk to Kubevirt VM, kill Px, Add another disk and validate the VM", nil, 0)
+	})
+
+	var appCtxs []*scheduler.Context
+	var nodes []string
+	itLog := "Add disk to Kubevirt VM, Kill Px and then add another disk"
+	It(itLog, func() {
+		appList := Inst().AppList
+		defer func() {
+			Inst().AppList = appList
+		}()
+		numberOfVolumes := 1
+		Inst().AppList = []string{"kubevirt-cirros-cd-with-pvc"}
+		stepLog := "schedule a kubevirtVM"
+		Step(stepLog, func() {
+			for i := 0; i < Inst().GlobalScaleFactor; i++ {
+				appCtxs = append(appCtxs, ScheduleApplications("test")...)
+			}
+		})
+		ValidateApplications(appCtxs)
+		for _, appCtx := range appCtxs {
+			bindMount, err := IsVMBindMounted(appCtx, false)
+			log.FailOnError(err, "Failed to verify bind mount")
+			dash.VerifyFatal(bindMount, true, "Failed to verify bind mount")
+		}
+
+		stepLog = "Add one disk to the kubevirt VM"
+		Step(stepLog, func() {
+			log.InfoD(stepLog)
+			success, err := AddDisksToKubevirtVM(appCtxs, numberOfVolumes, "0.5Gi")
+			log.FailOnError(err, "Failed to add disks to kubevirt VM")
+			dash.VerifyFatal(success, true, "Failed to add disks to kubevirt VM?")
+		})
+
+		stepLog = "Verify the new disk added is also bind mounted"
+		Step(stepLog, func() {
+			log.InfoD(stepLog)
+			for _, appCtx := range appCtxs {
+				isVmBindMounted, err := IsVMBindMounted(appCtx, true)
+				log.FailOnError(err, "Failed to verify disks in kubevirt VM")
+				if !isVmBindMounted {
+					log.Errorf("The newly added disk to vm %s is not bind mounted", appCtx.App.Key)
+				}
+			}
+		})
+		stepLog = "Kill Px on node hosting VM"
+		Step(stepLog, func() {
+			log.InfoD(stepLog)
+			// Collect all nodes to restart Px on
+			for _, appCtx := range appCtxs {
+				vms, err := GetAllVMsFromScheduledContexts([]*scheduler.Context{appCtx})
+				log.FailOnError(err, "Failed to get VMs from context")
+				for _, vm := range vms {
+					nodeName, err := GetNodeOfVM(vm)
+					log.FailOnError(err, "Failed to get node of vm %v", vm.Name)
+					nodes = append(nodes, nodeName)
+				}
+			}
+			// Restart Px on all relevant nodes one by one
+			for _, appNode := range node.GetStorageDriverNodes() {
+				for _, vmNode := range nodes {
+					if vmNode == appNode.Name {
+						stepLog = fmt.Sprintf("stop volume driver %s on node: %s",
+							Inst().V.String(), appNode.Name)
+						Step(stepLog,
+							func() {
+								log.InfoD(stepLog)
+								StopVolDriverAndWait([]node.Node{appNode})
+							})
+
+						stepLog = fmt.Sprintf("starting volume %s driver on node %s",
+							Inst().V.String(), appNode.Name)
+						Step(stepLog,
+							func() {
+								log.InfoD(stepLog)
+								StartVolDriverAndWait([]node.Node{appNode})
+							})
+
+						stepLog = "Giving few seconds for volume driver to stabilize"
+						Step(stepLog, func() {
+							log.InfoD(stepLog)
+							time.Sleep(20 * time.Second)
+						})
+					}
+				}
+			}
+		})
+		stepLog = "Add another disk to the kubevirt VM"
+		Step(stepLog, func() {
+			log.InfoD(stepLog)
+			numberOfVolumes = 2
+			success, err := AddDisksToKubevirtVM(appCtxs, numberOfVolumes, "0.5Gi")
+			log.FailOnError(err, "Failed to add disks to kubevirt VM")
+			dash.VerifyFatal(success, true, "Failed to add disks to kubevirt VM?")
+		})
+
+		stepLog = "Verify the new disk added is also bind mounted"
+		Step(stepLog, func() {
+			log.InfoD(stepLog)
+			for _, appCtx := range appCtxs {
+				isVmBindMounted, err := IsVMBindMounted(appCtx, true)
+				log.FailOnError(err, "Failed to verify disks in kubevirt VM")
+				if !isVmBindMounted {
+					log.Errorf("The newly added disk to vm %s is not bind mounted", appCtx.App.Key)
+				}
+			}
+		})
+
+	})
+
 	JustAfterEach(func() {
 		defer EndTorpedoTest()
 		AfterEachTest(appCtxs)
