a0ee171a23053a44a3978f451c8cf54587ba1a68
Suhas Pai <spai@purestorage.com>
2024-04-05 17:34:42 +0530

    Add test case for kubevirt Ha increase decrease

--- tests/basic/kubevirt_test.go
+++ tests/basic/kubevirt_test.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	. "github.com/onsi/ginkgo/v2"
 	"github.com/portworx/torpedo/drivers/scheduler"
+	"github.com/portworx/torpedo/drivers/volume"
 	"github.com/portworx/torpedo/pkg/log"
 	. "github.com/portworx/torpedo/tests"
 )
@@ -107,6 +108,158 @@ var _ = Describe("{KubeVirtLiveMigration}", func() {
 		})
 
 	})
+	JustAfterEach(func() {
+		defer EndTorpedoTest()
+		AfterEachTest(appCtxs)
+	})
+})
+
+var _ = Describe("{KubevirtVMVolHaIncrease}", func() {
+
+	JustBeforeEach(func() {
+		StartTorpedoTest("KubevirtVMVolHaIncrease", "Increase the volume HA of a kubevirt VM", nil, 0)
+	})
+	var appCtxs []*scheduler.Context
+
+	itLog := "Increase the volume HA of a kubevirt VM"
+	It(itLog, func() {
+		appList := Inst().AppList
+		defer func() {
+			Inst().AppList = appList
+		}()
+
+		Inst().AppList = []string{"kubevirt-fio-load-disk-repl-2"}
+		stepLog := "schedule a kubevirt VM"
+		Step(stepLog, func() {
+			for i := 0; i < Inst().GlobalScaleFactor; i++ {
+				appCtxs = append(appCtxs, ScheduleApplications("test")...)
+			}
+		})
+		ValidateApplications(appCtxs)
+		for _, appCtx := range appCtxs {
+			bindMount, err := IsVMBindMounted(appCtx, false)
+			log.FailOnError(err, "Failed to verify bind mount")
+			dash.VerifyFatal(bindMount, true, "Failed to verify bind mount")
+		}
+
+		stepLog = "Increase the volume HA of the kubevirt VM Volumes"
+		Step(stepLog, func() {
+			log.InfoD(stepLog)
+			for _, appCtx := range appCtxs {
+				vols, err := Inst().S.GetVolumes(appCtx)
+				log.FailOnError(err, "Failed to get volumes of kubevirt VM")
+				for _, vol := range vols {
+					currRep, err := Inst().V.GetReplicationFactor(vol)
+					log.FailOnError(err, "Failed to get Repl factor for vil %s", vol.Name)
+
+					if currRep < 3 {
+						opts := volume.Options{
+							ValidateReplicationUpdateTimeout: validateReplicationUpdateTimeout,
+						}
+						err = Inst().V.SetReplicationFactor(vol, currRep+1, nil, nil, true, opts)
+						dash.VerifyFatal(err, nil, fmt.Sprintf("Validate set repl factor to %d", currRep+1))
+					} else {
+						log.Warnf("Volume %s has reached maximum replication factor", vol.Name)
+					}
+				}
+			}
+		})
+
+		stepLog = "Verify if VM's are still bind mounted even after HA increase"
+		Step(stepLog, func() {
+			log.InfoD(stepLog)
+			for _, appCtx := range appCtxs {
+				isVmBindMounted, err := IsVMBindMounted(appCtx, true)
+				log.FailOnError(err, "Failed to run vm bind mount check")
+				if !isVmBindMounted {
+					log.Errorf("The newly added replication to vm %s is not bind mounted", appCtx.App.Key)
+				}
+			}
+		})
+		stepLog = "Destroy Applications"
+		Step(stepLog, func() {
+			log.InfoD(stepLog)
+			DestroyApps(appCtxs, nil)
+		})
+	})
+
+	JustAfterEach(func() {
+		defer EndTorpedoTest()
+		AfterEachTest(appCtxs)
+	})
+})
+
+var _ = Describe("{KubevirtVMVolHaDecrease}", func() {
+
+	JustBeforeEach(func() {
+		StartTorpedoTest("KubevirtVMVolHaDecrease", "Decrease the replication factor of kubevirt Vms", nil, 0)
+	})
+	var appCtxs []*scheduler.Context
+
+	itLog := "Decrease the volume HA of a kubevirt VM"
+	It(itLog, func() {
+		log.InfoD(stepLog)
+		appList := Inst().AppList
+		defer func() {
+			Inst().AppList = appList
+		}()
+
+		Inst().AppList = []string{"kubevirt-fio-load-multi-disk"}
+		stepLog := "schedule a kubevirt VM"
+		Step(stepLog, func() {
+			log.InfoD(stepLog)
+			for i := 0; i < Inst().GlobalScaleFactor; i++ {
+				appCtxs = append(appCtxs, ScheduleApplications("test")...)
+			}
+		})
+		ValidateApplications(appCtxs)
+		for _, appCtx := range appCtxs {
+			bindMount, err := IsVMBindMounted(appCtx, false)
+			log.FailOnError(err, "Failed to verify bind mount")
+			dash.VerifyFatal(bindMount, true, "Failed to verify bind mount")
+		}
+
+		stepLog = "Decrease the volume HA of the kubevirt VM Volumes"
+		Step(stepLog, func() {
+			log.InfoD(stepLog)
+			for _, appCtx := range appCtxs {
+				vols, err := Inst().S.GetVolumes(appCtx)
+				log.FailOnError(err, "Failed to get volumes of kubevirt VM")
+				for _, vol := range vols {
+					currRep, err := Inst().V.GetReplicationFactor(vol)
+					log.FailOnError(err, "Failed to get Repl factor for vil %s", vol.Name)
+
+					if currRep > 1 {
+						opts := volume.Options{
+							ValidateReplicationUpdateTimeout: validateReplicationUpdateTimeout,
+						}
+						err = Inst().V.SetReplicationFactor(vol, currRep-1, nil, nil, true, opts)
+						dash.VerifyFatal(err, nil, fmt.Sprintf("Validate set repl factor to %d", currRep-1))
+					} else {
+						log.Warnf("Volume %s has reached maximum replication factor", vol.Name)
+					}
+				}
+			}
+		})
+
+		stepLog = "Verify if VM's are still bind mounted even after HA De"
+		Step(stepLog, func() {
+			log.InfoD(stepLog)
+			for _, appCtx := range appCtxs {
+				isVmBindMounted, err := IsVMBindMounted(appCtx, true)
+				log.FailOnError(err, "Failed to run vm bind mount check")
+				if !isVmBindMounted {
+					log.Errorf("The newly added disk to vm %s is not bind mounted", appCtx.App.Key)
+				}
+			}
+		})
+		stepLog = "Destroy Applications"
+		Step(stepLog, func() {
+			log.InfoD(stepLog)
+			DestroyApps(appCtxs, nil)
+		})
+	})
+
 	JustAfterEach(func() {
 		defer EndTorpedoTest()
 		AfterEachTest(appCtxs)
