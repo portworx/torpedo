3276ae9baac4f8864b39a0f8d4ecbb473ba459b7
jkagliwal-px <jkagliwal@purestorage.com>
2024-04-12 21:48:17 +0530

    Removed extra log

--- drivers/unifiedPlatform/pdsLibs/dsLib.go
+++ drivers/unifiedPlatform/pdsLibs/dsLib.go
@@ -88,7 +88,6 @@ func GetDeployment(deploymentId string) (*automationModels.PDSDeploymentResponse
 // DeployDataService Deploys the dataservices based on the given params
 func DeployDataService(ds PDSDataService, namespaceId, projectId, targetClusterId, imageId, appConfigId, resConfigId, stConfigId string) (*automationModels.PDSDeploymentResponse, error) {
 	log.Info("Data service will be deployed as per the config map passed..")
-	log.InfoD("ProjectId is - [%v]", projectId)
 	depInputs := &automationModels.PDSDeploymentRequest{
 		Create: automationModels.PDSDeployment{
 			NamespaceID: namespaceId,
