f56f80ed420e6afcf433a2bd2a4f5839800fcdee
vprabhakar-px <vprabhakar@purestorage.com>
2024-04-23 12:14:28 +0530

    Create filesystem routine implementation

--- drivers/pure/flashblade/filesystems.go
+++ drivers/pure/flashblade/filesystems.go
@@ -14,6 +14,30 @@ func (fs *FileSystemService) GetAllFileSystems(params map[string]string, data in
 	return m, nil
 }
 
+// CreateNewFileSystem Creates New Filesystem on the cluster
+// fsName : Name of the filesystem that needs to be created
+// Data should be Interface map[string]interface{}
+/* e.x :
+    {
+	"nfs": {
+		"v3_enabled": true,
+		"v4_1_enabled": true,
+		"rules": "*(rw,no_root_squash)"
+		}
+}
+*/
+func (fs *FileSystemService) CreateNewFileSystem(fsName string, data interface{}) ([]FsItem, error) {
+	queryParams := make(map[string]string)
+	queryParams["names"] = fsName
+	req, _ := fs.client.NewRequest("POST", "file-systems", queryParams, data)
+	m := []FsItem{}
+	_, err := fs.client.Do(req, &m, true)
+	if err != nil {
+		return nil, err
+	}
+	return m, nil
+}
+
 /* Below set of Functions applicable for Snapshot Scheduling Policies for the filesystem */
 // GetSnapshotSchedulingPolicies Get list of Snapshot Scheduling policies of the filesystem
 func (fs *FileSystemService) GetSnapshotSchedulingPolicies(params map[string]string, data interface{}) ([]PolicyResponse, error) {
