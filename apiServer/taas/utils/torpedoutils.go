3bfeaa791a6ac34e61bb6ef73e1b088597f9e330
snigdha-px <sagrawal@purestorage.com>
2024-04-24 17:58:19 +0530

    Added snapclass creation

--- apiServer/taas/utils/torpedoutils.go
+++ apiServer/taas/utils/torpedoutils.go
@@ -199,7 +199,7 @@ func ScheduleAppsAndValidate(c *gin.Context) {
 	}
 
 	tests.Inst().AppList = requestBody.AppList
-	options := tests.CreateScheduleOptions(requestBody.NamespaceSuffix)
+	options := tests.CreateScheduleOptions("")
 	context, err := tests.Inst().S.Schedule(requestBody.NamespaceSuffix, options)
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
