d6ac22df5c0bdca09cff1da1c736ca89bb136676
sabrarhussaini <shussaini@purestorage.com>
2024-05-17 14:17:10 +0530

    Included torpedo changes for azure backup location for Pxb-2.7.1

--- deployments/deploy-ssh.sh
+++ deployments/deploy-ssh.sh
@@ -600,6 +600,8 @@ spec:
       value: "${TORPEDO_SSH_PASSWORD}"
     - name: TORPEDO_SSH_KEY
       value: "${TORPEDO_SSH_KEY}"
+    - name: AZURE_REGION
+      value: "${AZURE_REGION}"
     - name: AZURE_TENANT_ID
       value: "${AZURE_TENANTID}"
     - name: VOLUME_PROVIDER
