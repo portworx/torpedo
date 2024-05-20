e69df0b301cd1465df9c13fb15f3c9b390d75c75
sayalasomayajula-px <sayalasomayajula@purestorage.com>
2024-05-20 09:00:51 +0530

    [PTX-23995] Create pvc with iops and bandwidth

--- pkg/pureutils/purefb.go
+++ pkg/pureutils/purefb.go
@@ -4,12 +4,13 @@ import (
 	"fmt"
 	"github.com/portworx/torpedo/drivers/pure/flashblade"
 	"regexp"
+	"strings"
 )
 
 // PureCreateClientAndConnect Create FB Client and Connect
 func PureCreateFbClientAndConnect(fbMgmtEndpoint string, apiToken string) (*flashblade.Client, error) {
-	fbClient, err := flashblade.NewClient(fbMgmtEndpoint, "", "", apiToken,
-		"", false, false, "", nil)
+	fbClient, err := flashblade.NewClient(fbMgmtEndpoint, apiToken, "", "",
+		"2.2", false, false, "", nil)
 	if err != nil {
 		return nil, err
 	}
@@ -40,7 +41,7 @@ func GetFBClientMapFromPXPureSecret(secret PXPureSecret) (map[string]*flashblade
 
 // GetApiTokenForMgmtEndpoints Returns API token for Mgmt Endpoints
 func GetApiTokenForFbMgmtEndpoints(secret PXPureSecret, mgmtEndPoint string) string {
-	for _, faDetails := range secret.Arrays {
+	for _, faDetails := range secret.Blades {
 		if faDetails.MgmtEndPoint == mgmtEndPoint {
 			return faDetails.APIToken
 		}
@@ -97,6 +98,22 @@ func CreateNewFileSystem(fbClient *flashblade.Client, fsName string, data interf
 		return nil, err
 	}
 	return fileSys, nil
+}
+
+// GetFilesystemFullName Returns Full Name of the Filesystem
+func GetFilesystemFullName(fbClient *flashblade.Client, fsName string) (string, error) {
+	allFs, err := ListAllFileSystems(fbClient)
+	if err != nil {
+		return "", err
+	}
+	for _, eachPvc := range allFs {
+		for _, eachItem := range eachPvc.Items {
+			if strings.Contains(eachItem.Name, fsName) {
+				return eachItem.Name, nil
+			}
+		}
+	}
+	return "", nil
 }
 
 // DeleteFileSystem Deletes Filesystem from cluster
