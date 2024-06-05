ee464d9482a54b3c99669346826d1c0906d966ba
sayalasomayajula-px <sayalasomayajula@purestorage.com>
2024-06-05 19:00:22 +0530

    changes made according to comments

--- drivers/pure/flasharray/pods.go
+++ drivers/pure/flasharray/pods.go
@@ -1,9 +1,26 @@
 package flasharray
 
+type PodServices struct {
+	client *Client
+}
+
 func (p *PodServices) CreatePod(params map[string]string, data interface{}) (*[]PodResponse, error) {
 	req, _ := p.client.NewRequest("POST", "pods", params, data)
 	m := &[]PodResponse{}
 	_, err := p.client.Do(req, m)
+	if err != nil {
+		return nil, err
+	}
+	return m, nil
+}
+
+func (p *PodServices) ListAllAvailablePods(params map[string]string, data interface{}) ([]PodResponse, error) {
+	req, err := p.client.NewRequest("GET", "pods", params, data)
+	if err != nil {
+		return nil, err
+	}
+	m := []PodResponse{}
+	_, err = p.client.Do(req, &m)
 	if err != nil {
 		return nil, err
 	}
