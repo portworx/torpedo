4faaa8452072aca39453dca2cfbc4135eed7f1e2
sayalasomayajula-px <sayalasomayajula@purestorage.com>
2024-06-13 11:18:43 +0530

    added requested changes

--- tests/basic/pure_test.go
+++ tests/basic/pure_test.go
@@ -5234,7 +5234,7 @@ var _ = Describe("{ValidatePodNameinVolume}", func() {
 			}
 			log.InfoD("List of PVC names [%v]", listofPvcNames)
 			for _, pvcName := range listofPvcNames {
-				expectedVolName := PodNameinFA + "::" + clusterUUIDfirstPart + "-" + pvcName
+				expectedVolName := PodNameinFA + "::" + "px_" + clusterUUIDfirstPart + "-" + pvcName
 				log.InfoD("Expected Volume Name [%v]", expectedVolName)
 				volName, err := pureutils.GetCompleteVolumeNameFromFA(faClient, pvcName)
 				log.FailOnError(err, fmt.Sprintf("Failed to get volume name for volume [%v]", pvcName))
