106cd8ab06b47ddc0b03a8a873e09d00eed8decb
sayalasomayajula-px <sayalasomayajula@purestorage.com>
2024-06-24 15:13:24 +0530

    improvements

--- tests/common.go
+++ tests/common.go
@@ -8235,7 +8235,7 @@ func StartTorpedoTest(testName, testDescription string, tags map[string]string, 
 			faClient := PureFAMgmtMap[faMgmtIP]
 			networkInterfaces, err := pureutils.ListAllInterfaces(faClient)
 			log.FailOnError(err, "failed to list network interfaces on FA with IP [%s]", faMgmtIP)
-			for _, networkInterface := range *networkInterfaces {
+			for _, networkInterface := range networkInterfaces {
 				if networkInterface.Eth.Address == faMgmtIP {
 					for _, service := range networkInterface.Services {
 						if strings.Contains(service, "management") {
