f8e89d8028271a1f6171e277537f714c0d5f6bd6
sayalasomayajula-px <162689439+sayalasomayajula-px@users.noreply.github.com>
2024-07-15 08:41:14 +0530

    Test Improvements in Storagepool test and FA (#2675)

--- tests/basic/storage_pool_test.go
+++ tests/basic/storage_pool_test.go
@@ -8941,16 +8941,19 @@ var _ = Describe("{VolumeHAPoolOpsNoKVDBleaderDown}", func() {
 		log.InfoD("Pool UUID on which IO is running [%s]", poolUUID)
 
 		terminate := false
+		var deleteMutex sync.Mutex
 		stopRoutine := func() {
 			if !terminate {
 				terminate = true
 				time.Sleep(1 * time.Minute) // Wait for 1 min to settle down all other go routines to terminate
+				deleteMutex.Lock()
 				for _, each := range volumesCreated {
 					if IsVolumeExits(each) {
 						log.FailOnError(Inst().V.DeleteVolume(each), "volume deletion failed on the cluster with volume ID [%s]", each)
 					}
 
 				}
+				deleteMutex.Unlock()
 
 			}
 		}
@@ -8958,7 +8961,6 @@ var _ = Describe("{VolumeHAPoolOpsNoKVDBleaderDown}", func() {
 		defer stopRoutine()
 
 		// Wait for KVDB Nodes up and running and in healthy state
-		// Go routine to kill kvdb master in regular intervals
 		go func() {
 			defer wg.Done()
 			defer GinkgoRecover()
@@ -8971,8 +8973,6 @@ var _ = Describe("{VolumeHAPoolOpsNoKVDBleaderDown}", func() {
 					stopRoutine()
 					log.FailOnError(err, "not all kvdb members in healthy state")
 				}
-				// Wait for some time after killing kvdb master Node
-				time.Sleep(5 * time.Minute)
 			}
 		}()
 
@@ -9054,6 +9054,7 @@ var _ = Describe("{VolumeHAPoolOpsNoKVDBleaderDown}", func() {
 						}
 
 						time.Sleep(5 * time.Second)
+						deleteMutex.Lock()
 						// Delete the Volume
 						err = Inst().V.DeleteVolume(eachVol)
 						if err != nil {
@@ -9061,12 +9062,8 @@ var _ = Describe("{VolumeHAPoolOpsNoKVDBleaderDown}", func() {
 							log.FailOnError(err, "failed to delete volume with volume ID [%s]", eachVol)
 						}
 
-						// Remove the first element
-						for i := 0; i < len(volumesCreated)-1; i++ {
-							volumesCreated[i] = volumesCreated[i+1]
-						}
-						// Resize the array by truncating the last element
-						volumesCreated = volumesCreated[:len(volumesCreated)-1]
+						volumesCreated = volumesCreated[1:]
+						deleteMutex.Unlock()
 					}
 					if terminate {
 						break
@@ -10825,10 +10822,10 @@ var _ = Describe("{HAIncreasePoolresizeAndAdddisk}", func() {
 
 var _ = Describe("{PoolResizeInTrashCanNode}", func() {
 	/*
-	  1. Deploy apps
-	  2. Pick a volume and locate the node where this is attached
-	  3. Delete the volume and let it be placed in trashcan
-	  4. Trigger pool expand in the node where the trashcan volume is present
+	   1. Deploy apps
+	   2. Pick a volume and locate the node where this is attached
+	   3. Delete the volume and let it be placed in trashcan
+	   4. Trigger pool expand in the node where the trashcan volume is present
 
 	*/
 
@@ -11057,8 +11054,8 @@ var _ = Describe("{CheckPoolOffline}", func() {
 var _ = Describe("{FACDPoolIOPriorityCheck}", func() {
 
 	/* This test is created to provide functional testing coverage for ticket PWX-35590
-	1. Create a cluster with FACD backend
-	2. Check if the IO Priority for the storagepools is HIGH
+	   1. Create a cluster with FACD backend
+	   2. Check if the IO Priority for the storagepools is HIGH
 	*/
 
 	JustBeforeEach(func() {
