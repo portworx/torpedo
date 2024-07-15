2114919a7307a5e1a55e78701aafd72e135a3763
sayalasomayajula-px <sayalasomayajula@purestorage.com>
2024-07-15 19:48:23 +0530

    changed according to comments

--- drivers/volume/portworx/portworx.go
+++ drivers/volume/portworx/portworx.go
@@ -1422,7 +1422,10 @@ func (d *portworx) ValidateCreateVolume(volumeName string, params map[string]str
 		// Checking the device path when state is attached
 		if vol.State == api.VolumeState_VOLUME_STATE_ATTACHED && !strings.Contains(vol.DevicePath, DeviceMapper) {
 			_, err := task.DoRetryWithTimeout(volumeInspectDevicePath, 5*time.Minute, inspectVolumeRetryInterval)
-			return fmt.Errorf("Failed to validate the device path for a volume [%s] : %v", volumeName, err)
+			return &ErrFailedToInspectVolume{
+				ID:    volumeName,
+				Cause: fmt.Sprintf("Failed to validate device path [%s]: [%v]", vol.DevicePath, err),
+			}
 		}
 		log.Debugf("Successfully validated the device path for a volume [%s]", volumeName)
 	}
