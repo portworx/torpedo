346fabe29eebe28567d236a053d860339622b9f5
apattewar-px <apattewar@purestorage.com>
2024-07-15 12:22:48 +0530

    Fixed error

--- tests/basic/sample_test.go
+++ tests/basic/sample_test.go
@@ -5,6 +5,7 @@ import (
 	. "github.com/onsi/ginkgo/v2"
 	"github.com/portworx/torpedo/drivers/node"
 	"github.com/portworx/torpedo/pkg/log"
+	//"github.com/portworx/torpedo/drivers/scheduler"
 	. "github.com/portworx/torpedo/tests"
 )
 
@@ -16,19 +17,21 @@ var _ = Describe("{VerifyNoNodeRestartUponPxPodRestart}", func() {
 		StartTorpedoTest("VerifyNoNodeRestartUponPxPodRestart", "Verify that px serivce remain up even if px pod got deleted ", nil, 0)
 	})
 
-	err = DeletePXPods("portworx")
+	It("has to setup, validate and teardown apps", func() {
+		err = DeletePXPods("portworx")
 
-	//var contexts []*scheduler.Context
-	//Delete perticular pod
+		//var contexts []*scheduler.Context
+		//Delete perticular pod
 
-	stepLog = "Validate PX on all nodes"
-	//storageNodeList :=
-	Step(stepLog, func() {
-		log.InfoD(stepLog)
-		for _, node := range node.GetStorageNodes() {
-			status, err := IsPxRunningOnNode(&node)
-			log.FailOnError(err, fmt.Sprintf("Failed to check if PX is running on node [%s]", node.Name))
-			dash.VerifySafely(status, true, fmt.Sprintf("PX is not running on node [%s]", node.Name))
-		}
+		stepLog = "Validate PX on all nodes"
+		//storageNodeList :=
+		Step(stepLog, func() {
+			log.InfoD(stepLog)
+			for _, node := range node.GetStorageNodes() {
+				status, err := IsPxRunningOnNode(&node)
+				log.FailOnError(err, fmt.Sprintf("Failed to check if PX is running on node [%s]", node.Name))
+				dash.VerifySafely(status, true, fmt.Sprintf("PX is not running on node [%s]", node.Name))
+			}
+		})
 	})
 })
