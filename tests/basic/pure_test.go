455a4c17af24cce2264e806478a2d70a63df65c2
suhas <122518640+spai-px@users.noreply.github.com>
2024-09-10 23:25:22 +0530

    Update pure_test.go

--- tests/basic/pure_test.go
+++ tests/basic/pure_test.go
@@ -2354,7 +2354,7 @@ var _ = Describe("{FADAVolMigrateValidation}", func() {
 			stepLog = "run the multipath -ll command on the node where the pods were scheduled before deleting"
 			Step(stepLog, func() {
 				// sleep for 60 seconds for all the entries to update
-				time.Sleep(30 * time.Second)
+				time.Sleep(120 * time.Second)
 				log.InfoD("Sleeping for 30 seconds for all the entries to update")
 				cmd := fmt.Sprintf("multipath -ll")
 				output, err := runCmd(cmd, selectedNode)
