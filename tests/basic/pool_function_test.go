6b0da10fe9a9bea3bb28fb74275f8faff8773c11
Raj Sahu <rsahu@purestorage.com>
2024-11-11 10:17:49 +0530

    HAZEL-1321 : Automate drive add metadata with node maintenance cycle (#3014)
    * HAZEL-1321
    
    * HAZEL-1321
    
    * HAZEL-1321
    
    * HAZEL-1321

--- tests/basic/pool_function_test.go
+++ tests/basic/pool_function_test.go
@@ -2940,6 +2940,158 @@ var _ = Describe("{DriveAddAsJournalWithPXRestart}", Label("p1", "hal_ops_disrep
 	})
 })
 
+var _ = Describe("{AddMetadataDriveWithNodeMaintenanceCycle}", Label("p1", "hal_ops_disruption", "NodeMaintenance", "AddMetadata", "functional"), func() {
+
+	var (
+		selectedNode node.Node
+		contexts     []*scheduler.Context
+		kvdbNodesIDs []string
+		path         string
+	)
+
+	JustBeforeEach(func() {
+		StartTorpedoTest("AddMetadataDriveWithNodeMaintenanceCycle", "Add Metadata Drive With node maintenance cycle", nil, 0)
+	})
+
+	itLog := "AddMetadataDriveWithNodeMaintenanceCycle"
+	It(itLog, func() {
+		stepLog := "Schedule Apps"
+		Step(stepLog, func() {
+			log.InfoD(stepLog)
+			contexts = scheduleApps()
+			time.Sleep(5 * time.Minute)
+			log.InfoD("schedule app succeed")
+		})
+		storageNodes := node.GetStorageNodes()
+		index := rand.Intn(len(storageNodes))
+		tNode := storageNodes[index]
+
+		stepLog = "Get KVDB nodes"
+		Step(stepLog, func() {
+			log.InfoD(stepLog)
+			kvdbMembers, err := Inst().V.GetKvdbMembers(tNode)
+			log.FailOnError(err, "Error getting KVDB members")
+			log.InfoD("kvdb members %+v", kvdbMembers)
+			for _, n := range kvdbMembers {
+				kvdbNodesIDs = append(kvdbNodesIDs, n.Name)
+			}
+		})
+
+		stepLog = "Check which node has a metadata disk if not add one"
+		Step(stepLog, func() {
+			log.InfoD(stepLog)
+			isDedicatedMetadataDiskExist := false
+			//Check which node has metadata disk if not add one
+			for _, storageNode := range storageNodes {
+
+				path, err := getMetaDataDiskPath(storageNode)
+				log.FailOnError(err, "Failed to get metadata disk")
+				if path != "" {
+					log.InfoD("Metadata disk path: %v", path)
+					isDedicatedMetadataDiskExist = true
+					tNode = storageNode
+					break
+				}
+			}
+			if !isDedicatedMetadataDiskExist {
+				for _, storageNode := range storageNodes {
+					deviceSpec := fmt.Sprintf("size=100 --metadata")
+					log.InfoD("Initiate add cloud drive and validate")
+					// enter pool maintenance mode
+					if Contains(kvdbNodesIDs, storageNode.Id) {
+						log.InfoD("[%s] is kvdb node", storageNode.Hostname)
+						continue
+					}
+
+					stepLog := "Enter maintenance mode"
+					Step(stepLog, func() {
+						log.InfoD(stepLog)
+						err = Inst().V.EnterPoolMaintenance(storageNode)
+						log.FailOnError(err, "node: %v failed to transition to pool maintenance mode", storageNode.Name)
+						log.Info("enter pool maintenance mode succeed")
+					})
+
+					stepLog = "Add metadata disk"
+					Step(stepLog, func() {
+						log.InfoD(stepLog)
+						err := Inst().V.AddCloudDrive(&storageNode, deviceSpec, -1)
+						log.FailOnError(err, "Failed to add metadata device on node : %s", storageNode.Name)
+						log.InfoD("metadata disk added successfully on node [%s]", storageNode.Hostname)
+					})
+
+					sleepTime := rand.Intn(100) + 1
+					time.Sleep(time.Second * (time.Duration(sleepTime)))
+					stepLog = fmt.Sprintf("Performing node maintenance cycle on node [%s] after [%d] seconds", storageNode.Name, sleepTime)
+					Step(stepLog, func() {
+						log.InfoD(stepLog)
+						err = Inst().V.RecoverDriver(storageNode)
+						log.FailOnError(err, fmt.Sprintf("error performing maintenance cycle on node %s", storageNode.Name))
+						err = Inst().V.WaitDriverUpOnNode(storageNode, 5*time.Minute)
+						log.FailOnError(err, fmt.Sprintf("Driver is down on node %s", storageNode.Name))
+						dash.VerifyFatal(err == nil, true, fmt.Sprintf("PX is up after maintenance cycle on node %s", storageNode.Name))
+					})
+
+					// exit pool maintenance
+					stepLog = "Exit pool maintenance mode"
+					Step(stepLog, func() {
+						log.InfoD(stepLog)
+						err = Inst().V.ExitPoolMaintenance(storageNode)
+						log.FailOnError(err, "Node: %v Failed to exit out of maintenance mode", storageNode.Name)
+						log.Info("exit pool maintenance mode succeed")
+					})
+
+					selectedNode = storageNode
+					break
+				}
+			} else {
+				log.InfoD("Metadata disk already exist: [%s]", path)
+				Skip("Metadata disk already exist")
+			}
+			//check if selecteNode is empty or not
+			if selectedNode.Name == "" {
+				log.FailOnError(fmt.Errorf("No node found with metadata disk or metadata disks cannot be added to any nodes"), "No node found with metadata disk ")
+			}
+		})
+
+		//Check PX status
+		stepLog = "Check PX status"
+		Step(stepLog, func() {
+			log.InfoD(stepLog)
+			status, err := Inst().V.GetPxctlStatus(selectedNode)
+			log.FailOnError(err, fmt.Sprintf("failed to get pxctl status on node [%s]", selectedNode.Name))
+			dash.VerifyFatal(status == api.Status_STATUS_OK.String(), true, fmt.Sprintf("node [%s] status is up but PX cluster is not ok. Expected: %v Actual: %v",
+				selectedNode.Name, api.Status_STATUS_OK, status))
+			log.Infof("px status [%v]", status)
+
+		})
+
+		stepLog = fmt.Sprintf("Check if apps are running")
+		Step(stepLog, func() {
+			log.InfoD(stepLog)
+			ValidateApplications(contexts)
+			log.Info("validate application succeed")
+		})
+
+		stepLog = "validate metadata device has been added successfully"
+		Step(stepLog, func() {
+			log.InfoD(stepLog)
+			//get metadata device path from pxctl status
+			metadataDevicePath, err := getMetaDataDiskPath(selectedNode)
+			log.FailOnError(err, fmt.Sprintf("failed to get pxctl status on node [%s]", selectedNode.Name))
+			if metadataDevicePath == "" {
+				log.FailOnError(fmt.Errorf("metadata device not added"), "metadata device not added")
+			}
+			log.InfoD("Metadata device path from pxctl : %v", metadataDevicePath)
+		})
+
+	})
+	JustAfterEach(func() {
+		defer EndTorpedoTest()
+		appsValidateAndDestroy(contexts)
+		AfterEachTest(contexts)
+	})
+})
+
 var _ = Describe("{AddMetadataDriveWithNodeReboot}", Label("p1", "hal_ops_disruption", "node_reboot", "AddMetadata", "functional"), func() {
 
 	var (
