2896cdd6f24b0f2cc30ac45d1329fea693404023
Subha Palani <spalani@purestorage.com>
2024-11-26 09:32:06 +0530

    Hazel-1087-sharedvolcase (#3103)
    * Hazel-1087-sharedvolcase
    
    * add postive case
    
    * add postive case
    
    * add in env
    
    * add in env
    
    * add in env
    
    * add in env
    
    * add in env
    
    * add in steps
    
    * Addressed review comments
    
    * Address review comments
    
    * Address review comments
    
    * Address review comments
    
    * Address review comments
    
    * Address review comments
    
    * Address review comments
    
    * Address review comments
    
    * Address review comments
    
    * Address review comments
    
    * Address review comments
    
    * Address review comments

--- tests/basic/sharedv4_multivol_test.go
+++ tests/basic/sharedv4_multivol_test.go
@@ -2,13 +2,23 @@ package tests
 
 import (
 	"fmt"
-	"github.com/pure-px/torpedo/pkg/log"
+	"strings"
 	"time"
+
+	"github.com/portworx/sched-ops/k8s/core"
+	"github.com/portworx/sched-ops/k8s/storage"
+	"github.com/portworx/sched-ops/task"
+	"github.com/pure-px/torpedo/pkg/log"
+	corev1 "k8s.io/api/core/v1"
+	storageApi "k8s.io/api/storage/v1"
+	"k8s.io/apimachinery/pkg/api/resource"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	"github.com/pure-px/torpedo/drivers/node"
 	"github.com/pure-px/torpedo/drivers/scheduler"
+	"github.com/pure-px/torpedo/drivers/scheduler/k8s"
 	"github.com/pure-px/torpedo/drivers/volume"
 	"github.com/pure-px/torpedo/pkg/testrailuttils"
 	. "github.com/pure-px/torpedo/tests"
@@ -233,3 +243,221 @@ func sv4KillANodeAndValidate(nodeToKill node.Node) {
 	err = Inst().V.RefreshDriverEndpoints()
 	dash.VerifyFatal(err, nil, "Validate volume driver end points refresh")
 }
+
+var _ = Describe("{PVCCreationWithRWXAccessModeAndSharedv4False}", Label("p0", "staging", "negative", "sharedv4"), func() {
+	/*
+		    Ticket id:https://purestorage.atlassian.net/browse/HAZEL-1087
+			step1: set this parameter "sharedv4: false" in sc file
+			step2: create pvc in have readwrite many.
+			step3: pvc should in pending state with this error msg
+	*/
+	var testrailID = 0
+	var runID int
+	JustBeforeEach(func() {
+		StartTorpedoTest("PVCCreationWithRWXAccessModeAndSharedv4False", "When PVC with RWX is ignored due to sharedv4:false", nil, testrailID)
+		runID = testrailuttils.AddRunsToMilestone(testrailID)
+	})
+
+	stepLog := "Verify sc in file sharedv4 is false pvc with RWX Should not happen"
+	It(stepLog, func() {
+		log.InfoD(stepLog)
+		var (
+			scName     = fmt.Sprintf("test-sv4-sc-svc%v", time.Now().Unix())
+			pvcName    = fmt.Sprintf("test-sv4-pvc-svc-%v", time.Now().Unix())
+			namespace  = "default"
+			k8sStorage = storage.Instance()
+		)
+
+		stepLog = "Apply storage class with sharedv4:false"
+		Step(stepLog, func() {
+			log.InfoD(stepLog)
+			storageClass := storageApi.StorageClass{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: scName,
+				},
+				Provisioner: k8s.CsiProvisioner,
+				Parameters: map[string]string{
+					"sharedv4": "false",
+				},
+			}
+			_, err := k8sStorage.CreateStorageClass(&storageClass)
+			dash.VerifyFatal(err, nil, "Verifying creation of new storage class")
+		})
+
+		stepLog = "Apply persistent volume claim"
+		Step(stepLog, func() {
+			log.InfoD(stepLog)
+			_, err := core.Instance().CreatePersistentVolumeClaim(&corev1.PersistentVolumeClaim{
+				TypeMeta: metav1.TypeMeta{
+					Kind: "PersistentVolumeClaim",
+				},
+				ObjectMeta: metav1.ObjectMeta{
+					Name: pvcName,
+				},
+				Spec: corev1.PersistentVolumeClaimSpec{
+					AccessModes:      []corev1.PersistentVolumeAccessMode{corev1.ReadWriteMany},
+					StorageClassName: &scName,
+					Resources: corev1.ResourceRequirements{
+						Requests: corev1.ResourceList{
+							corev1.ResourceStorage: resource.MustParse("10Gi"),
+						},
+					},
+				},
+			})
+			dash.VerifyFatal(err, nil, "Verifying creation of new pvc")
+		})
+		log.Infof("Starting to monitor PVC status")
+		createdPVC, err := k8sCore.GetPersistentVolumeClaim(pvcName, namespace)
+		log.FailOnError(err, "unable to get pvc %v and namespace %v", pvcName, namespace)
+		if createdPVC.Status.Phase == "Pending" {
+			log.Infof("PVC status: %v", createdPVC.Status.Phase)
+
+			retryFunc := func() (interface{}, bool, error) {
+				log.Infof("Fetching PVC events...")
+				events := Inst().S.GetEvents()["PersistentVolumeClaim"]
+				if len(events) == 0 {
+					log.Infof("No events found for PersistentVolumeClaim, retrying...")
+					return nil, true, fmt.Errorf("No events found for PersistentVolumeClaim, retrying...")
+				}
+				for _, event := range events {
+					log.Infof("Checking event: %v", event)
+					if strings.Contains(event.Message, "Failed to create volume: ReadWriteMany PVC has a StorageClass that disallows sharedv4 volumes; use a different StorageClass") {
+						log.Infof("Volume creation error is: %v", event)
+						dash.VerifyFatal(createdPVC.Status.Phase == "Pending", true, "Check if volume creation status is pending")
+						errorMsg := "Failed to create volume: ReadWriteMany PVC has a StorageClass that disallows sharedv4 volumes; use a different StorageClass"
+						dash.VerifyFatal(strings.Contains(event.Message, errorMsg), true, "Check if the volume was not created due to sharedv4 issue")
+						return event, false, nil
+					}
+				}
+				return nil, true, fmt.Errorf("Event not found, retrying...")
+			}
+			_, err := task.DoRetryWithTimeout(retryFunc, 3*time.Minute, 30*time.Second)
+			log.FailOnError(err, "Retried for 3 minutes without finding the expected error message.")
+			log.Infof("Verified the error message when sharedv4 is false ")
+		} else {
+			log.Infof("PVC should not be in the bound state as sharedv4 is false")
+			err := fmt.Errorf("PVC should not be in the bound state because sharedv4 is false")
+			log.FailOnError(err, "Verified the error message when sharedv4 is false ")
+		}
+		Step("Cleanup", func() {
+			log.InfoD("Cleaning up resources")
+			err = core.Instance().DeletePersistentVolumeClaim(pvcName, namespace)
+			log.FailOnError(err, "Failed to delete PVC: %v", pvcName)
+			err = k8sStorage.DeleteStorageClass(scName)
+			log.FailOnError(err, "Failed to delete StorageClass: %v", scName)
+		})
+	})
+
+	JustAfterEach(func() {
+		defer EndTorpedoTest()
+		AfterEachTest(contexts, testrailID, runID)
+	})
+})
+
+var _ = Describe("{PVCCreationWithRWXAccessModeAndSharedv4True}", Label("p0", "staging", "positive", "sharedv4"), func() {
+	/*
+			step1: set this parameter "sharedv4: true" in sc file
+		    step2: pvc is rwx and it's should be bound
+	*/
+	var testrailID = 0
+	var runID int
+	JustBeforeEach(func() {
+		StartTorpedoTest("PVCCreationWithRWXAccessModeAndSharedv4True", "verifies PVC creation with RWX access mode and shared volume support for v4.", nil, testrailID)
+		runID = testrailuttils.AddRunsToMilestone(testrailID)
+	})
+
+	stepLog := "verifies PVC creation with RWX access mode and shared volume support for v4"
+	It(stepLog, func() {
+		log.InfoD(stepLog)
+		var (
+			scName     = fmt.Sprintf("test-sv4-sc-svc%v", time.Now().Unix())
+			pvcName    = fmt.Sprintf("test-sv4-pvc-svc-%v", time.Now().Unix())
+			namespace  = "default"
+			k8sStorage = storage.Instance()
+		)
+
+		stepLog = "Apply storage class with sharedv4:true"
+		Step(stepLog, func() {
+			log.InfoD(stepLog)
+			storageClass := storageApi.StorageClass{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: scName,
+				},
+				Provisioner: k8s.CsiProvisioner,
+				Parameters: map[string]string{
+					"sharedv4": "true",
+				},
+			}
+			_, err := k8sStorage.CreateStorageClass(&storageClass)
+			dash.VerifyFatal(err, nil, "Verifying creation of new storage class with sharedv4=true")
+		})
+
+		stepLog = "Apply persistent volume claim with RWX access mode"
+		Step(stepLog, func() {
+			log.InfoD(stepLog)
+			_, err := core.Instance().CreatePersistentVolumeClaim(&corev1.PersistentVolumeClaim{
+				TypeMeta: metav1.TypeMeta{
+					Kind: "PersistentVolumeClaim",
+				},
+				ObjectMeta: metav1.ObjectMeta{
+					Name: pvcName,
+				},
+				Spec: corev1.PersistentVolumeClaimSpec{
+					AccessModes:      []corev1.PersistentVolumeAccessMode{corev1.ReadWriteMany},
+					StorageClassName: &scName,
+					Resources: corev1.ResourceRequirements{
+						Requests: corev1.ResourceList{
+							corev1.ResourceStorage: resource.MustParse("10Gi"),
+						},
+					},
+				},
+			})
+			dash.VerifyFatal(err, nil, "Verifying creation of new PVC with RWX access mode")
+		})
+		log.Infof("Starting to monitor PVC status and PV attributes")
+		retryFunc := func() (interface{}, bool, error) {
+			createdPVC, err := core.Instance().GetPersistentVolumeClaim(pvcName, namespace)
+			if err != nil {
+				return nil, true, fmt.Errorf("failed to get PVC: %v", err)
+			}
+			if createdPVC.Status.Phase == "Pending" {
+				log.Infof("PVC status: Pending, still waiting to be bound. Retrying...")
+				return nil, true, fmt.Errorf("PVC status is neither Pending nor Bound, retrying...")
+			}
+
+			log.Infof("PVC successfully bound. PVC status: %v", createdPVC.Status.Phase)
+			if createdPVC.Spec.VolumeName == "" {
+				return nil, true, fmt.Errorf("PVC has no bound PersistentVolumeName. Retrying..")
+			}
+
+			pvName := createdPVC.Spec.VolumeName
+			pv, err := core.Instance().GetPersistentVolume(pvName)
+			if err != nil {
+				return nil, true, fmt.Errorf("failed to get PV: %v", err)
+			}
+
+			log.Infof("Persistent Volume details: %v", pv)
+			sharedv4Param, exists := pv.Spec.PersistentVolumeSource.CSI.VolumeAttributes["sharedv4"]
+			dash.VerifyFatal(exists, true, "Verify if the PV has sharedv4 attribute")
+			dash.VerifyFatal(sharedv4Param, "true", "Verify if sharedv4 attribute is set to true")
+
+			return createdPVC, false, nil
+
+		}
+		_, err := task.DoRetryWithTimeout(retryFunc, 3*time.Minute, 30*time.Second)
+		log.FailOnError(err, "Error checking PVC status and binding after retries")
+		log.Infof("PVC check completed and verified the PVC bounding and PV attributes successfully")
+		Step("Cleanup", func() {
+			log.InfoD("Cleaning up resources")
+			err = core.Instance().DeletePersistentVolumeClaim(pvcName, namespace)
+			log.FailOnError(err, "Failed to delete PVC: %v", pvcName)
+			err = k8sStorage.DeleteStorageClass(scName)
+			log.FailOnError(err, "Failed to delete StorageClass: %v", scName)
+		})
+	})
+
+	JustAfterEach(func() {
+		defer EndTorpedoTest()
+		AfterEachTest(contexts, testrailID, runID)
+	})
+})
