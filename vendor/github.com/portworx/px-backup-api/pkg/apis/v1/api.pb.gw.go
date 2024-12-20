30fba985db04e3ebb289cdec4eb6170d871bf9d5
Shalini Kumari <shkumari@purestorage.com>
2024-12-20 23:05:49 +0530

    PB-9059-vendor: vendor px-backup-api master to torpedo master (#3277)

--- vendor/github.com/portworx/px-backup-api/pkg/apis/v1/api.pb.gw.go
+++ vendor/github.com/portworx/px-backup-api/pkg/apis/v1/api.pb.gw.go
@@ -2480,6 +2480,41 @@ func request_Role_Permission_0(ctx context.Context, marshaler runtime.Marshaler,
 
 }
 
+var (
+	filter_ResourceCollector_ResourceTypes_0 = &utilities.DoubleArray{Encoding: map[string]int{"org_id": 0}, Base: []int{1, 1, 0}, Check: []int{0, 1, 2}}
+)
+
+func request_ResourceCollector_ResourceTypes_0(ctx context.Context, marshaler runtime.Marshaler, client ResourceCollectorClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq ResourceTypeRequest
+	var metadata runtime.ServerMetadata
+
+	var (
+		val string
+		ok  bool
+		err error
+		_   = err
+	)
+
+	val, ok = pathParams["org_id"]
+	if !ok {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "missing parameter %s", "org_id")
+	}
+
+	protoReq.OrgId, err = runtime.String(val)
+
+	if err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "type mismatch, parameter: %s, error: %v", "org_id", err)
+	}
+
+	if err := runtime.PopulateQueryParameters(&protoReq, req.URL.Query(), filter_ResourceCollector_ResourceTypes_0); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+
+	msg, err := client.ResourceTypes(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
+	return msg, metadata, err
+
+}
+
 // RegisterHealthHandlerFromEndpoint is same as RegisterHealthHandler but
 // automatically dials to "endpoint" and closes the connection when "ctx" gets done.
 func RegisterHealthHandlerFromEndpoint(ctx context.Context, mux *runtime.ServeMux, endpoint string, opts []grpc.DialOption) (err error) {
@@ -5256,4 +5291,73 @@ var (
 	forward_Role_Delete_0 = runtime.ForwardResponseMessage
 
 	forward_Role_Permission_0 = runtime.ForwardResponseMessage
+)
+
+// RegisterResourceCollectorHandlerFromEndpoint is same as RegisterResourceCollectorHandler but
+// automatically dials to "endpoint" and closes the connection when "ctx" gets done.
+func RegisterResourceCollectorHandlerFromEndpoint(ctx context.Context, mux *runtime.ServeMux, endpoint string, opts []grpc.DialOption) (err error) {
+	conn, err := grpc.Dial(endpoint, opts...)
+	if err != nil {
+		return err
+	}
+	defer func() {
+		if err != nil {
+			if cerr := conn.Close(); cerr != nil {
+				grpclog.Infof("Failed to close conn to %s: %v", endpoint, cerr)
+			}
+			return
+		}
+		go func() {
+			<-ctx.Done()
+			if cerr := conn.Close(); cerr != nil {
+				grpclog.Infof("Failed to close conn to %s: %v", endpoint, cerr)
+			}
+		}()
+	}()
+
+	return RegisterResourceCollectorHandler(ctx, mux, conn)
+}
+
+// RegisterResourceCollectorHandler registers the http handlers for service ResourceCollector to "mux".
+// The handlers forward requests to the grpc endpoint over "conn".
+func RegisterResourceCollectorHandler(ctx context.Context, mux *runtime.ServeMux, conn *grpc.ClientConn) error {
+	return RegisterResourceCollectorHandlerClient(ctx, mux, NewResourceCollectorClient(conn))
+}
+
+// RegisterResourceCollectorHandlerClient registers the http handlers for service ResourceCollector
+// to "mux". The handlers forward requests to the grpc endpoint over the given implementation of "ResourceCollectorClient".
+// Note: the gRPC framework executes interceptors within the gRPC handler. If the passed in "ResourceCollectorClient"
+// doesn't go through the normal gRPC flow (creating a gRPC client etc.) then it will be up to the passed in
+// "ResourceCollectorClient" to call the correct interceptors.
+func RegisterResourceCollectorHandlerClient(ctx context.Context, mux *runtime.ServeMux, client ResourceCollectorClient) error {
+
+	mux.Handle("GET", pattern_ResourceCollector_ResourceTypes_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		rctx, err := runtime.AnnotateContext(ctx, mux, req)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := request_ResourceCollector_ResourceTypes_0(rctx, inboundMarshaler, client, req, pathParams)
+		ctx = runtime.NewServerMetadataContext(ctx, md)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_ResourceCollector_ResourceTypes_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
+	return nil
+}
+
+var (
+	pattern_ResourceCollector_ResourceTypes_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 1, 0, 4, 1, 5, 2}, []string{"v1", "resourceType", "org_id"}, ""))
+)
+
+var (
+	forward_ResourceCollector_ResourceTypes_0 = runtime.ForwardResponseMessage
 )
