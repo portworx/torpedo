7eea77740de41d2b88fd85145cfc6eb10152a606
Krishna Phalgun <kphalgun@purestorage.com>
2025-02-17 15:15:51 +0530

    [PTX-29822] Increase WaitDriverUpOnNode Timeout In AddDiskPoolMaintenanceMode (#3586)

--- tests/basic/storage_pool_test.go
+++ tests/basic/storage_pool_test.go
@@ -4452,7 +4452,7 @@ var _ = Describe("{AddDiskPoolMaintenanceMode}", Label("p1", "negative", "error_
 			return nil, false, nil
 		}
 		_, err = task.DoRetryWithTimeout(t, 5*time.Minute, 1*time.Minute)
-		err = Inst().V.WaitDriverUpOnNode(*stNode, 5*time.Minute)
+		err = Inst().V.WaitDriverUpOnNode(*stNode, 10*time.Minute)
 		log.FailOnError(err, fmt.Sprintf("Driver is down on node %s", stNode.Name))
 		dash.VerifyFatal(err == nil, true, fmt.Sprintf("PX is up after maintenance cycle on node %s", stNode.Name))
 		status, err := Inst().V.GetNodeStatus(*stNode)
